8deff52ad5fb5f56344bffac00a06d84339855ab
Peter Beaman <pbeaman@akiban.com>
2010-01-05 23:15:58 +0000

    Add prototype CServer and WriteRowMessage classes.  Modified pom.xml to depend on the akiba-networking and akiba-message artifacts.

---
+++ iter7/src/main/java/com/akiba/cserver/CServer.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.akiba.cserver;
+
+import java.nio.ByteBuffer;
+import java.util.logging.Logger;
+
+import com.akiba.message.MessageRegistrationBase;
+import com.akiba.network.AkibaNetworkHandler;
+import com.akiba.network.CommEventNotifier;
+import com.akiba.network.NetworkHandlerFactory;
+/**
+ * 
+ * @author pbeaman
+ */
+public class CServer {
+
+	private static final Logger LOGGER = Logger.getLogger(CServer.class
+			.getName());
+
+	public static class ChannelNotifier implements CommEventNotifier {
+
+		private volatile int connectCounter = 0;
+
+		@Override
+		public void onConnect(AkibaNetworkHandler handler) {
+			int counter = ++connectCounter;
+			System.out.println("Connection #" + connectCounter + " created");
+			LOGGER.info("Connection #" + connectCounter + " created");
+			new Thread(new CServerRunnable(handler), "CServer_" + counter)
+					.start();
+		}
+
+		@Override
+		public void onDisconnect(AkibaNetworkHandler handler) {
+
+		}
+	}
+
+	/**
+	 * A Runnable that reads Network messages, acts on them and returns results.
+	 * 
+	 * @author peter
+	 * 
+	 */
+	private static class CServerRunnable implements Runnable {
+
+		private final AkibaNetworkHandler handler;
+
+		public CServerRunnable(final AkibaNetworkHandler handler) {
+			this.handler = handler;
+		}
+
+		public void run() {
+
+			final ByteBuffer bb = handler.getMsg();
+		}
+	}
+	
+	private static class MessageRegistration extends MessageRegistrationBase {
+	    public static synchronized void initialize()
+	    {
+	        if (!initialized()) {
+	            maxTypeCodes(1000);
+	            register(1, WriteRowMessage.class);
+	            markInitialized();
+	        }
+	    }
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		ChannelNotifier callback = new ChannelNotifier();
+		NetworkHandlerFactory.initializeNetwork("localhost", "8080",
+				(CommEventNotifier) callback);
+		MessageRegistration.initialize();
+		try {
+			//
+			// For now this is "crash-only software" - there is no
+			// graceful shutdown. Just kill or ctrl-c the process.
+			//
+			while (true) {
+				Thread.sleep(5000);
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		// TODO code application logic here
+		System.out.println("test this");
+		NetworkHandlerFactory.closeNetwork();
+	}
+
+}
---
+++ iter7/src/main/java/com/akiba/cserver/WriteRowMessage.java
@@ -0,0 +1,51 @@
+package com.akiba.cserver;
+
+import java.nio.ByteBuffer;
+
+import com.akiba.message.Message;
+
+public class WriteRowMessage extends Message {
+
+	private RowData rowData;
+
+	public WriteRowMessage(final short type) {
+		super(type);
+	}
+
+	public RowData getRowData() {
+		return rowData;
+	}
+
+	public void setRowData(RowData rowData) {
+		this.rowData = rowData;
+	}
+
+	public void read(ByteBuffer payload) {
+		super.read(payload);
+		if (!payload.hasArray()) {
+			throw new UnsupportedOperationException(
+					"This version requires a heap-based ByteBuffer");
+		}
+		byte[] bytes = payload.array();
+		rowData = new RowData(bytes, payload.position(), payload.limit()
+				- payload.position());
+	}
+
+	public void write(ByteBuffer payload) {
+		if (payload.hasArray() && payload.array() == rowData.getBytes()) {
+			final byte[] bytes = rowData.getBytes();
+			if (rowData.getBufferStart() != payload.position() + 2) {
+				System.arraycopy(bytes, rowData.getBufferStart(), bytes,
+						payload.position() + 2, rowData.getBufferLength());
+				super.write(payload);
+				payload
+						.position(payload.position()
+								+ rowData.getBufferLength());
+			}
+		} else {
+			super.write(payload);
+			payload.put(rowData.getBytes(), rowData.getBufferLength(), rowData
+					.getBufferLength());
+		}
+	}
+}
