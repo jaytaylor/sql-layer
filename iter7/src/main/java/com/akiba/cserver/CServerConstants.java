22d1f1453e8c100b06275e53c5beae8acc83cbd5
Thomas Jones-Low <tjoneslo@akiban.com>
2010-01-28 21:43:16 +0000

    Update the error values generated (and returned over the wire) to match those expected by MySQL.

--- iter7/src/main/java/com/akiba/cserver/CServerConstants.java
+++ iter7/src/main/java/com/akiba/cserver/CServerConstants.java
@@ -6,8 +6,8 @@ public interface CServerConstants {
 	public final static short END = 2;
 	public final static short ERR = 100;
 	public final static short MISSING_OR_CORRUPT_ROW_DEF = 99;
-	public final static short NON_UNIQUE = 101;
-	public final static short FOREIGN_KEY_MISSING= 102;
+	public final static short NON_UNIQUE = 121;	// copied from mysql include/my_base.h (line 377)
+	public final static short FOREIGN_KEY_MISSING= 151; // copied from mysql include/my_base.h (line 405)
 
 	public final static int MAX_VERSIONS_PER_TABLE = 65536;
 	public final static int MAX_GROUP_DEPTH = 256;
