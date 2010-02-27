2ced55f541988a097c9ef6581d9416ca5308ca03
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2010-02-27 03:14:56 +0000

    Updated chunkserver start script to use a config file.

--- conf/chunkserver.in.sh
+++ conf/chunkserver.in.sh
@@ -7,6 +7,9 @@ chunkserver_home=`dirname $0`/..
 # it's just used here in constructing the classpath.
 chunkserver_bin=$chunkserver_home/target/classes
 
+# the config file to use for the chunk server
+CONFIG_FILE="../conf/chunkserver.properties"
+
 # JAVA_HOME can optionally be set here
 #JAVA_HOME=/usr/local/jdk6
 
@@ -16,10 +19,9 @@ JVM_OPTS=" \
         -Xnoagent \
         -Xmx1G \
         -Djava.compiler=NONE \
+        -Dcom.akiban.config=${CONFIG_FILE} \
         -Xrunjdwp:transport=dt_socket,address=8000,suspend=n,server=y"
 
 # the JAR file we use to launch the chunk server
 JAR_FILE="../target/akiban-cserver-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
-# the config file to use for the chunk server
-CONFIG_FILE="../conf/cserver.properties"
