3527eb97cc0bcdf9acb57513404e38e3dcab56f6
Yuval Shavit <yshavit@akiban.com>
2010-11-29 17:30:27 +0000

    Removing unused class.

--- src/main/java/com/akiban/cserver/manage/SchemaMXBeanImpl.java
+++
@@ -1,7 +0,0 @@
-package com.akiban.cserver.manage;
-
-public class SchemaMXBeanImpl {
-    // PDB: temporarily has been replaced by PersistitStoreSchemaManager which
-    // now has the old code from this implementation and implements
-    // SchemaMXBean
-}
