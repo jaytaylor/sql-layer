423e3455c35ad5ac1d2fa9406c3a59a0ca37ae36
Yuval Shavit <yshavit@akiban.com>
2010-12-16 17:32:26 -0500

    Converting test to be parameterized. Next step is to put together the numbers from bug 687048

--- src/test/java/com/akiban/cserver/encoding/DecimalEncoderTest.java
+++ src/test/java/com/akiban/cserver/encoding/DecimalEncoderTest.java
@@ -4,27 +4,51 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
 import static junit.framework.Assert.*;
 
 public final class DecimalEncoderTest {
     @Test
-    public void t1() {
-        doTest("1234567890.1234", 14, 4, "0x810DFB38D204D2");
+    public void bug687048() {
+        class TestElement {
+            private final int precision;
+            private final int scale;
+            private final String asNumber;
+            private final String asHex;
+            
+            public TestElement(int precision, int scale, String asNumber, String asHex) {
+                this.precision = precision;
+                this.scale = scale;
+                this.asNumber = asNumber;
+                this.asHex = asHex;
+            }
+            
+            @Override
+            public String toString() {
+                return String.format("(%d, %d, %s, %s)", precision, scale, asNumber, asHex);
+            }
+        }
+
+        List<TestElement> tests = Arrays.asList(
+                // These next two aren't part of the bug, but we have them here anyway.
+                // One's an example from mysql docs, and the other caused us a problem before.
+                new TestElement(14, 4, "1234567890.1234", "0x810DFB38D204D2"),
+                new TestElement(12, 10, "90.1956251262", "0xDA0BA900A602")
+        );
+        
+        for (TestElement test : tests) {
+            doTest(test.toString(), test.asNumber, test.precision, test.scale, test.asHex);
+        }
     }
 
-    @Test
-    public void t2() {
-        doTest("90.1956251262", 12, 10, "0xDA0BA900A602");
-    }
-
-    private static void doTest(String expected, int precision, int scale, String bytesHex) {
+    private static void doTest(String label, String expected, int precision, int scale, String bytesHex) {
         byte[] bytes = bytes(bytesHex);
         StringBuilder sb = new StringBuilder();
         BigDecimal actual = DecimalEncoder.decodeAndParse(bytes, 0, precision, scale, sb);
 
-        assertEquals("BigDecimal", new BigDecimal(expected), actual);
-        assertEquals("String", expected, sb.toString());
+        assertEquals(label + ": BigDecimal", new BigDecimal(expected), actual);
+        assertEquals(label + ": String", expected, sb.toString());
     }
 
     private static byte[] bytes(String string) {
