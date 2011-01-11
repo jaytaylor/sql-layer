85b2ee3419b623d6cf7d4bdc6c95796e9b4c1842
Yuval Shavit <yshavit@akiban.com>
2011-01-11 11:31:28 -0500

    Reverting to old import style; didn't mean to change this.

--- src/main/java/com/akiban/ais/ddl/SchemaDef.java
+++ src/main/java/com/akiban/ais/ddl/SchemaDef.java
@@ -1,6 +1,17 @@
 package com.akiban.ais.ddl;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeMap;
 
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
