284db141cc41d8cfb89136148dd7166b148817d2
Yuval Shavit <yshavit@akiban.com>
2011-01-13 15:19:16 -0500

    Minor: formatting to make it easier to see how each test differs.

--- src/test/java/com/akiban/cserver/itests/bugs/bug687049/TableAndColumnDuplicationIT.java
+++ src/test/java/com/akiban/cserver/itests/bugs/bug687049/TableAndColumnDuplicationIT.java
@@ -9,22 +9,26 @@ public final class TableAndColumnDuplicationIT extends ApiTestBase {
 
     @Test
     public void sameTableAndColumn() throws InvalidOperationException {
-        doTest("tbl1", "id1", "tbl1", "id1");
+        doTest( "tbl1", "id1",
+                "tbl1", "id1");
     }
 
     @Test
     public void sameTableDifferentColumn() throws InvalidOperationException {
-        doTest("tbl1", "id1", "tbl1", "id2");
+        doTest( "tbl1", "id1",
+                "tbl1", "id2");
     }
 
     @Test
     public void differentTableSameColumn() throws InvalidOperationException {
-        doTest("tbl1", "id1", "tbl2", "id1");
+        doTest( "tbl1", "id1",
+                "tbl2", "id1");
     }
 
     @Test
     public void differentTableDifferentColumn() throws InvalidOperationException {
-        doTest("tbl1", "id1", "tbl2", "id2");
+        doTest( "tbl1", "id1",
+                "tbl2", "id2");
     }
 
     /**
