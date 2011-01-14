0332718b0b303f9b27a3c03845f5c0e39016a287
Yuval Shavit <eleusinian@gmail.com>
2011-01-13 22:48:18 -0500

    Refactoring the two quoting encoders to use the same one method.

--- src/main/java/com/akiban/cserver/Quote.java
+++ src/main/java/com/akiban/cserver/Quote.java
@@ -10,39 +10,31 @@ public enum Quote {
     SINGLE_QUOTE {
         @Override
         public void append(StringBuilder sb, String s) {
-            if (s == null) {
-                sb.append(s);
-                return;
-            }
-            sb.append('\'');
-            for (int i = 0; i < s.length(); i++) {
-                char ch = s.charAt(i);
-                if (ch == '\'' || ch == '\\') {
-                    sb.append('\\');
-                }
-                sb.append(ch);
-            }
-            sb.append('\'');
+            doAppend(sb, s, '\'');
         }
     },
     DOUBLE_QUOTE {
         @Override
         public void append(StringBuilder sb, String s) {
-            if (s == null) {
-                sb.append(s);
-                return;
-            }
-            sb.append('\"');
-            for (int i = 0; i < s.length(); i++) {
-                char ch = s.charAt(i);
-                if (ch == '\"' || ch == '\\') {
-                    sb.append('\\');
-                }
-                sb.append(ch);
-            }
-            sb.append('\"');
+            doAppend(sb, s, '"');
         }
     };
 
     public abstract void append(final StringBuilder sb, String s);
+    
+    private static void doAppend(StringBuilder sb, String s, char quote) {
+        if (s == null) {
+            sb.append(s);
+            return;
+        }
+        sb.append(quote);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == quote || ch == '\\') {
+                sb.append('\\');
+            }
+            sb.append(ch);
+        }
+        sb.append(quote);
+    }
 }
