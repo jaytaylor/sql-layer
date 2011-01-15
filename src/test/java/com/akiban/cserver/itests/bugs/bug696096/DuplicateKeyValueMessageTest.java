22f80ba1525f93ed14fe8f13cb4562d88c4b5ffc
Yuval Shavit <yshavit@akiban.com>
2011-01-14 19:02:16 -0500

    Further improvements to the test (validating in an @After that the data is unchanged), and fixing the bug in PersistitStore by refactoring all duplicate key exceptions from one, new method.

--- src/test/java/com/akiban/cserver/itests/bugs/bug696096/DuplicateKeyValueMessageTest.java
+++ src/test/java/com/akiban/cserver/itests/bugs/bug696096/DuplicateKeyValueMessageTest.java
@@ -5,6 +5,7 @@ import com.akiban.cserver.api.common.TableId;
 import com.akiban.cserver.api.dml.DuplicateKeyException;
 import com.akiban.cserver.api.dml.scan.NewRow;
 import com.akiban.cserver.itests.ApiTestBase;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,58 +17,67 @@ public final class DuplicateKeyValueMessageTest extends ApiTestBase {
     @Before
     public void setUp() throws InvalidOperationException {
         assert tableId == null;
-        tableId = createTable("schema_one", "table_one",
+        tableId = createTable("sa", "ta",
                 "c0 INT KEY",
                 "c1 int",
                 "c2 int",
                 "c3 int",
+                "name varchar(32)",
                 "UNIQUE (c1, c2)",
                 "UNIQUE my_key(c3)"
         );
         writeRows(
-                createNewRow(tableId, 0, 1, 2, 3),
-                createNewRow(tableId, 20, 21, 22, 23)
+                createNewRow(tableId, 10, 11, 12, 13, "from setup"),
+                createNewRow(tableId, 20, 21, 22, 23, "from setup")
+        );
+    }
+
+    @After
+    public void tearDown() throws InvalidOperationException {
+        expectFullRows(tableId,
+                createNewRow(tableId, 10L, 11L, 12L, 13L, "from setup"),
+                createNewRow(tableId, 20L, 21L, 22L, 23L, "from setup")
         );
     }
 
     @Test
     public void writeDuplicatesPrimary() {
-        duplicateOnWrite("PRIMARY", 0, 11, 12, 13);
+        duplicateOnWrite("PRIMARY", 10, 91, 92, 93);
     }
 
     @Test
     public void writeDuplicatesC1() {
-        duplicateOnWrite("c1", 10, 1, 2, 13);
+        duplicateOnWrite("c1", 90, 11, 12, 93);
     }
 
     @Test
     public void writeDuplicatesMyKey() {
-        duplicateOnWrite("my_key", 10, 11, 12, 3);
+        duplicateOnWrite("my_key", 90, 91, 92, 13);
     }
 
     @Test
     public void writeDuplicatesMultiple() {
-        duplicateOnWrite("PRIMARY", 0, 1, 2, 3);
+        duplicateOnWrite("PRIMARY", 10, 11, 12, 13);
     }
 
     @Test
     public void updateDuplicatesPrimary() {
-        duplicateOnUpdate("PRIMARY", 0, 11, 12, 13);
+        duplicateOnUpdate("PRIMARY", 10, 91, 92, 93);
     }
 
     @Test
     public void updateDuplicatesC1() {
-        duplicateOnUpdate("c1", 10, 1, 2, 13);
+        duplicateOnUpdate("c1", 90, 11, 12, 93);
     }
 
     @Test
     public void updateDuplicatesMyKey() {
-        duplicateOnUpdate("my_key", 10, 11, 12, 3);
+        duplicateOnUpdate("my_key", 90, 91, 92, 13);
     }
 
     @Test
     public void updateDuplicatesMultiple() {
-        duplicateOnUpdate("PRIMARY", 0, 1, 2, 3);
+        duplicateOnUpdate("PRIMARY", 10, 11, 12, 13);
     }
 
     private static void dupMessageValid(DuplicateKeyException e, String indexName) {
@@ -84,9 +94,10 @@ public final class DuplicateKeyValueMessageTest extends ApiTestBase {
 
     private void duplicateOnWrite(String indexName, int c0, int c1, int c2, int c3) {
         try {
-            writeRows(createNewRow(tableId, c0, c1, c2, c3));
+            writeRows(createNewRow(tableId, c0, c1, c2, c3, "from write"));
         } catch (DuplicateKeyException e) {
             dupMessageValid(e, indexName);
+            return;
         } catch (InvalidOperationException e) {
             throw new TestException("unexpected exception", e);
         }
@@ -95,8 +106,8 @@ public final class DuplicateKeyValueMessageTest extends ApiTestBase {
     
     private void duplicateOnUpdate(String indexName, int c0, int c1, int c2, int c3) {
         try {
-            NewRow oldRow = createNewRow(tableId, 20, 21, 22, 23);
-            NewRow newRow = createNewRow(tableId, c0, c1, c2, c3);
+            NewRow oldRow = createNewRow(tableId, 20, 21, 22, 23, "from setup");
+            NewRow newRow = createNewRow(tableId, c0, c1, c2, c3, "from update");
             dml().updateRow(session, oldRow, newRow);
         } catch (DuplicateKeyException e) {
             dupMessageValid(e, indexName);
