3438669fb7050b8962f920cec7aa914d931426d9
Yuval Shavit <yshavit@akiban.com>
2011-01-20 15:15:32 -0500

    Adding a test for decimal with just one arg. This fails parsing.

--- src/test/java/com/akiban/cserver/itests/bugs/bug704443/DecimalsWithoutParamsIT.java
+++ src/test/java/com/akiban/cserver/itests/bugs/bug704443/DecimalsWithoutParamsIT.java
@@ -1,5 +1,6 @@
 package com.akiban.cserver.itests.bugs.bug704443;
 
+import com.akiban.ais.model.Column;
 import com.akiban.ais.model.TableName;
 import com.akiban.cserver.InvalidOperationException;
 import com.akiban.cserver.api.common.TableId;
@@ -14,7 +15,7 @@ import static junit.framework.Assert.*;
 
 public final class DecimalsWithoutParamsIT extends ApiTestBase {
     @Test
-    public void bug704443() throws InvalidOperationException {
+    public void decimalHasNoParams() throws InvalidOperationException {
         TableId t1Id = createTable("schema", "t1",
                 "c1 DECIMAL NOT NULL, c2 DECIMAL NOT NULL, c3 DECIMAL NOT NULL, PRIMARY KEY(c1,c2,c3)"
         );
@@ -31,5 +32,36 @@ public final class DecimalsWithoutParamsIT extends ApiTestBase {
         expectFullRows( t1Id,
                 createNewRow(t1Id, new BigDecimal("10"), new BigDecimal("10"), new BigDecimal("10"))
         );
+
+        for(Column column : getUserTable(t1Id).getColumns()) {
+            assertEquals("param[0] for column " + column, Long.valueOf(10), column.getTypeParameter1());
+            assertEquals("param[1] for column " + column, Long.valueOf(0), column.getTypeParameter2());
+        }
+        assertEquals("columns.size()", 3, getUserTable(t1Id).getColumns().size());
+    }
+
+    @Test
+    public void decimalHasOneParams() throws InvalidOperationException {
+        TableId t1Id = createTable("schema", "t1",
+                "c1(17) DECIMAL NOT NULL,PRIMARY KEY(c1)"
+        );
+
+        Set<TableName> t1TableNameSet = new HashSet<TableName>();
+        t1TableNameSet.add(new TableName("schema", "t1"));
+
+        assertEquals("user tables", t1TableNameSet, getUserTables().keySet());
+        assertEquals("group tables size", 1, getGroupTables().size());
+
+        writeRows(
+                createNewRow(t1Id, "10", "10", "10")
+        );
+        expectFullRows( t1Id,
+                createNewRow(t1Id, new BigDecimal("10"), new BigDecimal("10"), new BigDecimal("10"))
+        );
+        for(Column column : getUserTable(t1Id).getColumns()) {
+            assertEquals("param[0] for column " + column, Long.valueOf(17), column.getTypeParameter1());
+            assertEquals("param[1] for column " + column, Long.valueOf(0), column.getTypeParameter2());
+        }
+        assertEquals("columns.size()", 1, getUserTable(t1Id).getColumns().size());
     }
 }
