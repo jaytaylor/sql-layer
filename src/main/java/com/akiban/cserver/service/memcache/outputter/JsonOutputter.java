4190d0403e6c10ac836ee30db168f2ffdf093a59
Yuval Shavit <yshavit@akiban.com>
2011-01-27 11:25:00 -0500

    Slight efficiencies the json output code -- making it more compact.

--- src/main/java/com/akiban/cserver/service/memcache/outputter/JsonOutputter.java
+++ src/main/java/com/akiban/cserver/service/memcache/outputter/JsonOutputter.java
@@ -36,25 +36,25 @@ public final class JsonOutputter implements HapiProcessor.Outputter {
             if(def_id_stack.isEmpty()) {
                 current_def_id = def_id;
                 def_id_stack.add(parent_def_id);
-                pr.write("{ \"");
+                pr.write("{\"");
                 pr.print(def.getTableName());
-                pr.write("\" : ");
+                pr.write("\":");
 //                if(min_val == null) {
 //                    sb.append(" [ ");
 //                }
             }
             else if(def_id == current_def_id) {
                 // another leaf on current branch (add to current open array)
-                pr.write(" }, ");
+                pr.write("},");
             }
             else if(parent_def_id == current_def_id) {
                 // down the tree, new branch (new open array)
                 current_def_id = def_id;
                 def_id_stack.add(parent_def_id);
 
-                pr.write(", \"");
+                pr.write(",\"");
                 pr.print(def.getTableName());
-                pr.write("\" : [ ");
+                pr.write("\":[");
             }
             else {
                 // a) sibling branch or b) up the tree to an old branch (close array for each step up)
@@ -62,43 +62,43 @@ public final class JsonOutputter implements HapiProcessor.Outputter {
                 int pop_count = 0;
                 int last = def_id_stack.size() - 1;
 
-                pr.write(" } ]");
+                pr.write("}]");
                 while(!def_id_stack.get(last).equals(parent_def_id)) {
                     if(pop_count++ > 0) {
                         pr.write(" ]");
                     }
-                    pr.write(" }");
+                    pr.write("}");
                     def_id_stack.remove(last--);
                 }
 
                 if(pop_count == 0) {
                     // Was sibling
-                    pr.write(", \"");
+                    pr.write(",\"");
                     pr.print(def.getTableName());
-                    pr.write("\" : [ ");
+                    pr.write("\":[");
                 }
                 else {
                     // Was child
-                    pr.write(", ");
+                    pr.write(',');
                 }
             }
 
             String json_row = data.toJSONString(cache);
-            pr.write("{ ");
+            pr.write('{');
             pr.print(json_row);
         }
 
         if(wrote) {
             int last = def_id_stack.size() - 1;
             while(last > 0) {
-                pr.write(" } ]");
+                pr.write("}]");
                 def_id_stack.remove(last--);
             }
-            pr.write(" }");
+            pr.write('}');
 //            if(min_val == null) {
 //                sb.append(" ]");
 //            }
-            pr.write(" }");
+            pr.write('}');
         }
         pr.flush();
     }
