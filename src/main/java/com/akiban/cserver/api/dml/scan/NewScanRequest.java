1af03884c57d0b083397a33e851ae35b9b763e80
Yuval Shavit <yshavit@akiban.com>
2011-01-31 12:20:02 -0500

    Removing IndexId

--- src/main/java/com/akiban/cserver/api/dml/scan/NewScanRequest.java
+++ src/main/java/com/akiban/cserver/api/dml/scan/NewScanRequest.java
@@ -18,19 +18,18 @@ package com.akiban.cserver.api.dml.scan;
 import java.util.Set;
 
 import com.akiban.cserver.api.common.IdResolver;
-import com.akiban.cserver.api.common.IndexId;
 import com.akiban.cserver.api.common.TableId;
 
 public final class NewScanRequest extends NewScanRange {
 
-    private final IndexId indexId;
+    private final int indexId;
 
-    public NewScanRequest(TableId tableId, IndexId indexId, Set<Integer> columns, Predicate predicate) {
+    public NewScanRequest(TableId tableId, int indexId, Set<Integer> columns, Predicate predicate) {
         super(tableId, columns, predicate);
         this.indexId = indexId;
     }
 
     public int getIndexIdInt(IdResolver resolver) {
-        return indexId.getIndexId(resolver);
+        return indexId;
     }
 }
