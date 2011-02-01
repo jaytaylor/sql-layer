9b9768f3b76201ba641c3a78a76376df22499170
Nathan Williams <nwilliams@akiban.com>
2011-02-01 14:21:46 -0500

    Update test for FooId removal

--- src/test/java/com/akiban/cserver/itests/bugs/bug687225/TruncateTableIT.java
+++ src/test/java/com/akiban/cserver/itests/bugs/bug687225/TruncateTableIT.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2011 Akiban Technologies Inc.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License, version 3,
@@ -15,64 +15,53 @@
 
 package com.akiban.cserver.itests.bugs.bug687225;
 
-import java.nio.ByteBuffer;
-import java.util.List;
-
 import com.akiban.cserver.InvalidOperationException;
-import com.akiban.cserver.RowData;
 import com.akiban.cserver.api.GenericInvalidOperationException;
-import com.akiban.cserver.api.common.TableId;
-import com.akiban.cserver.api.dml.scan.CursorId;
-import com.akiban.cserver.api.dml.scan.LegacyRowOutput;
-import com.akiban.cserver.api.dml.scan.LegacyScanRequest;
 import com.akiban.cserver.api.dml.scan.NewRow;
 import com.akiban.cserver.api.dml.scan.ScanAllRequest;
-import com.akiban.cserver.api.dml.scan.ScanRequest;
-import com.akiban.cserver.api.dml.scan.WrappingRowOutput;
 import com.akiban.cserver.itests.ApiTestBase;
-import com.akiban.cserver.message.ScanRowsRequest;
-import com.akiban.cserver.message.ScanRowsResponse;
 import com.akiban.cserver.store.RowCollector;
-
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public final class TruncateTableIT extends ApiTestBase {
     @Test
     public void basicTruncate() throws InvalidOperationException {
-        TableId tId = createTable("test", "t", "id int key");
+        int tableId = createTable("test", "t", "id int key");
 
-        expectRowCount(tId, 0);
+        expectRowCount(tableId, 0);
         final int rowCount = 5;
         for(int i = 1; i <= rowCount; ++i) {
-            dml().writeRow(session, createNewRow(tId, i));
+            dml().writeRow(session, createNewRow(tableId, i));
         }
-        expectRowCount(tId, rowCount);
+        expectRowCount(tableId, rowCount);
 
-        dml().truncateTable(session, tId);
+        dml().truncateTable(session, tableId);
 
         // Check that we can still get the rows
-        List<NewRow> rows = scanAll(new ScanAllRequest(tId, null));
+        List<NewRow> rows = scanAll(new ScanAllRequest(tableId, null));
         assertEquals("Rows scanned", 0, rows.size());
     }
     
     @Test(expected=GenericInvalidOperationException.class)
     public void bugTestCase() throws InvalidOperationException {
-        TableId tId = createTable("test", "t", "id int NOT NULL, pid int NOT NULL, PRIMARY KEY(id), UNIQUE KEY pid(pid)");
+        int tableId = createTable("test", "t", "id int NOT NULL, pid int NOT NULL, PRIMARY KEY(id), UNIQUE KEY pid(pid)");
 
-        expectRowCount(tId, 0);
-        dml().writeRow(session, createNewRow(tId, 1, 1));
-        dml().writeRow(session, createNewRow(tId, 2, 2));
-        expectRowCount(tId, 2);
+        expectRowCount(tableId, 0);
+        dml().writeRow(session, createNewRow(tableId, 1, 1));
+        dml().writeRow(session, createNewRow(tableId, 2, 2));
+        expectRowCount(tableId, 2);
 
-        dml().truncateTable(session, tId);
+        dml().truncateTable(session, tableId);
         
         int indexId = ddl().getAIS(session).getTable("test", "t").getIndex("pid").getIndexId();
         int scanFlags = RowCollector.SCAN_FLAGS_START_AT_EDGE | RowCollector.SCAN_FLAGS_END_AT_EDGE;
         
         // Exception currently thrown during dml.doScan: Corrupt RowData at {1,(long)1}
-        List<NewRow> rows = scanAll(new ScanAllRequest(tId, null, indexId, scanFlags));
+        List<NewRow> rows = scanAll(new ScanAllRequest(tableId, null, indexId, scanFlags));
         assertEquals("Rows scanned", 0, rows.size());
     }
 }
