feb9d4ab794f5389cbbf1eb75623c7301667b7ea
Nathan Williams <nwilliams@akiban.com>
2011-03-28 10:27:23 -0400

    Example schemas and data for social demos until other docs in place. Simple correctness test script for comparing MySQL based and Akiban based output.

---
+++ doc/examples/social/test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+case "$1" in
+    rails)
+        POST_MYSQL_URL="http://localhost:3000/home/post/"
+        POST_AKIBAN_URL="http://localhost:3000/home/postm/"
+        MYSQL_SCHEMA="social_rails_development"
+        AKIBAN_SCHEMA="social_rails_mdevelopment"
+        echo "Starting server"
+        ./rails/script/server &> /dev/null &
+        SERVER_PID="$!"
+        ;;
+    java|php)
+        echo "Not yet implemented"
+        exit 1
+        ;;
+    *)
+        echo "usage: $0 <rails|php|java>" 1>&2
+        exit 1
+        ;;
+esac
+
+echo "Create MySQL schema"
+mysql -u root -e "create schema $MYSQL_SCHEMA"
+echo "Create MySQL tables"
+mysql -u root $MYSQL_SCHEMA < schema.sql
+echo "Load MySQL data"
+mysql -u root $MYSQL_SCHEMA < data.sql
+
+echo "Create Akiban schema"
+mysql -u root -e "create schema $AKIBAN_SCHEMA"
+echo "Create Akiban tables"
+mysql -u root --init-command="set default_storage_engine=akibandb" $AKIBAN_SCHEMA < schema_grouped.sql
+echo "Load Akiban data"
+mysql -u root $AKIBAN_SCHEMA < data.sql
+
+FAILURE="no"
+
+echo "Compare post output "
+for i in `seq 1 100`; do
+    curl -s "$POST_MYSQL_URL""$i" > post_mysql.html
+    curl -s "$POST_AKIBAN_URL""$i" > post_akiban.html
+    diff post_mysql.html post_akiban.html > /dev/null
+    if [ $? -ne 0 ]; then
+        FAILURE="yes"
+        echo -n "$i(x) "
+    else
+        echo -n "$i "
+    fi
+    rm -f post_mysql.html post_akiban.html
+done
+echo ""
+echo "Done"
+
+echo "Stopping server"
+kill -9 "$SERVER_PID"
+sleep 1
+
+echo "Drop MySQL tables"
+mysql -u root -e "drop schema $MYSQL_SCHEMA"
+echo "Drop Akiban tables"
+mysql -u root $AKIBAN_SCHEMA -e "drop table post_votes,post_tags,post_links,comments,posts,user_friends,users"
+mysql -u root -e "drop schema $AKIBAN_SCHEMA"
+
+if [ "$FAILURE" = "yes" ]; then
+    echo "Failure" && exit 1
+else
+    echo "Success" && exit 0
+fi
