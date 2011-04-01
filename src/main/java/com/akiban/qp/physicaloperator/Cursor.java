ffefd8f05bb9c1b72479b4c4896995dcea3cf8b2
Jack Orenstein <jack@akiban.com>
2011-04-01 18:15:11 -0400

    Missed some files in my last checkin.

---
+++ src/main/java/com/akiban/qp/physicaloperator/Cursor.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.qp.physicaloperator;
+
+import com.akiban.qp.row.ManagedRow;
+
+public interface Cursor
+{
+    /**
+     * Starts a complete scan of the underlying table or index.
+     */
+    void open();
+
+    /**
+     * Advances to the next row of the underlying table or index.
+     *
+     * @return true if there is a next row, false otherwise.
+     */
+    boolean next();
+
+    /**
+     * Terminates the scan of the underlying table or index. Further calls to next() will return false.
+     */
+    void close();
+
+    /**
+     * The current row of the underlying table or index.
+     *
+     * @return The current row of the underlying table or index, or null if the scan has ended.
+     */
+    ManagedRow currentRow();
+}
---
+++ src/main/java/com/akiban/qp/physicaloperator/StoreAdapter.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.qp.physicaloperator;
+
+import com.akiban.ais.model.GroupTable;
+import com.akiban.ais.model.Index;
+import com.akiban.qp.rowtype.Schema;
+
+public abstract class StoreAdapter
+{
+    public abstract GroupCursor newGroupCursor(GroupTable groupTable);
+
+    public abstract IndexCursor newIndexCursor(Index index);
+
+    public final Schema schema()
+    {
+        return schema;
+    }
+
+    // For use by subclasses
+
+    protected StoreAdapter(Schema schema)
+    {
+        this.schema = schema;
+    }
+
+    // Object state
+
+    protected final Schema schema;
+}
