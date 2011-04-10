9ab893c86ad39503275f0d8876b9c385346bcc19
Nathan Williams <nwilliams@akiban.com>
2011-04-10 18:45:00 -0400

    Rewrite TimestampEncoder to treat it just as a 4 byte int for both input and output (i.e. no string date conversions). Add unit and Hapi test.

--- src/main/java/com/akiban/server/encoding/TimestampEncoder.java
+++ src/main/java/com/akiban/server/encoding/TimestampEncoder.java
@@ -15,99 +15,99 @@
 
 package com.akiban.server.encoding;
 
-import java.text.ParseException;
-import java.util.Date;
-
 import com.akiban.ais.model.Type;
 import com.akiban.server.FieldDef;
+import com.akiban.server.Quote;
 import com.akiban.server.RowData;
+import com.akiban.util.AkibanAppender;
 import com.persistit.Key;
 
-public final class TimestampEncoder extends EncodingBase<Date> {
-    TimestampEncoder() {
-    }
+/**
+ * Encoder for working with time when stored as a 4 byte int (standard
+ * UNIX timestamp). This is how MySQL stores the SQL TIMESTAMP type.
+ * See: http://dev.mysql.com/doc/refman/5.5/en/timestamp.html
+ * and  http://dev.mysql.com/doc/refman/5.5/en/storage-requirements.html
+ */
+public final class TimestampEncoder extends EncodingBase<Integer> {
+    static final int STORAGE_SIZE = 4;
 
-    @Override
-    public int fromObject(FieldDef fieldDef, Object value, byte[] dest,
-                          int offset) {
-        final long v;
-        if (value instanceof String) {
-            try {
-                final Date date = EncodingUtils.getDateFormat(SDF_DATETIME).parse(
-                        (String) value);
-                v = (int) (date.getTime() / 1000);
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-        } else if (value instanceof Date) {
-            final Date date = (Date) value;
-            v = (int) (date.getTime() / 1000);
-        } else if (value instanceof Long) {
-            v = ((Long) value).longValue();
-        } else {
-            throw new IllegalArgumentException(
-                    "Requires a String or a Date");
+    static int encodeFromObject(Object obj) {
+        int value = 0;
+        if(obj instanceof String) {
+            value = Integer.parseInt((String)obj);
+        } else if(obj instanceof Number) {
+            value = ((Number)obj).intValue();
+        } else if(obj != null) {
+            throw new IllegalArgumentException("Requires String or Number");
         }
-        return EncodingUtils.putUInt(dest, offset, v, 4);
+        return value;
     }
 
-    @Override
-    public Date toObject(FieldDef fieldDef, RowData rowData) throws EncodingException {
-        final long location = getLocation(fieldDef, rowData);
-        final long time = ((long) rowData.getIntegerValue(
-                (int) location, 4)) * 1000;
-        return new Date(time);
+    static String decodeToString(int value) {
+        return String.format("%d", value);
     }
+
+    static int fromRowData(RowData rowData, int location) {
+        final int shiftSize = 64 - STORAGE_SIZE * 8;
+        long v = rowData.getIntegerValue(location, STORAGE_SIZE);
+        v <<= shiftSize;
+        v >>= shiftSize;
+        return (int)v;
+    }
+    
 
     @Override
     public boolean validate(Type type) {
-        long w = type.maxSizeBytes();
-        return type.fixedSize() && w == 4;
+        return type.fixedSize() && (type.maxSizeBytes() == STORAGE_SIZE);
     }
 
     @Override
-    public int widthFromObject(final FieldDef fieldDef, final Object value) {
+    public Integer toObject(FieldDef fieldDef, RowData rowData) throws EncodingException {
+        final int location = (int)getLocation(fieldDef, rowData);
+        return fromRowData(rowData, location);
+    }
+
+    @Override
+    public int fromObject(FieldDef fieldDef, Object value, byte[] dest, int offset) {
+        assert fieldDef.getMaxStorageSize() == STORAGE_SIZE : fieldDef;
+        final int longValue = encodeFromObject(value);
+        return EncodingUtils.putInt(dest, offset, longValue, STORAGE_SIZE);
+    }
+
+    @Override
+    public int widthFromObject(FieldDef fieldDef, Object value) {
         return fieldDef.getMaxStorageSize();
     }
 
     @Override
     public void toKey(FieldDef fieldDef, RowData rowData, Key key) {
-        final long location = fieldDef.getRowDef().fieldLocation(rowData,
-                fieldDef.getFieldIndex());
-        if (location == 0) {
+        final int location = (int)getLocation(fieldDef, rowData);
+        if(location == 0) {
             key.append(null);
         } else {
-            long v = rowData.getIntegerValue((int) location,
-                    (int) (location >>> 32));
+            int v = fromRowData(rowData, location);
             key.append(v);
         }
     }
 
     @Override
     public void toKey(FieldDef fieldDef, Object value, Key key) {
-        if (value == null) {
+        assert fieldDef.getMaxStorageSize() == STORAGE_SIZE : fieldDef;
+        if(value == null) {
             key.append(null);
         } else {
-            key.append(dateTimeAsLong((Date) value));
+            int v = encodeFromObject(value);
+            key.append(v);
         }
     }
 
-    private long dateTimeAsLong(Date date) {
-        // This is NOT what the documentation says:
-        // http://dev.mysql.com/doc/refman/5.4/en/storage-requirements.html.
-        // This formula is based on Peter's reverse engineering of mysql
-        // packed data.
-        return ((date.getYear() + 1900) * LONG_1_E10)
-                + ((date.getMonth() + 1) * LONG_1_E8)
-                + (date.getDate() * LONG_1_E6)
-                + (date.getHours() * LONG_1_E4)
-                + (date.getMinutes() * LONG_100) + (date.getSeconds());
+    @Override
+    public void toString(FieldDef fieldDef, RowData rowData, AkibanAppender sb, Quote quote) {
+        try {
+            final int value = toObject(fieldDef, rowData);
+            sb.append(decodeToString(value));
+        } catch(EncodingException e) {
+            sb.append("null");
+        }
     }
-
-    final static String SDF_DATETIME = "yyyy-MM-dd HH:mm:ss";
-    private static final long LONG_100 = 100;
-    private static final long LONG_1_E4 = LONG_100 * 100;
-    private static final long LONG_1_E6 = LONG_1_E4 * 100;
-    private static final long LONG_1_E8 = LONG_1_E6 * 100;
-    private static final long LONG_1_E10 = LONG_1_E8 * 100;
 }
