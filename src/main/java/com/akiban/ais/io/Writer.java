31c47e926d659c18440ab464370775097d91884e
Nathan Williams <nwilliams@akiban.com>
2011-05-05 11:19:04 -0400

    Generalize Writer class slightly and add protected gettors to allow extending. Add abstract TableSubsetWriter that provides an interfacing for writing a portion of a given AIS by allowing individual tables to be included/skipped.
    Add AISTarget, implementing Target interface, that writes the map right back into another AIS. Provides an easy way to copy an existing AIS.

--- src/main/java/com/akiban/ais/io/Writer.java
+++ src/main/java/com/akiban/ais/io/Writer.java
@@ -31,101 +31,109 @@ import com.akiban.ais.model.Target;
 import com.akiban.ais.model.Type;
 import com.akiban.ais.model.UserTable;
 
-public class Writer implements ModelNames
+public class Writer
 {
-    private Target target;
-
-    public Writer(Target target)
-    {
+    public Writer(Target target) {
         this.target = target;
     }
-    
-    private void saveVersion (AkibanInformationSchema ais) throws Exception
-    {
-        target.writeVersion(ais.getModelVersion());
+
+    protected int getVersion(AkibanInformationSchema ais) throws Exception {
+        return ais.getModelVersion();
     }
 
-    private void saveTypes(AkibanInformationSchema ais) throws Exception
-    {
-        Collection<Type> types = ais.getTypes();
+    protected Collection<Type> getTypes(AkibanInformationSchema ais) throws Exception {
+        return ais.getTypes();
+    }
+
+    protected Collection<Group> getGroups(AkibanInformationSchema ais) throws Exception {
+        return ais.getGroups().values();
+    }
+
+    protected Collection<GroupTable> getGroupTables(AkibanInformationSchema ais) throws Exception {
+        return ais.getGroupTables().values();
+    }
+
+    protected Collection<UserTable> getUserTables(AkibanInformationSchema ais) throws Exception {
+        return ais.getUserTables().values();
+    }
+
+    protected Collection<Join> getJoins(AkibanInformationSchema ais) throws Exception {
+        return ais.getJoins().values();
+    }
+    
+    private void saveVersion(int version) throws Exception {
+        target.writeVersion(version);
+    }
+
+    private void saveTypes(Collection<Type> types) throws Exception {
         target.writeCount(types.size());
         for (Type type : types) {
             target.writeType(type.map());
         }
     }
 
-    private void saveGroups(AkibanInformationSchema ais) throws Exception
-    {
-        target.writeCount(ais.getGroups().size());
-        for (Group group : ais.getGroups().values()) {
+    private void saveGroups(Collection<Group> groups) throws Exception {
+        target.writeCount(groups.size());
+        for (Group group : groups) {
             target.writeGroup(group.map());
         }
     }
 
-    private void saveTables(AkibanInformationSchema ais) throws Exception
-    {
-        target.writeCount(ais.getGroupTables().size() + ais.getUserTables().size());
-        for (GroupTable groupTable : ais.getGroupTables().values()) {
+    private void saveTables(Collection<GroupTable> groupTables, Collection<UserTable> userTables) throws Exception {
+        target.writeCount(groupTables.size() + userTables.size());
+        for (GroupTable groupTable : groupTables) {
             target.writeTable(groupTable.map());
-            UserTable root = groupTable.getRoot();
-            assert root != null : groupTable;
+            assert groupTable.getRoot() != null : groupTable;
             nColumns += groupTable.getColumns().size();
             nIndexes += groupTable.getIndexes().size();
         }
-        for (UserTable userTable : ais.getUserTables().values()) {
+        for (UserTable userTable : userTables) {
             target.writeTable(userTable.map());
             nColumns += userTable.getColumnsIncludingInternal().size();
             nIndexes += userTable.getIndexesIncludingInternal().size();
         }
     }
 
-    private void saveColumns(AkibanInformationSchema ais) throws Exception
-    {
+    private void saveColumns(Collection<GroupTable> groupTables, Collection<UserTable> userTables) throws Exception {
         target.writeCount(nColumns);
-        for (GroupTable groupTable : ais.getGroupTables().values()) {
-            saveColumns(groupTable);
+        for (GroupTable groupTable : groupTables) {
+            for (Column column : groupTable.getColumns()) {
+                target.writeColumn(column.map());
+            }
         }
-        for (UserTable userTable : ais.getUserTables().values()) {
-            saveColumns(userTable);
-        }
-    }
-
-    private void saveColumns(Table table) throws Exception
-    {
-        for (Column column : table.getColumnsIncludingInternal()) {
-            target.writeColumn(column.map());
+        for (UserTable userTable : userTables) {
+            for (Column column : userTable.getColumnsIncludingInternal()) {
+                target.writeColumn(column.map());
+            }
         }
     }
 
-    private void saveJoins(AkibanInformationSchema ais) throws Exception
-    {
-        target.writeCount(ais.getJoins().size());
-        for (Join join : ais.getJoins().values()) {
+    private void saveJoins(Collection<Join> joins) throws Exception {
+        target.writeCount(joins.size());
+        for (Join join : joins) {
             target.writeJoin(join.map());
             nJoinColumns += join.getJoinColumns().size();
         }
     }
 
-    private void saveJoinColumns(AkibanInformationSchema ais) throws Exception
-    {
+    private void saveJoinColumns(Collection<Join> joins) throws Exception {
         target.writeCount(nJoinColumns);
-        for (Join join : ais.getJoins().values()) {
+        for (Join join : joins) {
             for (JoinColumn joinColumn : join.getJoinColumns()) {
                 target.writeJoinColumn(joinColumn.map());
             }
         }
     }
 
-    private void saveIndexes(AkibanInformationSchema ais) throws Exception
-    {
+    private void saveIndexes(Collection<GroupTable> groupTables, Collection<UserTable> userTables) throws Exception {
         target.writeCount(nIndexes);
-        for (UserTable userTable : ais.getUserTables().values()) {
+        for (UserTable userTable : userTables) {
             for (Index index : userTable.getIndexesIncludingInternal()) {
                 target.writeIndex(index.map());
                 nIndexColumns += index.getColumns().size();
             }
         }
-        for (GroupTable groupTable : ais.getGroupTables().values()) {
+        for (GroupTable groupTable : groupTables) {
             for (Index index : groupTable.getIndexes()) {
                 target.writeIndex(index.map());
                 nIndexColumns += index.getColumns().size();
@@ -133,17 +141,16 @@ public class Writer implements ModelNames
         }
     }
 
-    private void saveIndexColumns(AkibanInformationSchema ais) throws Exception
-    {
+    private void saveIndexColumns(Collection<GroupTable> groupTables, Collection<UserTable> userTables) throws Exception {
         target.writeCount(nIndexColumns);
-        for (UserTable userTable : ais.getUserTables().values()) {
+        for (UserTable userTable : userTables) {
             for (Index index : userTable.getIndexesIncludingInternal()) {
                 for (IndexColumn indexColumn : index.getColumns()) {
                     target.writeIndexColumn(indexColumn.map());
                 }
             }
         }
-        for (GroupTable groupTable : ais.getGroupTables().values()) {
+        for (GroupTable groupTable : groupTables) {
             for (Index index : groupTable.getIndexes()) {
                 for (IndexColumn indexColumn : index.getColumns()) {
                     target.writeIndexColumn(indexColumn.map());
@@ -152,29 +159,30 @@ public class Writer implements ModelNames
         }
     }
 
-    protected void close() throws Exception
-    {
-        target.close();
-    }
-
-    public void save(AkibanInformationSchema ais) throws Exception
-    {
+    public final void save(AkibanInformationSchema ais) throws Exception {
+        final int version = getVersion(ais);
+        final Collection<Type> types = getTypes(ais);
+        final Collection<Group> groups = getGroups(ais);
+        final Collection<GroupTable> groupTables = getGroupTables(ais);
+        final Collection<UserTable> userTables = getUserTables(ais);
+        final Collection<Join> joins = getJoins(ais);
         try {
             target.deleteAll();
-            saveVersion(ais);
-            saveTypes(ais);
-            saveGroups(ais);
-            saveTables(ais);
-            saveColumns(ais);
-            saveJoins(ais);
-            saveJoinColumns(ais);
-            saveIndexes(ais);
-            saveIndexColumns(ais);
+            saveVersion(version);
+            saveTypes(types);
+            saveGroups(groups);
+            saveTables(groupTables, userTables);
+            saveColumns(groupTables, userTables);
+            saveJoins(joins);
+            saveJoinColumns(joins);
+            saveIndexes(groupTables, userTables);
+            saveIndexColumns(groupTables, userTables);
         } finally {
-            close();
+            target.close();
         }
     }
 
+    private final Target target;
     private int nColumns = 0;
     private int nJoinColumns = 0;
     private int nIndexes = 0;
