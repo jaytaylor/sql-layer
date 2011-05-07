9ee26999b8eba99bc0d4217d91198bbc592a2840
Yuval Shavit <eleusinian@gmail.com>
2011-05-07 01:40:46 -0400

    Adding more javadocs and impls. Also adding a few more overloads.

--- src/main/java/com/akiban/ais/model/aisb2/NewAkibanJoinBuilder.java
+++ src/main/java/com/akiban/ais/model/aisb2/NewAkibanJoinBuilder.java
@@ -1,6 +1,22 @@
 package com.akiban.ais.model.aisb2;
 
 public interface NewAkibanJoinBuilder {
+    /**
+     * Adds a child -&gt; parent column pair to this join.
+     *
+     * <p>{@linkplain #and(String, String)} is a synonym of this method.</p>
+     * @param childColumn the name of the column on the child table
+     * @param parentColumn  the name of the column on the parent table
+     * @return this
+     */
     NewAkibanJoinBuilder on(String childColumn, String parentColumn);
+
+    /**
+     * Synonym for {@link #on(String, String)}. This method is just here to make the code more "English-sounding."
+     * Example: {@code build.joinTo("parent").on("child_col_1", "parent_col_1").and("child_col_2", "parent_col_2").}
+     * @param childColumn the name of the column on the child table
+     * @param parentColumn  the name of the column on the parent table
+     * @return this
+     */
     NewAkibanJoinBuilder and(String childColumn, String parentColumn);
 }
--- src/main/java/com/akiban/ais/model/aisb2/NewUserTableBuilder.java
+++ src/main/java/com/akiban/ais/model/aisb2/NewUserTableBuilder.java
@@ -16,10 +16,84 @@ public interface NewUserTableBuilder {
      */
     NewAkibanJoinBuilder joinTo(String schema, String table);
 
+    /**
+     * Joins this table to another one.
+     * @param schema the schema of the table to join to
+     * @param table the table name of the table to join to
+     * @param fkName the name of the Akiban FK, <em>without</em> the {@code __akiban} prefix.
+     * @return a builder that will create the new join
+     */
+    NewAkibanJoinBuilder joinTo(String schema, String table, String fkName);
+
+    /**
+     * Adds a non-nullable Long column
+     * @param name the column's name
+     * @return this
+     */
     NewUserTableBuilder colLong(String name);
+
+    /**
+     * Adds an optionally nullable Long column
+     * @param name the column's name
+     * @param nullable whether the column is nullable
+     * @return this
+     */
+    NewUserTableBuilder colLong(String name, boolean nullable);
+
+    /**
+     * Adds a non-nullable auto-incrementing column
+     * @param name the column's name
+     * @return this
+     */
+    NewUserTableBuilder autoIncLong(String name);
+
+    /**
+     * Adds a non-nullable varchar with UTF-8 encoding
+     * @param name the column's name
+     * @param length the varchar's max length
+     * @return this
+     */
     NewUserTableBuilder colString(String name, int length);
 
+    /**
+     * Adds an optionally nullable varchar with UTF-8 encoding
+     * @param name the column's name
+     * @param length the varchar's max length
+     * @param nullable whether the column is nullable
+     * @return this
+     */
+    NewUserTableBuilder colString(String name, int length, boolean nullable);
+
+    /**
+     * Adds an optionally nullable varchar with a specified encoding
+     * @param name the column's name
+     * @param length the varchar's max length
+     * @param nullable whether the column is nullable
+     * @param charset the column's encoding
+     * @return this
+     */
+    NewUserTableBuilder colString(String name, int length, boolean nullable, String charset);
+
+    /**
+     * Adds a PK
+     * @param columns the columns that are in the PK
+     * @return this
+     */
     NewUserTableBuilder pk(String... columns);
+
+    /**
+     * Adds a unique key
+     * @param indexName the key's name
+     * @param columns the columns in the key
+     * @return this
+     */
     NewUserTableBuilder uniqueKey(String indexName, String... columns);
+
+    /**
+     * Adds a non-unique key
+     * @param indexName the key's name
+     * @param columns the columns in the key
+     * @return this
+     */
     NewUserTableBuilder key(String indexName, String... columns);
 }
