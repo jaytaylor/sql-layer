5439412dd10643105eb84bc8d0847433f81e7b4c
Yuval Shavit <eleusinian@gmail.com>
2011-05-07 02:34:53 -0400

    Adding multi-column joins to the example

--- src/test/java/com/akiban/ais/model/aisb2/AISBBasedBuilderTest.java
+++ src/test/java/com/akiban/ais/model/aisb2/AISBBasedBuilderTest.java
@@ -14,8 +14,8 @@ public class AISBBasedBuilderTest {
         AkibanInformationSchema ais =
             builder.defaultSchema("sch")
             .userTable("customer").colLong("cid").colString("name", 32).pk("cid")
-            .userTable("order").colLong("oid").colLong("c_id").pk("oid").key("c_id", "c_id").joinTo("customer").on("c_id", "cid")
-            .userTable("item").colLong("iid").colLong("o_id").key("o_id", "o_id").joinTo("order").on("o_id", "oid")
+            .userTable("order").colLong("oid").colLong("c2").colLong("c_id").pk("oid", "c2").key("c_id", "c_id").joinTo("customer").on("c_id", "cid")
+            .userTable("item").colLong("iid").colLong("o_id").colLong("o_c2").key("o_id", "o_id", "o_c2").joinTo("order").on("o_id", "oid").and("o_c2", "c2")
             .userTable("address").colLong("aid").colLong("c_id").key("c_id", "c_id").joinTo("customer").on("c_id", "cid")
             .ais();
 
@@ -26,7 +26,7 @@ public class AISBBasedBuilderTest {
         // address goes first, since GroupsBuilder.fromAis goes in alphabetical order
         expectedGrouping.joinTables("sch", "customer", "sch", "address").column("cid", "c_id");
         expectedGrouping.joinTables("sch", "customer", "sch", "order").column("cid", "c_id");
-        expectedGrouping.joinTables("sch", "order", "sch", "item").column("oid", "o_id");
+        expectedGrouping.joinTables("sch", "order", "sch", "item").column("oid", "o_id").column("c2", "o_c2");
 
         assertEquals("grouping string", expectedGrouping.getGrouping().toString(), actualGrouping.toString());
 
