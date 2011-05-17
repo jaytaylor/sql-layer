6518b80d40140dddcfe23e63c70c2b615bc044c8
Nathan Williams <nwilliams@akiban.com>
2011-05-17 18:54:33 -0400

    Add new exception for NO_SUCH_GROUP error and launder it.

---
+++ src/main/java/com/akiban/server/api/common/NoSuchGroupException.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.server.api.common;
+
+import com.akiban.server.InvalidOperationException;
+import com.akiban.server.api.dml.DMLException;
+import com.akiban.message.ErrorCode;
+
+public final class NoSuchGroupException extends DMLException {
+    public NoSuchGroupException(InvalidOperationException e) {
+        super(e);
+    }
+
+    public NoSuchGroupException(String groupName) {
+        super(ErrorCode.NO_SUCH_GROUP, "No group with name %s", groupName);
+    }
+}
