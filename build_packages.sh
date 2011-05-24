ce9444f145d81e0a1a20315960773aea692a8b55
akiba <akiba@sleepy.akiban.local>
2011-05-24 11:41:08 -0400

    Fixing paths

--- build_packages.sh
+++ build_packages.sh
@@ -32,11 +32,11 @@ elif [ ${platform} == "redhat" ]; then
     tar_file=${PWD}/redhat/rpmbuild/SOURCES/akserver.tar
     bzr export --format=tar $tar_file
     cp packages-common/* ${PWD}/redhat/akserver
-    for to_add in $(bzr st -S | sed s/\?//); do
-        pushd akserver
+    pushd redhat
+    for to_add in $(bzr st -S . | sed 's/\?\s\+ redhat\///'); do
         tar --append -f $tar_file $to_add
-        popd
     done
+    popd
     gzip $tar_file
     cat ${PWD}/redhat/akiban-server.spec | sed "9,9s/REVISION/${bzr_revno}/g" > ${PWD}/redhat/akiban-server-${bzr_revno}.spec
     rpmbuild --target=noarch --define "_topdir ${PWD}/redhat/rpmbuild" -ba ${PWD}/redhat/akiban-server-${bzr_revno}.spec
