a84ca8e8a65db5c60044e659acd86a81f77c9b48
Yuval Shavit <yshavit@akiban.com>
2011-05-25 14:23:35 -0400

    Uncommenting check

--- src/main/java/com/akiban/qp/persistitadapter/OperatorStore.java
+++ src/main/java/com/akiban/qp/persistitadapter/OperatorStore.java
@@ -114,15 +114,14 @@ public final class OperatorStore extends DelegatingStore<PersistitStore> {
         if (result.rowsModified() == 0 || result.rowsTouched() == 0) {
             throw new NoSuchRowException(describeRow(oldRowData, rowDef));
         }
-        // TODO not yet implemented
-//        else if(result.rowsModified() != 1 || result.rowsTouched() != 1) {
-//            throw new RuntimeException(String.format(
-//                    "%s: %d touched, %d modified",
-//                    describeRow(oldRowData, rowDef),
-//                    result.rowsTouched(),
-//                    result.rowsModified()
-//            ));
-//        }
+        else if(result.rowsModified() != 1 || result.rowsTouched() != 1) {
+            throw new RuntimeException(String.format(
+                    "%s: %d touched, %d modified",
+                    describeRow(oldRowData, rowDef),
+                    result.rowsTouched(),
+                    result.rowsModified()
+            ));
+        }
     }
 
     private static String describeRow(RowData oldRowData, RowDef rowDef) {
