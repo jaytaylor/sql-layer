2cf657f1701ffbbc8f18303886b9a6ad3e3b2fcd
Jack Orenstein <jack@akiban.com>
2011-06-02 18:16:30 -0400

    - Got rid of unnecessary Schema argument to Cut and Extract.
    - Added ExtractIT.
    
    - Improved memory management in Cut and Extract: Advancing cursor before retrieving row reduces reference count, leads to fewer row allocations.
    
    - Minor optimizations in PersistitGroupRow.
    
    - Made AkibanCommandHandler tap threadsafe.
    
    - Added GroupScanProfileIT to be used in profiling.

---
+++ src/test/java/com/akiban/server/test/it/qp/GroupScanProfileIT.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.server.test.it.qp;
+
+import com.akiban.qp.physicaloperator.Bindings;
+import com.akiban.qp.physicaloperator.Cursor;
+import com.akiban.qp.physicaloperator.PhysicalOperator;
+import com.akiban.qp.physicaloperator.UndefBindings;
+import org.junit.Test;
+
+import static com.akiban.qp.physicaloperator.API.cursor;
+import static com.akiban.qp.physicaloperator.API.groupScan_Default;
+
+public class GroupScanProfileIT extends QPProfileITBase
+{
+    @Test
+    public void profileGroupScan()
+    {
+/* Needs to be an IT, but we only want it run manually (under a profiler). How can this be done?
+        final int SCANS = 1000;
+        final int CUSTOMERS = 1000;
+        final int ORDERS_PER_CUSTOMER = 5;
+        final int ITEMS_PER_ORDER = 2;
+        populateDB(CUSTOMERS, ORDERS_PER_CUSTOMER, ITEMS_PER_ORDER);
+        long start = System.nanoTime();
+        PhysicalOperator plan = groupScan_Default(coi);
+        for (int s = 0; s < SCANS; s++) {
+            Cursor cursor = cursor(plan, adapter);
+            cursor.open(NO_BINDINGS);
+            while (cursor.next()) {
+                cursor.currentRow();
+            }
+            cursor.close();
+        }
+        long end = System.nanoTime();
+        double sec = (end - start) / (1000.0 * 1000 * 1000);
+        System.out.println(String.format("scans: %s, db: %s/%s/%s, time: %s",
+                                         SCANS, CUSTOMERS, ORDERS_PER_CUSTOMER, ITEMS_PER_ORDER, sec));
+*/
+    }
+
+    private static final Bindings NO_BINDINGS = UndefBindings.only();
+}
