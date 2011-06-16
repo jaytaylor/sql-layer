62a16cfb7fcea29e1b08cfe3ed106e7a77579217
Mike McMahon <mmcm@comcast.net>
2011-06-16 18:03:06 -0400

    Need to cleanup any begun transaction on connection lost or persistit will hang.

--- src/main/java/com/akiban/sql/pg/PostgresSessionStatement.java
+++ src/main/java/com/akiban/sql/pg/PostgresSessionStatement.java
@@ -19,10 +19,6 @@ import com.akiban.sql.parser.StatementNode;
 
 import com.akiban.sql.StandardException;
 
-import com.persistit.Transaction;
-import com.persistit.exception.PersistitException;
-import com.persistit.exception.RollbackException;
-
 import java.io.IOException;
 
 /** SQL statements that affect session / environment state. */
@@ -82,70 +78,15 @@ public class PostgresSessionStatement implements PostgresStatement
             server.setDefaultSchemaName("...");
             break;
         case BEGIN_TRANSACTION:
-            beginTransaction(server);
+            server.beginTransaction();
             break;
         case COMMIT_TRANSACTION:
-            commitTransaction(server);
+            server.commitTransaction();
             break;
         case ROLLBACK_TRANSACTION:
-            rollbackTransaction(server);
+            server.rollbackTransaction();
             break;
         }
-    }
-
-    protected void beginTransaction(PostgresServerSession server) 
-            throws StandardException {
-        Transaction transaction = (Transaction)
-            server.getAttribute("transaction");
-        if (transaction != null)
-            throw new StandardException("A transaction is already in progress.");
-        transaction = server.getServiceManager()
-            .getTreeService().getTransaction(server.getSession());
-        try {
-            transaction.begin();
-        }
-        catch (PersistitException ex) {
-            throw new StandardException(ex);
-        }
-        server.setAttribute("transaction", transaction);
-    }
-
-    protected void commitTransaction(PostgresServerSession server) 
-            throws StandardException {
-        Transaction transaction = (Transaction)
-            server.getAttribute("transaction");
-        if (transaction == null)
-            throw new StandardException("No transaction is in progress.");
-        try {
-            transaction.commit();
-        }
-        catch (PersistitException ex) {
-            throw new StandardException(ex);
-        }
-        finally {
-            transaction.end();
-        }
-        server.setAttribute("transaction", null);
-    }
-
-    protected void rollbackTransaction(PostgresServerSession server) 
-            throws StandardException {
-        Transaction transaction = (Transaction)
-            server.getAttribute("transaction");
-        if (transaction == null)
-            throw new StandardException("No transaction is in progress.");
-        try {
-            transaction.rollback();
-        }
-        catch (PersistitException ex) {
-            throw new StandardException(ex);
-        }
-        catch (RollbackException ex) {
-        }
-        finally {
-            transaction.end();
-        }
-        server.setAttribute("transaction", null);
     }
 
 }
