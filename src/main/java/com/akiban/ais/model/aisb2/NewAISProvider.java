8daa96b272cb84afada3e8256a2e4718a2add817
Yuval Shavit <yshavit@akiban.com>
2011-06-17 17:55:37 -0400

    moving method to a new superclass

--- src/main/java/com/akiban/ais/model/aisb2/NewAISProvider.java
+++ src/main/java/com/akiban/ais/model/aisb2/NewAISProvider.java
@@ -17,26 +17,10 @@ package com.akiban.ais.model.aisb2;
 
 import com.akiban.ais.model.AkibanInformationSchema;
 
-public interface NewAISBuilder extends NewAISProvider {
+public interface NewAISProvider {
     /**
-     * Sets the default schema
-     * @param schema the new default schema name; like SQL's {@code USING}.
-     * @return {@code this}
+     * Gets the AIS that's been built.
+     * @return the AIS
      */
-    NewAISBuilder defaultSchema(String schema);
-
-    /**
-     * Starts creating a new table using the default schema.
-     * @param table the table's name
-     * @return the new table's builder
-     */
-    NewUserTableBuilder userTable(String table);
-
-    /**
-     * Starts creating a new table using the given schema
-     * @param schema the new table's schema
-     * @param table the new table's table name
-     * @return the new table's builder
-     */
-    NewUserTableBuilder userTable(String schema, String table);
+    AkibanInformationSchema ais();
 }
