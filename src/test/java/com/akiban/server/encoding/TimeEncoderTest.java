3d737dd7f19163a58138c322627189d89de30be7
Nathan Williams <nwilliams@akiban.com>
2011-06-30 18:35:11 -0400

    Fix TimeEncoder to use proper scales

--- src/test/java/com/akiban/server/encoding/TimeEncoderTest.java
+++ src/test/java/com/akiban/server/encoding/TimeEncoderTest.java
@@ -27,7 +27,7 @@ public class TimeEncoderTest extends LongEncoderTestBase {
                 new TestElement("00:00:01", 1),
                 new TestElement("-00:00:01", -1),
                 new TestElement("838:59:59", 8385959),
-                new TestElement("-838:59:59", 8385959),
+                new TestElement("-838:59:59", -8385959),
                 new TestElement("14:20:32", 142032),
                 new TestElement("-147:21:01", -1472101L)
               });
