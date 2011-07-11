9be82deeef17c470b97519d417e6043697d3d569
Peter Beaman <pbeaman@akiban.com>
2011-07-11 17:31:03 -0400

    transaction commit in PersistitStoreIndexManager passes store.forceToDisk

--- src/main/java/com/akiban/server/store/PersistitStoreIndexManager.java
+++ src/main/java/com/akiban/server/store/PersistitStoreIndexManager.java
@@ -150,7 +150,7 @@ public class PersistitStoreIndexManager implements IndexManager {
                     throw new RuntimeException(e);
                 }
                 treeService.getTableStatusCache().truncate(indexAnalysisRowDef.getRowDefId());
-                transaction.commit();
+                transaction.commit(store.forceToDisk);
                 break;
 
             } catch (RollbackException re) {
