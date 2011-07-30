2f1bf68b19fbea13c0dcdd141ad2f050729a59d9
Yuval Shavit <yshavit@akiban.com>
2011-07-30 04:12:36 -0400

    Converting the request messages to take a ServiceManager instance to their execute method. This is a slight improvement over SMI.get in that it at least lets us prove there are no circular dependencies. If there were any, the offending nodes would have to both go through SMI.get() directly for it to go uncaught; if they go through guice, we'll catch it.

--- src/main/java/com/akiban/server/service/ServiceManagerImpl.java
+++ src/main/java/com/akiban/server/service/ServiceManagerImpl.java
@@ -17,21 +17,10 @@ package com.akiban.server.service;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-import com.akiban.server.AkServer;
-import com.akiban.server.service.config.ConfigurationService;
-import com.akiban.server.service.dxl.DXLService;
-import com.akiban.server.service.instrumentation.InstrumentationService;
-import com.akiban.server.service.jmx.JmxRegistryService;
-import com.akiban.server.service.memcache.MemcacheService;
+import com.akiban.server.service.servicemanager.DelegatingServiceManager;
 import com.akiban.server.service.session.Session;
-import com.akiban.server.service.session.SessionService;
-import com.akiban.server.service.stats.StatisticsService;
-import com.akiban.server.service.tree.TreeService;
-import com.akiban.server.store.SchemaManager;
-import com.akiban.server.store.Store;
-import com.akiban.sql.pg.PostgresService;
 
-public final class ServiceManagerImpl implements ServiceManager
+public final class ServiceManagerImpl extends DelegatingServiceManager
 {
     private static final AtomicReference<ServiceManager> instance = new AtomicReference<ServiceManager>(null);
 
@@ -44,7 +33,7 @@ public final class ServiceManagerImpl implements ServiceManager
         }
     }
 
-    private ServiceManagerImpl() {}
+    public ServiceManagerImpl() {}
 
     /**
      * Gets the active ServiceManager; you can then use the returned instance to get any service you want.
@@ -64,92 +53,15 @@ public final class ServiceManagerImpl implements ServiceManager
         return installed().getSessionService().createSession();
     }
 
-    // ServiceManager interface
+    // DelegatingServiceManager override
+
 
     @Override
-    public void startServices() throws ServiceStartupException {
-        throw new UnsupportedOperationException("can't start services via the static delegate");
+    protected ServiceManager delegate() {
+        return installed();
     }
 
-    @Override
-    public void stopServices() throws Exception {
-        throw new UnsupportedOperationException("can't start services via the static delegate");
-    }
-
-    @Override
-    public void crashServices() throws Exception {
-        throw new UnsupportedOperationException("can't start services via the static delegate");
-    }
-
-    @Override
-    public ConfigurationService getConfigurationService() {
-        return installed().getConfigurationService();
-    }
-
-    @Override
-    public AkServer getAkSserver() {
-        return installed().getAkSserver();
-    }
-
-    @Override
-    public Store getStore() {
-        return installed().getStore();
-    }
-
-    @Override
-    public TreeService getTreeService() {
-        return installed().getTreeService();
-    }
-
-    @Override
-    public MemcacheService getMemcacheService() {
-        return installed().getMemcacheService();
-    }
-
-    @Override
-    public PostgresService getPostgresService() {
-        return installed().getPostgresService();
-    }
-
-    @Override
-    public SchemaManager getSchemaManager() {
-        return installed().getSchemaManager();
-    }
-
-    @Override
-    public JmxRegistryService getJmxRegistryService() {
-        return installed().getJmxRegistryService();
-    }
-
-    @Override
-    public StatisticsService getStatisticsService() {
-        return installed().getStatisticsService();
-    }
-
-    @Override
-    public SessionService getSessionService() {
-        return installed().getSessionService();
-    }
-
-    @Override
-    public <T> T getServiceByClass(Class<T> serviceClass) {
-        return installed().getServiceByClass(serviceClass);
-    }
-
-    @Override
-    public DXLService getDXL() {
-        return installed().getDXL();
-    }
-
-    @Override
-    public boolean serviceIsStarted(Class<?> serviceClass) {
-        return installed().serviceIsStarted(serviceClass);
-    }
-
-    @Override
-    public InstrumentationService getInstrumentationService() {
-        return installed().getInstrumentationService();
-    }
+    // for use by this class
 
     private static ServiceManager installed() {
         ServiceManager sm = instance.get();
