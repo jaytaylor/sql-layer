73efd655b27e8ef16017b92079de183766a26ceb
Thomas Jones-Low <tjoneslo@akiban.com>
2011-08-24 15:56:34 -0400

    Update PostgresDDLStatement to support more DDL statements. Add code to TableDDL for Rename Table (which doesn't work yet) Add code to IndexDDL for create index (which isn't tested yet)

--- src/main/java/com/akiban/sql/aisddl/IndexDDL.java
+++ src/main/java/com/akiban/sql/aisddl/IndexDDL.java
@@ -15,17 +15,28 @@
 
 package com.akiban.sql.aisddl;
 
-import com.akiban.ais.model.TableIndex;
-import com.akiban.server.error.NoSuchColumnException;
-import com.akiban.server.error.NoSuchTableException;
-import com.akiban.server.error.WrongTableForIndexException;
-import com.akiban.sql.parser.CreateIndexNode;
-import com.akiban.sql.parser.TableName;
+import java.util.Collection;
+import java.util.LinkedList;
 
+import com.akiban.server.api.DDLFunctions;
+import com.akiban.server.error.NoSuchColumnException;
+import com.akiban.server.error.NoSuchGroupException;
+import com.akiban.server.error.NoSuchTableException;
+import com.akiban.server.error.UnsupportedSQLException;
+import com.akiban.server.service.session.Session;
+import com.akiban.sql.parser.CreateIndexNode;
+import com.akiban.sql.parser.DropIndexNode;
+import com.akiban.sql.parser.IndexColumn;
+import com.akiban.sql.parser.RenameNode;
+
+import com.akiban.ais.io.AISTarget;
+import com.akiban.ais.io.TableSubsetWriter;
+import com.akiban.ais.model.AISBuilder;
 import com.akiban.ais.model.AkibanInformationSchema;
 import com.akiban.ais.model.Column;
-import com.akiban.ais.model.UserTable;
 import com.akiban.ais.model.Index;
+import com.akiban.ais.model.Table;
+import com.akiban.ais.model.TableName;
 
 /** DDL operations on Indices */
 public class IndexDDL
@@ -33,77 +44,144 @@ public class IndexDDL
     private IndexDDL() {
     }
 
-    public static void createIndex(AkibanInformationSchema ais,
+    public static void dropIndex (DDLFunctions ddlFunctions,
+                                    Session session,
+                                    String defaultSchemaName,
+                                    DropIndexNode dropIndex) {
+        
+        throw new UnsupportedSQLException (dropIndex.statementToString(), dropIndex);
+        //ddlFunctions.dropTableIndexes(session, tableName, indexesToDrop)
+    }
+    
+    public static void renameIndex (DDLFunctions ddlFunctions,
+                                    Session session,
+                                    String defaultSchemaName, 
+                                    RenameNode renameIndex) {
+        throw new UnsupportedSQLException (renameIndex.statementToString(), renameIndex); 
+    }
+    
+    public static void createIndex(DDLFunctions ddlFunctions,
+                                    Session session,
                                    String defaultSchemaName,
                                    CreateIndexNode createIndex)  {
-        TableName indexName = createIndex.getObjectName();
-        TableName tableName = createIndex.getIndexTableName();
-        String schemaName = tableName.getSchemaName();
-        if (schemaName == null)
-            schemaName = defaultSchemaName;
-        UserTable table = ais.getUserTable(schemaName, tableName.getTableName());
-        if (table == null)
-            throw new NoSuchTableException (tableName.getSchemaName(), tableName.getTableName());
-        // TODO: What about indexName schemaName?
-        Index index = new TableIndex(table,
-                                // TODO: Any case issues?
-                                indexName.getTableName(),
-                                -1, 
-                                createIndex.getUniqueness(), 
-                                null);
+        AkibanInformationSchema ais = ddlFunctions.getAIS(session);
+        
+        Collection<Index> indexesToAdd = new LinkedList<Index>();
 
-        // TODO: Not at all clear that the following is right.
-
-        int pos = 0;
-        for (com.akiban.sql.parser.IndexColumn cindexColumn : 
-                 createIndex.getColumnList()) {
-            com.akiban.ais.model.IndexColumn aindexColumn = 
-                new com.akiban.ais.model.IndexColumn(index,
-                                                     getColumn(ais, 
-                                                               defaultSchemaName,
-                                                               table,
-                                                               cindexColumn.getTableName(),
-                                                               cindexColumn.getColumnName()),
-                                                     pos++,
-                                                     cindexColumn.isAscending(),
-                                                     null);
-            index.getColumns().add(aindexColumn);
-        }
-
-        // TODO: Now what? table.addIndex(index)? Call something on
-        // ais? Or did I need a session, which would make this
-        // non-static?
+        indexesToAdd.add(buildIndex(ais, defaultSchemaName, createIndex));
+        
+        ddlFunctions.createIndexes(session, indexesToAdd);
     }
-
-    protected static Column getColumn(AkibanInformationSchema ais, 
-                                      String defaultSchemaName,
-                                      UserTable defaultTable, 
-                                      TableName tableName, String columnName) {
-        UserTable table = defaultTable;
-        if (tableName != null) {
-            String schemaName = tableName.getSchemaName();
-            if (schemaName == null)
-                schemaName = defaultSchemaName;
-            table = ais.getUserTable(schemaName, tableName.getTableName());
-            if (table == null)
-                throw new NoSuchTableException (tableName.getSchemaName(), tableName.getTableName());
-        }
-        if (!isAccessible(table, defaultTable))
-            throw new WrongTableForIndexException (defaultTable.getName());
-        Column column = table.getColumn(columnName);
-        if (column == null)
-            throw new NoSuchColumnException (columnName);
-        return column;
-    }
-
-    protected static boolean isAccessible(UserTable table, UserTable fromTable) {
-        while (true) {
-            if (table == fromTable)
-                return true;
-            fromTable = fromTable.parentTable();
-            if (fromTable == null)
-                return false;
+    
+    private static Index buildIndex (AkibanInformationSchema ais, String defaultSchemaName, CreateIndexNode index) {
+        final String schemaName = index.getObjectName().getSchemaName() != null ? index.getObjectName().getSchemaName() : defaultSchemaName;
+        final TableName tableName = TableName.create(schemaName, index.getIndexName().getTableName());
+        
+        if (checkIndexType (index, tableName) == Index.IndexType.TABLE) {
+            return buildTableIndex (ais, tableName, index);
+        } else {
+            return buildGroupIndex (ais, tableName, index);
         }
     }
+    
+    private static Index.IndexType checkIndexType(CreateIndexNode index, TableName tableName) {
+        for (IndexColumn col : index.getColumnList()) {
+            if (col.getTableName() != null && 
+                    !(col.getTableName().getSchemaName().equalsIgnoreCase(tableName.getSchemaName()) &&
+                      col.getTableName().getTableName().equalsIgnoreCase(tableName.getTableName()))) {
+                return Index.IndexType.GROUP;
+            }
+        }
+        return Index.IndexType.TABLE;
+    }
+    
+    private static Index buildTableIndex (AkibanInformationSchema ais, TableName tableName, CreateIndexNode index) {
+        final String indexName = index.getObjectName().getTableName();
 
+        if (ais.getUserTable(tableName) == null) {
+            throw new NoSuchTableException (tableName);
+        }
+
+        AISBuilder builder = new AISBuilder();
+        addTable (builder, ais, tableName);
+        
+        builder.index(tableName.getSchemaName(), tableName.getTableName(), indexName, index.getUniqueness(),
+                index.getUniqueness() ? Index.UNIQUE_KEY_CONSTRAINT : Index.KEY_CONSTRAINT);
+
+        int i = 0;
+        for (IndexColumn col : index.getColumnList()) {
+            Column tableCol = ais.getTable(tableName).getColumn(col.getColumnName());
+            if (tableCol == null) {
+                throw new NoSuchColumnException (col.getColumnName());
+            }          
+            builder.indexColumn(tableName.getSchemaName(), tableName.getTableName(), indexName, tableCol.getName(), i, col.isAscending(), 0);
+            i++;
+        }
+        builder.basicSchemaIsComplete();
+        
+        return builder.akibanInformationSchema().getTable(tableName).getIndex(indexName);
+    }
+    
+    
+    private static Index buildGroupIndex (AkibanInformationSchema ais, TableName tableName, CreateIndexNode index) {
+        final String indexName = index.getObjectName().getTableName();
+        
+        if (ais.getUserTable(tableName) == null) {
+            throw new NoSuchTableException (tableName);
+        }
+        
+        final String groupName = ais.getUserTable(tableName).getGroup().getName();
+        
+        if (ais.getGroup(groupName) == null) {
+            throw new NoSuchGroupException(groupName);
+        }
+
+        AISBuilder builder = new AISBuilder();
+        addGroup(builder, ais, groupName);
+        builder.groupIndex(groupName, indexName, index.getUniqueness());
+        
+        int i = 0;
+        String schemaName;
+        TableName columnTable;
+        for (IndexColumn col : index.getColumnList()) {
+            if (col.getTableName() != null) {
+                schemaName = col.getTableName().hasSchema() ? col.getTableName().getSchemaName() : tableName.getSchemaName();
+                columnTable = TableName.create(schemaName, col.getTableName().getTableName());
+            } else {
+                columnTable = tableName;
+                schemaName = tableName.getSchemaName();
+            }
+
+            final String columnName = col.getColumnName(); 
+
+            Column tableCol = ais.getUserTable(columnTable).getColumn(columnName); 
+            if (tableCol == null) {
+                throw new NoSuchColumnException (col.getColumnName());
+            }
+            
+            builder.groupIndexColumn(groupName, indexName, schemaName, columnTable.getTableName(), columnName, i);
+            i++;
+        }
+        builder.basicSchemaIsComplete();
+        return builder.akibanInformationSchema().getGroup(groupName).getIndex(indexName);
+    }
+
+    private static void addGroup (AISBuilder builder, AkibanInformationSchema ais, final String groupName) {
+
+        new TableSubsetWriter(new AISTarget(builder.akibanInformationSchema())) {
+            @Override
+            public boolean shouldSaveTable(Table table) {
+                return table.getGroup().getName().equalsIgnoreCase(groupName);
+            }
+        }.save(ais);
+    }
+    
+    private static void addTable (AISBuilder builder, AkibanInformationSchema ais, final TableName tableName) {
+        new TableSubsetWriter(new AISTarget(builder.akibanInformationSchema())) {
+            @Override
+            public boolean shouldSaveTable(Table table) {
+                return table.getName().equals(tableName);
+            }
+        }.save(ais);
+    }
 }
