a8f6a6cafe1da1a6cba4760e7f44a7637068dc69
Mike McMahon <mmcm@comcast.net>
2011-09-05 17:17:08 -0400

    Base test framework for duplication.

---
+++ src/test/java/com/akiban/sql/optimizer/plan/DuplicatePlanTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.sql.optimizer.plan;
+
+import com.akiban.sql.optimizer.OptimizerTestBase;
+import com.akiban.sql.optimizer.rule.ASTToStatement;
+
+import com.akiban.sql.parser.DMLStatementNode;
+import com.akiban.sql.parser.StatementNode;
+
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.*;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.ArrayList;
+
+@RunWith(Parameterized.class)
+public class DuplicatePlanTest extends OptimizerTestBase
+{
+    public static final File RESOURCE_DIR = 
+        new File(OptimizerTestBase.RESOURCE_DIR, "plan/duplicate");
+
+    @Parameters
+    public static Collection<Object[]> statements() throws Exception {
+        Collection<Object[]> result = new ArrayList<Object[]>();
+        for (File sqlFile : listSQLFiles(RESOURCE_DIR)) {
+            String caseName = sqlFile.getName().replace(".sql", "");
+            String sql = fileContents(sqlFile);
+            result.add(new Object[] {
+                           caseName, sql
+                       });
+        }
+        return result;
+    }
+
+    public DuplicatePlanTest(String caseName, String sql) {
+        super(caseName, sql, null);
+    }
+
+    @Before
+    public void loadDDL() throws Exception {
+        loadSchema(new File(RESOURCE_DIR, "schema.ddl"));
+    }
+
+    @Test
+    public void testDuplicatePlan() throws Exception {
+        StatementNode stmt = parser.parseStatement(sql);
+        binder.bind(stmt);
+        stmt = booleanNormalizer.normalize(stmt);
+        typeComputer.compute(stmt);
+        stmt = subqueryFlattener.flatten((DMLStatementNode)stmt);
+        // Turn parsed AST into intermediate form.
+        PlanNode plan = new AST((DMLStatementNode)stmt);
+        plan = new ASTToStatement().apply(plan);
+        PlanNode duplicate = (PlanNode)plan.duplicate();
+        assertFalse(plan == duplicate);
+        assertEqualsWithoutHashes(caseName, 
+                                  PlanToString.of(plan), 
+                                  PlanToString.of(duplicate));
+    }
+
+}
