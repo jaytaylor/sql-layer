c56a789968557de25c814886ab28af076851b8ff
Mike McMahon <mmcm@comcast.net>
2011-09-10 17:38:54 -0400

    Skeleton of unit tests.

---
+++ src/main/java/com/akiban/sql/optimizer/plan/TableGroup.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.sql.optimizer.plan;
+
+import com.akiban.ais.model.Group;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/** A set of tables with common group joins.
+ */
+public class TableGroup
+{
+    private Group group;
+    private List<TableSource> tables;
+
+    public TableGroup(Group group) {
+        this.group = group;
+        tables = new ArrayList<TableSource>();
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public List<TableSource> getTables() {
+        return tables;
+    }
+
+    public void addTable(TableSource use) {
+        tables.add(use);
+    }
+
+}
