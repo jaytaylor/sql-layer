f6b90cf8de9b4454300d09f57225fb773311f751
Mike McMahon <mmcm@comcast.net>
2011-09-13 18:58:40 -0400

    Checkpoint.

--- src/main/java/com/akiban/sql/optimizer/plan/IndexUsage.java
+++ src/main/java/com/akiban/sql/optimizer/plan/IndexUsage.java
@@ -24,25 +24,79 @@ import java.util.*;
 public class IndexUsage extends BaseDuplicatable
 {
     private Index index;
-    private List<ConditionExpression> equalityConditions;
-    private ConditionExpression lowCondition, highCondition;
-    private List<OrderByExpression> ordering;
 
-    // TODO: how successful: ordering, grouping, partial grouping, none
-    // hkey depth returned?
+    // Conditions subsumed by this index.
+    // TODO: any cases where a condition is only partially handled and
+    // still needs to be checked with Select?
+    private List<ConditionExpression> conditions;
+
+    // First equalities in the order of the index.
+    private List<ExpressionNode> equalityComparands;
+    // Followed by an optional inequality.
+    private ExpressionNode lowComparand, highComparand;
+    // TODO: This doesn't work for merging: consider x < ? AND x <= ?. 
+    // May need building of index keys in the expressions subsystem.
+    private boolean lowInclusive, highInclusive;
+
+    // This is how the indexed result will be ordered from using this index.
+    // TODO: Is this right? Are we allowed to switch directions between segments?
+    private List<OrderByExpression> ordering;
 
     public IndexUsage(Index index) {
         this.index = index;
     }
 
+    public Index getIndex() {
+        return index;
+    }
+
+    public List<ConditionExpression> getConditions() {
+        return conditions;
+    }
+
+    public List<ExpressionNode> getEqualityComparands() {
+        return equalityComparands;
+    }
+    public ExpressionNode getLowComparand() {
+        return lowComparand;
+    }
+    publc boolean isLowInclusive() {
+        return lowInclusive;
+    }
+    public ExpressionNode getHighComparand() {
+        return highComparand;
+    }
+    publc boolean isHighInclusive() {
+        return highInclusive;
+    }
+
+    public void setConditions(List<ConditionExpression> conditions,
+                              List<ExpressionNode> equalityComparands,
+                              ExpressionNode lowComparand, boolean lowInclusive,
+                              ExpressionNode highComparand, boolean highInclusive) {
+        this.conditions = conditions;
+        this.equalityComparands = equalityComparands;
+        this.lowComparand = lowComparand;
+        this.lowInclusive = lowInclusive;
+        this.highComparand = highComparand;
+        this.highInclusive = highInclusive;
+    }
+
+    public List<OrderByExpression> getOrdering() {
+        return ordering;
+    }
+    public void setOrdering(List<OrderByExpression> ordering) {
+        this.ordering = ordering;
+    }
+                              
     @Override
     protected void deepCopy(DuplicateMap map) {
         super.deepCopy(map);
-        equalityConditions = duplicateList(equalityConditions, map);
-        if (lowCondition != null)
-            lowCondition = (ConditionExpression)lowCondition.duplicate(map);
-        if (highCondition != null)
-            highCondition = (ConditionExpression)highCondition.duplicate(map);
+        equalityComparands = duplicateList(equalityComparands, map);
+        if (lowComparand != null)
+            lowComparand = (ConditionExpression)lowComparand.duplicate(map);
+        if (highComparand != null)
+            highComparand = (ConditionExpression)highComparand.duplicate(map);
         ordering = duplicateList(ordering, map);
     }
     
--- src/main/java/com/akiban/sql/optimizer/rule/IndexGoal.java
+++ src/main/java/com/akiban/sql/optimizer/rule/IndexGoal.java
@@ -19,16 +19,26 @@ import com.akiban.sql.optimizer.plan.*;
 
 import com.akiban.sql.optimizer.plan.Sort.OrderByExpression;
 
+import com.akiban.ais.model.Column;
+import com.akiban.ais.model.GroupIndex;
+import com.akiban.ais.model.Index;
+import com.akiban.ais.model.IndexColumn;
+import com.akiban.ais.model.Table;
+
 import java.util.*;
 
 public class IndexGoal
 {
+    // Tables already bound outside.
+    private Set<TableSource> boundTables;
+
     // The group we want to index.
     private TableGroup group;
     // All the tables that are joined together here, including outer
     // join ones that can't have indexes, but might have sorting or
     // covering.
     private List<TableSource> tables;
+
     // All the conditions that might be indexable.
     private List<ConditionExpression> conditions;
 
@@ -40,4 +50,97 @@ public class IndexGoal
 
     public IndexGoal(TableGroup group) {
     }
+
+    public static enum OrderEffectiveness {
+        NONE, PARTIAL_GROUP, GROUP, SORT
+    }
+    
+    /** Populate given index usage according to goal.
+     * Return <code>null</code> if useless, otherwise {@link OrderEffectiveness} 
+     * for how well it orders.
+     */
+    public Effectiveness usable(IndexUsage index) {
+        Index aisIndex = index.getIndex();
+        if (aisIndex.isGroupIndex()) {
+            // The leaf must be used or else we'll get duplicates from a
+            // scan (the indexed columns need not be root to leaf, making
+            // ancestors discontiguous and duplicates hard to eliminate).
+            if (!contains(tables, index.leafMostTable()))
+                return null;
+            // The root must be present, since the index does not contain
+            // orphans.
+            if (!contains(tables, index.rootMostTable()))
+                return null;
+        }        
+        List<IndexColumn> indexColumns = aisIndex.getColumns();
+        int ncols = indexColumns.size();
+        int nequals = 0;
+        while (nequals < ncols) {
+            IndexColumn indexColumn = indexColumns.get(nequals);
+            ExpressionNode indexExpression = getIndexExpression(indexColumn);
+            ConditionExpression equalityCondition = null;
+            ExpressionNode otherComparand = null;
+            for (ConditionExpression condition : conditions) {
+                if (condition instanceof ComparisonCondition) {
+                    ComparisonCondition ccond = (ComparisonCondition)condition;
+                    if (ccond.getOperation() == Comparison.EQ) {
+                        if (indexExpression.equals(ccond.getLeft())) {
+                            otherComparand = ccond.getRight();
+                            equalityCondition = condition;
+                            break;
+                        }
+                        else if (indexExpression.equals(ccond.getRight())) {
+                            otherComparand = ccond.getLeft();
+                            equalityCondition = condition;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (equalityCondition == null)
+                break;
+            index.addEqualityCondition(equalityCondition, otherComparand);
+            nequals++;
+        }
+        if (nequals < ncols) {
+            IndexColumn indexColumn = indexColumns.get(nequals);
+            ExpressionNode indexExpression = getIndexExpression(indexColumn);
+            for (ConditionExpression condition : conditions) {
+                if (condition instanceof ComparisonCondition) {
+                    ComparisonCondition ccond = (ComparisonCondition)condition;
+                    ExpressionNode otherComparand = null;
+                    if (indexExpression.equals(ccond.getLeft())) {
+                        otherComparand = ccond.getRight();
+                    }
+                    else if (indexExpression.equals(ccond.getRight())) {
+                        otherComparand = ccond.getLeft();
+                    }
+                    if (otherComparand != null) {
+                        index.addInequalityCondition(condition,
+                                                     ccond.getOperation(),
+                                                     otherComparand);
+                    }
+                }
+            }
+            List<OrderByExpression> ordering = 
+                new ArrayList<OrderByExpression(ncols - nequals);
+            for (int i = nequals; i < ncols; i++) {
+                IndexColumn indexColumn = indexColumns.get(i);
+                ExpressionNode indexExpression = getIndexExpression(indexColumn);
+                OrderByExpression order = 
+                    new OrderByExpression(indexExpresion, indexColumn.isAscending());
+            }
+            // TODO: Check whether better to reverse the sort.
+        }
+    }
+
+    protected static boolean contains(Collection<TableSource> tables,
+                                      Table aisTable) {
+        for (TableSource table : tables) {
+            if (table.getTable().getTable() == aisTable)
+                return true;
+        }
+        return false;
+    }
+
 }
