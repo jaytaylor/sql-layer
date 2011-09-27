4c10dbd9e4d759d6afc94b506c5c0d831828afb2
Mike McMahon <mmcm@comcast.net>
2011-09-27 19:31:55 -0400

    Copy over all the old unit tests.

--- src/test/resources/com/akiban/sql/optimizer/operator-new/coia-group-index/select-1.sql
+++ src/test/resources/com/akiban/sql/optimizer/operator-new/coia-group-index/select-1.sql
@@ -2,6 +2,5 @@ SELECT customers.name,order_date,sku,quan
 FROM customers,orders,items
 WHERE customers.cid = orders.cid
 AND orders.oid = items.oid
-AND orders.order_date > '2011-01-01'
+AND customers.name = 'Smith'
 AND items.sku = '1234'
-ORDER BY order_date
