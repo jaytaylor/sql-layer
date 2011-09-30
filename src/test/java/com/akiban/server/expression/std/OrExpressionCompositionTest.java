16f0df3385c1d7acc1f8ba2b2499a4b8d6705a49
Yuval Shavit <eleusinian@gmail.com>
2011-09-30 01:49:38 -0400

    Adding OrExpression

---
+++ src/test/java/com/akiban/server/expression/std/OrExpressionCompositionTest.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+package com.akiban.server.expression.std;
+
+import com.akiban.server.expression.Expression;
+
+import java.util.List;
+
+public final class OrExpressionCompositionTest extends ComposedExpressionTestBase {
+    @Override
+    protected int childrenCount() {
+        return 2;
+    }
+
+    @Override
+    protected Expression getExpression(List<? extends Expression> children) {
+        return new OrExpression(children);
+    }
+}
