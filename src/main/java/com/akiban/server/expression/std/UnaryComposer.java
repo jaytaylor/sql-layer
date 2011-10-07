7005989a90d4fbf4fbfc7a0cfad1daf71952f7e3
Yuval Shavit <yshavit@akiban.com>
2011-10-07 15:40:46 -0400

    cleaning up an import

--- src/main/java/com/akiban/server/expression/std/UnaryComposer.java
+++ src/main/java/com/akiban/server/expression/std/UnaryComposer.java
@@ -18,7 +18,6 @@ package com.akiban.server.expression.std;
 import com.akiban.server.error.WrongExpressionArityException;
 import com.akiban.server.expression.Expression;
 import com.akiban.server.expression.ExpressionComposer;
-import com.akiban.util.ArgumentValidation;
 
 import java.util.List;
 
