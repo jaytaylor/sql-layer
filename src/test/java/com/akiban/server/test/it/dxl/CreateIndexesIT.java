e16f600d312ebdb1d3c46af1e671fad3e1761207
Yuval Shavit <yshavit@akiban.com>
2011-10-27 19:49:10 -0400

    cleanup

--- src/test/java/com/akiban/server/test/it/dxl/CreateIndexesIT.java
+++ src/test/java/com/akiban/server/test/it/dxl/CreateIndexesIT.java
@@ -60,11 +60,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 public final class CreateIndexesIT extends ITBase {
 
