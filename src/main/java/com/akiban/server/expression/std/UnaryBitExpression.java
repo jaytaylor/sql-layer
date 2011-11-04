e53a4badee73e70e00528fc06ec679cf2ea4fcfe
Vy Nguyen <oontvoo@hotmail.com>
2011-11-04 17:12:24 -0400

    - add UnaryBitExpression : bitwise NOT, BIT_COUNT

---
+++ src/main/java/com/akiban/server/expression/std/UnaryBitExpression.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.error.InconvertibleTypesException;
+import com.akiban.server.expression.Expression;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.expression.ExpressionEvaluation;
+import com.akiban.server.service.functions.Scalar;
+import com.akiban.server.types.AkType;
+import com.akiban.server.types.NullValueSource;
+import com.akiban.server.types.ValueSource;
+import com.akiban.server.types.extract.Extractors;
+import com.akiban.server.types.util.ValueHolder;
+import java.math.BigInteger;
+
+public class UnaryBitExpression extends AbstractUnaryExpression
+{
+    private static interface UnaryBitOp
+    {
+        ValueSource exc (BigInteger arg);
+        ValueSource errorCase ();
+    }
+    
+    public static enum UnaryBitOperator implements UnaryBitOp
+    {
+        NOT
+        {
+            @Override
+            public ValueSource exc (BigInteger arg)
+            {
+                return new ValueHolder(AkType.U_BIGINT, arg.not());
+            }
+            
+            @Override
+            public ValueSource errorCase ()
+            {
+                return new ValueHolder(AkType.U_BIGINT,  
+                        BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.valueOf(2L)));
+            }
+        },
+        
+        COUNT
+        {
+            @Override
+            public ValueSource exc (BigInteger arg)
+            {
+                return new ValueHolder(AkType.LONG, (long)arg.bitCount());
+            }
+            
+            @Override
+            public ValueSource errorCase ()
+            {
+                return new ValueHolder(AkType.LONG, 0L);
+            }
+        }
+    }
+    
+    @Scalar("~")
+    public static final ExpressionComposer NOT_COMPOSER = new InternalComposer(UnaryBitOperator.NOT);
+    
+    @Scalar("bit_count")
+    public static final ExpressionComposer BIT_COUNT_COMPOSER = new InternalComposer(UnaryBitOperator.COUNT);
+    
+    private static class InternalComposer extends UnaryComposer
+    {
+        private final UnaryBitOperator op;
+        public InternalComposer (UnaryBitOperator op)
+        {
+            this.op = op;
+        }
+
+        @Override
+        protected Expression compose(Expression argument) 
+        {
+            return new UnaryBitExpression(argument, op);
+        }
+    }
+            
+    private static class InnerEvaluation extends AbstractUnaryExpressionEvaluation
+    {
+        private final UnaryBitOperator op;
+        
+        public InnerEvaluation (ExpressionEvaluation ev, UnaryBitOperator op)
+        {
+            super(ev);
+            this.op = op;
+        }
+
+        @Override
+        public ValueSource eval() 
+        {
+            ValueSource source;
+            if ((source = this.operand()).isNull()) return NullValueSource.only();
+            
+            BigInteger arg;
+            try
+            {
+              arg = Extractors.getUBigIntExtractor().getObject(source);
+            }
+            catch (InconvertibleTypesException ex) 
+            { 
+                return op.errorCase();
+            } 
+            catch (NumberFormatException ex)
+            {
+                return op.errorCase();
+            }
+           
+            return op.exc(arg);
+            
+        }
+        
+    }
+    
+    private final UnaryBitOperator op;
+    
+    public UnaryBitExpression (Expression ex, UnaryBitOperator op)
+    {
+        super(op == UnaryBitOperator.COUNT ? AkType.LONG : AkType.U_BIGINT, ex);
+        this.op = op;
+    }
+
+    @Override
+    protected String name()
+    {
+        return op.name();
+    }
+
+    @Override
+    public ExpressionEvaluation evaluation() 
+    {
+        return new InnerEvaluation(operandEvaluation(), op);
+    }
+    
+}
---
+++ src/test/java/com/akiban/server/expression/std/UnaryBitExpressionTest.java
@@ -0,0 +1,172 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.expression.std.UnaryBitExpression.UnaryBitOperator;
+import org.junit.runner.RunWith;
+import com.akiban.server.types.util.ValueHolder;
+import com.akiban.junit.ParameterizationBuilder;
+import com.akiban.junit.Parameterization;
+import java.util.Collection;
+import com.akiban.junit.NamedParameterizedRunner;
+import com.akiban.server.types.ValueSource;
+import java.math.BigInteger;
+import com.akiban.server.expression.Expression;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.types.AkType;
+
+import com.akiban.server.types.NullValueSource;
+import java.util.Arrays;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(NamedParameterizedRunner.class)
+public class UnaryBitExpressionTest extends ComposedExpressionTestBase
+{
+    static interface Functors
+    {
+        ValueSource calc (BigInteger n);
+        ValueSource error();
+    }
+    
+    private static final Functors functor[] = new Functors[]
+    {
+        new Functors() 
+        {
+            @Override
+            public ValueSource calc(BigInteger n)
+            {
+                return new ValueHolder(AkType.U_BIGINT, n.not());
+            }
+            
+            @Override 
+            public ValueSource error()
+            {
+                return new ValueHolder(AkType.U_BIGINT, 
+                               BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.valueOf(2L)));
+            }
+        },
+        new Functors()
+        {
+            @Override
+            public ValueSource calc(BigInteger n)
+            {
+                return new ValueHolder(AkType.LONG, n.bitCount());
+            }
+            
+            @Override 
+            public ValueSource error()
+            {
+                return new ValueHolder(AkType.LONG, 0L);
+            }
+        }
+    };
+    
+    private ExpressionComposer composer; 
+    private UnaryBitOperator op;
+    
+    
+    public UnaryBitExpressionTest (ExpressionComposer com, UnaryBitOperator op)
+    {
+        composer = com;
+        this.op = op;
+    }
+    
+    @NamedParameterizedRunner.TestParameters
+    public static Collection<Parameterization> params()
+    {
+        ParameterizationBuilder pb = new ParameterizationBuilder();
+        
+        param(pb, UnaryBitExpression.BIT_COUNT_COMPOSER, UnaryBitOperator.COUNT);
+        param(pb, UnaryBitExpression.NOT_COMPOSER, UnaryBitOperator.NOT);
+        
+        return pb.asList();
+    }
+    
+    private static void param(ParameterizationBuilder pb, ExpressionComposer c,  UnaryBitOperator op)
+    {
+        pb.add(op.name() , c, op);
+    }
+
+    @Test
+    public void testLong ()
+    {
+        Expression arg = new LiteralExpression(AkType.LONG, 15L);
+        
+        assertEquals(functor[op.ordinal()].calc(BigInteger.valueOf(15L)), getActualSource(arg));
+    }
+    
+    @Test 
+    public void testDouble() // will fail since doubles cannot (yet) be extracted as long values
+    {
+        Expression arg = new LiteralExpression(AkType.DOUBLE, 15.5);
+        
+        assertEquals(functor[op.ordinal()].calc(BigInteger.valueOf(15L)), getActualSource(arg));
+    }
+    
+    @Test
+    public void testBigInteger()
+    {
+        Expression arg = new LiteralExpression(AkType.U_BIGINT, new BigInteger("101011", 2));
+        
+        assertEquals(functor[op.ordinal()].calc(new BigInteger("101011",2)), getActualSource(arg));
+    }
+    
+    @Test
+    public void testString()
+    {
+        Expression arg = new LiteralExpression(AkType.VARCHAR, "15");
+        
+        assertEquals(functor[op.ordinal()].calc(BigInteger.valueOf(15)), getActualSource(arg));
+    }
+    
+    @Test
+    public void testNull ()
+    {
+        Expression arg = new LiteralExpression(AkType.NULL, null);
+        
+        assertEquals(NullValueSource.only(), getActualSource(arg));
+    }
+    
+    @Test
+    public void testNonNumeric()
+    {
+        Expression arg = new LiteralExpression(AkType.VARCHAR, "a");
+        
+        assertEquals(functor[op.ordinal()].error(), getActualSource(arg));
+    }
+    
+  
+    private ValueSource getActualSource (Expression arg)
+    {
+        return getComposer().compose(Arrays.asList(arg)).evaluation().eval();
+    }
+    
+    @Override
+    protected int childrenCount() 
+    {
+       return 1;
+    }
+
+    @Override
+    protected ExpressionComposer getComposer() 
+    {
+        return composer;
+    }
+    
+}
