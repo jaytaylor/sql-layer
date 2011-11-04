57ed783d2cd117e0c500bd00b558e0e5a9a11bf7
Mike McMahon <mmcm@comcast.net>
2011-11-04 10:16:03 -0400

    Hook up new rule. Add some unit tests for it.

---
+++ src/test/resources/com/akiban/sql/optimizer/rule/split-sort/README.txt
@@ -0,0 +1,7 @@
+sort-1: SELECT list matches ORDER BY
+
+sort-2: columns reversed
+
+sort-3: additional SELECT list columns
+
+sort-4: additional sort columns
--- src/test/resources/com/akiban/sql/optimizer/rule/split-sort/sort-3.sql
+++ src/test/resources/com/akiban/sql/optimizer/rule/split-sort/sort-3.sql
@@ -1,5 +1,6 @@
-SELECT order_date, child.name FROM customers,orders,parent,child
+SELECT order_date, child.name
+  FROM customers,orders,parent,child
  WHERE customers.cid = orders.cid
    AND parent.id = child.pid
    AND customers.name = parent.name
- ORDER BY 1,2
+ ORDER BY child.name
