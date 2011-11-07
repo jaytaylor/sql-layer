2193864efcca1248d1058df0b039887b457d3306
Tim Blackman <tblackman@akiban.com>
2011-11-07 07:32:14 -0500

    - Cleanup

--- src/test/java/com/akiban/sql/pg/YamlTesterParseTest.java
+++ src/test/java/com/akiban/sql/pg/YamlTesterParseTest.java
@@ -16,141 +16,166 @@
 package com.akiban.sql.pg;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.io.StringReader;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
-
 /**
  * Run basic tests of {@code YamlTester} for YAML files that specify tests that
- * fail during parsing.
+ * fail during parsing, or that succeed without performing any statement
+ * execution.
  */
 public class YamlTesterParseTest {
 
     private static final boolean DEBUG =
 	Boolean.getBoolean(YamlTesterParseTest.class.getName() + ".DEBUG");
 
+    /* Test general syntax */
+
     @Test
     public void testIncorrectYaml() {
-	test("!!blah-blah", "testIncorrectYaml");
+	testFail("!!blah-blah", "testIncorrectYaml");
     }
 
     @Test
     public void testEmptyYaml() {
-	test("", "testEmptyYaml");
+	testFail("", "testEmptyYaml");
     }
 
     @Test
     public void testEmptyDocument() {
-	test("---\n...", "testEmptyDocument");
+	testFail("---\n...", "testEmptyDocument");
     }
 
     @Test
     public void testNotSequence() {
-	test("a: b\n", "testNotSequence");
+	testFail("a: b\n", "testNotSequence");
     }
 
     @Test
     public void testFirstElementNotMap() {
-	test("- a\n", "testNotSequence");
+	testFail("- a\n", "testNotSequence");
     }
 
     @Test
     public void testFirstElementKeyNotString() {
-	test("- 1\n", "testFirstElementKeyNotString");
-	test("- [a]\n", "testFirstElementKeyNotString");
+	testFail("- 1\n", "testFirstElementKeyNotString");
+	testFail("- [a]\n", "testFirstElementKeyNotString");
     }
 
     @Test
     public void testFirstElementKeyNotKnown() {
-	test("- UnknownCommand:", "testFirstElementKeyNotKnown");
+	testFail("- UnknownCommand:", "testFirstElementKeyNotKnown");
     }
 
-    @Test
-    public void testStatementValueInteger() {
-	test("- Statement: 3", "testStatementValueInteger");
-    }
-
-    @Test
-    public void testStatementValueSequence() {
-	test("- Statement: [a, b, c]", "testStatementValueInteger");
-    }
-
-    @Test
-    public void testStatementUnknownAttribute() {
-	test("- Statement: a b c" +
-	     "\n- unknown_attrib: x y z", "testStatementValueInteger");
-    }
-
-    @Test
-    public void testStatementParamsNoValue() {
-	test("- Statement: a b c" +
-	     "\n- params:",
-	     "testStatementParamsNoValue");
-    }
-
-    @Test
-    public void testStatementParamsNotSequence() {
-	test("- Statement: a b c" +
-	     "\n- params: 3",
-	     "testStatementParamsNotSequence");
-    }
-
-    @Test
-    public void testStatementParamsValueSequenceOfMaps() {
-	test("- Statement: a b c" +
-	     "\n- params: [a: b]",
-	     "testStatementParamsValueSequenceOfMaps");
-    }
-
-    @Test
-    public void testStatementParamsValueDifferentLengthParams() {
-	test("- Statement: a b c" +
-	     "\n- params: [[a, b], [c, d, e]]",
-	     "testStatementParamsValueDifferentLengthParams");
-    }
-
-    @Test
-    public void testStatementParamsTypesUnknown() {
-	test("- Statement: a b c" +
-	     "\n- params: [[a, b]]" +
-	     "\n- param_types: [CHAR, WHATTYPE]",
-	     "testStatementParamsTypesUnknown");
-    }
+    /* Test Include */
 
     @Test
     public void testIncludeMissingValue() {
-	test("- Include:", "testIncludeMissingValue");
+	testFail("- Include:", "testIncludeMissingValue");
     }
 
     @Test
     public void testIncludeWrongTypeValue() {
-	test("- Include: [a, b]",
-	     "testIncludeWrongTypeValue");
+	testFail("- Include: [a, b]",
+		 "testIncludeWrongTypeValue");
     }
 
     @Test
     public void testIncludeFileNotFound() {
-	test("- Include: file-definitely-not-found",
-	     "testIncludeFileNotFound");
+	testFail("- Include: file-definitely-not-found",
+		 "testIncludeFileNotFound");
     }
 
     @Test
     public void testIncludeUnexpectedAttributes() throws Exception {
-	File tmp = File.createTempFile("abc", null);
-	test("- Include: " + tmp +
-	     "\n- foo: bar",
-	     "testIncludeUnexpectedAttributes");
+	File include = File.createTempFile("include", null);
+	include.deleteOnExit();
+	testFail("- Include: " + include +
+		 "\n- foo: bar",
+		 "testIncludeUnexpectedAttributes");
     }
 
-    private void test(String yaml, String testMethod) {
+    // Test nested includes
+    // Test relative pathname parsing
+
+    @Test
+    public void testIncludeProperties() throws Exception {
+	File include = File.createTempFile("include", null);
+	include.deleteOnExit();
+	writeFile(include,
+		  "---\n" +
+		  "- Properties: all\n");
+	testPass("- Include: " + include);
+    }
+
+    /* Test Properties */
+
+    /* Test Statement */
+
+    @Test
+    public void testStatementValueInteger() {
+	testFail("- Statement: 3", "testStatementValueInteger");
+    }
+
+    @Test
+    public void testStatementValueSequence() {
+	testFail("- Statement: [a, b, c]", "testStatementValueInteger");
+    }
+
+    @Test
+    public void testStatementUnknownAttribute() {
+	testFail("- Statement: a b c" +
+		 "\n- unknown_attrib: x y z", "testStatementValueInteger");
+    }
+
+    @Test
+    public void testStatementParamsNoValue() {
+	testFail("- Statement: a b c" +
+		 "\n- params:",
+		 "testStatementParamsNoValue");
+    }
+
+    @Test
+    public void testStatementParamsNotSequence() {
+	testFail("- Statement: a b c" +
+		 "\n- params: 3",
+		 "testStatementParamsNotSequence");
+    }
+
+    @Test
+    public void testStatementParamsValueSequenceOfMaps() {
+	testFail("- Statement: a b c" +
+		 "\n- params: [a: b]",
+		 "testStatementParamsValueSequenceOfMaps");
+    }
+
+    @Test
+    public void testStatementParamsValueDifferentLengthParams() {
+	testFail("- Statement: a b c" +
+		 "\n- params: [[a, b], [c, d, e]]",
+		 "testStatementParamsValueDifferentLengthParams");
+    }
+
+    @Test
+    public void testStatementParamsTypesUnknown() {
+	testFail("- Statement: a b c" +
+		 "\n- params: [[a, b]]" +
+		 "\n- param_types: [CHAR, WHATTYPE]",
+		 "testStatementParamsTypesUnknown");
+    }
+
+    /* Other methods */
+
+    private void testPass(String yaml) {
+	new YamlTester(null, new StringReader(yaml), null).test();
+    }
+
+    private void testFail(String yaml, String testMethod) {
 	try {
 	    new YamlTester(null, new StringReader(yaml), null).test();
 	} catch (Throwable t) {
@@ -161,5 +186,14 @@ public class YamlTesterParseTest {
 	    return;
 	}
 	fail("Expected exception");
+    }
+
+    private void writeFile(File file, String text) throws IOException {
+	Writer out = new FileWriter(file);
+	try {
+	    out.write(text);
+	} finally {
+	    out.close();
+	}
     }
 }
