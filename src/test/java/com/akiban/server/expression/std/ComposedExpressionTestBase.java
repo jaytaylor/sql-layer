f7f48df2c35440ba508eede136578f3d1c04696b
Vy Nguyen <oontvoo@hotmail.com>
2011-11-08 11:22:12 -0500

    - add checking for null - child

--- src/test/java/com/akiban/server/expression/std/ComposedExpressionTestBase.java
+++ src/test/java/com/akiban/server/expression/std/ComposedExpressionTestBase.java
@@ -204,12 +204,18 @@ public abstract class ComposedExpressionTestBase {
         }
         Expression expression = getComposer().compose(children);
         Set<ExpressionAttribute> attributeSet = attributesSet(attributes);
-        assertEquals("isConstant", attributeSet.contains(IS_CONSTANT) , expression.isConstant());
-        assertEquals("needsRow", attributeSet.contains(NEEDS_ROW)  , expression.needsRow());
-        assertEquals("needsBindings", attributeSet.contains(NEEDS_BINDINGS), expression.needsBindings());
+        assertEquals("isConstant", attributeSet.contains(IS_CONSTANT) /*|| anyChildIsNull(children) && getTestInfo().nullIsContaminating*/ , expression.isConstant());
+        assertEquals("needsRow", attributeSet.contains(NEEDS_ROW) /*&& !(anyChildIsNull(children) && getTestInfo().nullIsContaminating())*/ , expression.needsRow());
+        assertEquals("needsBindings", attributeSet.contains(NEEDS_BINDINGS) /*&& !(anyChildIsNull(children) && getTestInfo().nullIsContaminating())*/, expression.needsBindings());
         return new EvaluationPair(expression.evaluation(), messages);
     }
 
+    private boolean anyChildIsNull (List<Expression> children)
+    {
+        for (Expression child: children)
+            if (child.valueType() == AkType.NULL) return true;
+        return false;
+    }
     private void expectEvalError(ExpressionEvaluation evaluation) {
         try {
             evaluation.eval().isNull();
