d23410e83ffcc98ae9483c8794a69f460d7ce0f9
rsa-key-20111018 <>
2011-11-09 03:52:06 -0500

    add comments, change datastructre to make the code cleaner

--- src/main/java/com/akiban/server/expression/std/ArithExpression.java
+++ src/main/java/com/akiban/server/expression/std/ArithExpression.java
@@ -24,16 +24,40 @@ import com.akiban.server.types.util.AbstractArithValueSource;
 import com.akiban.util.ArgumentValidation;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.List;
 
 public class ArithExpression extends AbstractBinaryExpression
 {
     private final ArithOp op;
     protected final AkType topT;
-    protected static final  List<AkType> SUPPORTED_TYPES = Arrays.asList( // order is IMPORTANT!!!
-            AkType.INTERVAL, AkType.DECIMAL, AkType.DATE, AkType.DOUBLE,
-            AkType.TIME, AkType.U_BIGINT, AkType.DATETIME, AkType.LONG, AkType.YEAR);
+
+    /**
+     * SUPPORTED_TYPES: contains all types that are supported in ArithExpression.
+     *
+     * Notes about the map's structure:
+     * It is important that all date/time types be *put* with an even value key (e.g., 0, 2, 4, ... 2n)
+     * and regular numeric types (double, int, etc) odd value key (e.g., 1, 3, 5, ... 2n +1),
+     * since these keys are going to be used to determine the return type of the expression
+     *
+     * INTERVAL is put with '0' because INTERVAL is special; that is, it can "interact" with both date/time types
+     * and regular numeric types
+     */
+    protected static final BidirectionalMap SUPPORTED_TYPES = new BidirectionalMap(9, 0.5f);
+    static
+    {
+        // date/time types : key is even
+        SUPPORTED_TYPES.put(AkType.INTERVAL, 0);
+        SUPPORTED_TYPES.put(AkType.DATE, 2);
+        SUPPORTED_TYPES.put(AkType.TIME, 4);
+        SUPPORTED_TYPES.put(AkType.DATETIME, 6);
+        SUPPORTED_TYPES.put(AkType.YEAR, 8);
+
+        // regular numeric types: key is odd
+        SUPPORTED_TYPES.put(AkType.DECIMAL, 1);
+        SUPPORTED_TYPES.put(AkType.DOUBLE, 3);
+        SUPPORTED_TYPES.put(AkType.U_BIGINT, 5);
+        SUPPORTED_TYPES.put(AkType.LONG, 7);
+    }
 
     public ArithExpression (Expression lhs, ArithOp op, Expression rhs)
     {
@@ -55,6 +79,7 @@ public class ArithExpression extends AbstractBinaryExpression
         return new InnerEvaluation(op, topT, this.childrenEvaluations());
     }
 
+    //--------------------------------------------------------------------------
     /**
      * Date/time types:
      *      DATE - DATE, TIME - TIME, YEAR - YEAR , DATETIME - DATETIME, => result is an INTERVAL
@@ -74,17 +99,43 @@ public class ArithExpression extends AbstractBinaryExpression
      *
      * Anything else is unsupported
      *
+     *
      * @param leftT
      * @param rightT
      * @param op
      * @return topType
      * @author VyNguyen
+     *
+     * * Why/how this works:
+     *      1) find leftT and rightT's key
+     *      2) find product of the two keys
+     *      3) find sum of the two keys
+     *
+     *      if sum is neg then both are not supported since .get() only returns -1 if the types aren't in the map
+     *      else
+     *          if product is zero then at least one of the two is an INTERVAL (key of INTERVAL is zero)
+     *              check sum :
+     *              if sum is even (0, 2, 4...etc..) then the other operand is either an interval or date/time
+     *                  check op : if it's anything other than + or - => throw exception
+     *              if sum is odd (1, 3, 5...etc..) then the other operand is a regular numeric type
+     *                  check of : if it's anything other than * or / => throw exception
+     *          if product is positive, then both are supported types
+     *              check product:
+     *              if it's odd, then the two operands are both regular numeric (since the product of two numbers can only be odd if both the numbers are odd)
+     *              if it's even, at least one of the two is date/time
+     *                  the only legal case is when both operands are date, or time, and the operator is minus
+     *                  else => throw exception
+     *          if product is negative, then one is supported and one is NOT supported
+     *              check sum:
+     *              if sum is odd => one of the two operand is a date/time and the other is unsupported (since unsupported type get a key of -1, and date/time's key is an even. even -1 = odd)
+     *                  in which case, throw an exception
+     *              else if sum is even => unsupported and a numeric => return the numeric type
      */
     private static AkType getTopType (AkType leftT, AkType rightT, ArithOp op)
     {
        if (leftT == AkType.NULL || rightT == AkType.NULL)  return AkType.NULL;
        String msg = leftT.name() + " " + op.opName() + " " + rightT.name();
-       int l = SUPPORTED_TYPES.indexOf(leftT), r = SUPPORTED_TYPES.indexOf(rightT);
+       int l = SUPPORTED_TYPES.get(leftT), r = SUPPORTED_TYPES.get(rightT);
        int prod = l*r, sum = r + l;
 
        if (sum <= -1 ) throw new UnsupportedOperationException(msg); // both are NOT supported || interval and a NOT supported
@@ -106,11 +157,7 @@ public class ArithExpression extends AbstractBinaryExpression
         else if (prod > 0) // both are supported
         {
             if (prod % 2 == 1) // odd => numeric values only
-            {
-                for (int n = 1; n <= 7; n +=2)
-                    if (l == n || r == n)  return SUPPORTED_TYPES.get(n);
-                return leftT; //should never make it to here
-            }
+                return SUPPORTED_TYPES.get(l < r ? l : r);
             else // even => at least one is datetime
             {
                 if (l == r && op.opName() == '-') return AkType.INTERVAL;
@@ -200,7 +247,20 @@ public class ArithExpression extends AbstractBinaryExpression
         @Override
         protected long rawInterval ()
         {
-            switch (SUPPORTED_TYPES.indexOf(left.getConversionType()) - SUPPORTED_TYPES.indexOf(right.getConversionType()))
+            /*
+             * The rawInterval() is called only if the top's type is INTERVAL, which  only happens when
+             *      one of the two operands is an interval and the other is a regular numeric value
+             *      or both operands are of the same date/time type, that is, both are date, or both are time, etc
+             *
+             * DECIMAL's key in SUPPORTED is 1, DOUBLE's key is 3, [...etc...] and INTERVAL'S is 0.
+             * So if the difference is -1 or 1 => rawDecimal
+             *    if  //               -3 or 3 => rawDouble
+             *      [...etc...]
+             *    else  (when the difference is either 0 or something different than the above)
+                    : must be date/times => rawLong
+             *
+             */
+            switch (SUPPORTED_TYPES.get(left.getConversionType()) - SUPPORTED_TYPES.get(right.getConversionType()))
             {
                 case -1:
                 case 1:     return rawDecimal().longValue();
---
+++ src/main/java/com/akiban/server/expression/std/BidirectionalMap.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.types.AkType;
+import java.util.EnumMap;
+import java.util.Hashtable;
+
+/**
+ * BidirectionalMap: implements a simple bi-directional  map: AkType <--> Integer
+ * (To be used in ArithExpression.java)
+ */
+public class BidirectionalMap
+{
+    private EnumMap<AkType,Integer> map2Int;
+    private Hashtable<Integer,AkType> map2Ak;
+
+    public BidirectionalMap (int size, float lf)
+    {
+        map2Int = new EnumMap<AkType,Integer>(AkType.class);
+        map2Ak = new Hashtable<Integer, AkType>(size, lf);
+    }
+
+    public void put (AkType type, int k)
+    {
+        map2Int.put(type, k);
+        map2Ak.put(k, type);
+    }
+
+    /**
+     *
+     * @param key
+     * @return the AkType corresponding to the key, if there is no such thing, null is returned
+     */
+    public AkType get (int key)
+    {
+        return map2Ak.get(key);
+    }
+
+    /**
+     *
+     * @param key
+     * @return the (integer) key corresponding to AkType, if there is no such thing, -1 is returned
+     */
+    public int get (AkType key)
+    {
+        Integer r = map2Int.get(key);
+        return r == null ? -1 : r;
+    }
+}
