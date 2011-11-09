549b829a53f08be2895ca5161a49b001670df95e
Mike McMahon <mmcm@comcast.net>
2011-11-09 17:35:55 -0500

    Remove unit test data files.

--- src/test/resources/com/akiban/sql/optimizer/operator-old/coi-no-index/select-6l.sql
+++
@@ -1,1 +0,0 @@
-SELECT customers.name, order_date FROM customers LEFT OUTER JOIN (orders INNER JOIN items ON orders.oid = items.oid) ON customers.cid = orders.cid WHERE quan > 100 ORDER BY order_date DESC LIMIT 10
--- src/test/resources/com/akiban/sql/pg/select-old/types_i_udouble.sql
+++
@@ -1,1 +0,0 @@
-SELECT a_int FROM types_i WHERE a_udouble = 3456.0e0
