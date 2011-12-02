e1472610eef938c3416904a9023b8522cbd51aa7
Mike McMahon <mmcm@comcast.net>
2011-12-01 19:08:34 -0500

    Make that unit test more interesting.

--- src/test/resources/com/akiban/sql/optimizer/operator/coi-index/select-13.sql
+++ src/test/resources/com/akiban/sql/optimizer/operator/coi-index/select-13.sql
@@ -1,2 +1,3 @@
 SELECT o1.order_date, o1.oid, o2.oid
-  FROM orders o1 LEFT JOIN orders o2 ON o1.order_date = o2.order_date
+  FROM orders o1 LEFT JOIN orders o2 
+    ON o1.order_date = o2.order_date AND o1.oid <> o2.oid
