cd07767c3328209a6808273abd4381e639740e70
Vy Nguyen <oontvoo@hotmail.com>
2011-12-02 18:29:30 -0500

    set time zone to utc for testing

--- src/test/java/com/akiban/server/expression/std/FromUnixExpressionTest.java
+++ src/test/java/com/akiban/server/expression/std/FromUnixExpressionTest.java
@@ -15,69 +15,72 @@
 
 package com.akiban.server.expression.std;
 
+import org.joda.time.DateTimeZone;
 import com.akiban.server.expression.Expression;
 import com.akiban.server.expression.ExpressionComposer;
 import com.akiban.server.types.AkType;
 import java.util.Arrays;
+import java.util.TimeZone;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * 
- * Test for FromUnixExpression
- * 
- * from_unixtime(...) expression is timezone-sensitive, and thus so as the test.
- * The test was written with the assumption that current timezone is GMT +5,
- * hence it'll only pass in this timezone
- */
+
 public class FromUnixExpressionTest extends ComposedExpressionTestBase
 {
     @Test
     public void test()
     {
-        // epoch
-        test(0L, 19691231190000L); // NOT 1970-01-01 00:00:00 because current timezone isn't UTC
+        DateTimeZone defaultTimezone = DateTimeZone.getDefault();
+        // set time zone to UTC for testing
+        DateTimeZone testingTimeZone = DateTimeZone.forTimeZone(TimeZone.getTimeZone("UTC"));
+        DateTimeZone.setDefault(testingTimeZone);
         
-        // 2007-11-30 11:30:19
-        test(1196440219L, 20071130113019L);
+        // epoch
+        test(0L, 19700101000000L); 
+        
+        // 2007-11-30 16:30:19
+        test(1196440219L, 20071130163019L);
         
         // epoch: unix -> numeric string
-        test(0L, "%y-%m-%d", "69-12-31");
+        test(0L, "%y-%m-%d", "70-1-1");
         
         // epoch : unix -> string, month name
-        test(0L, "%Y-%M-%d", "1969-December-31");
+        test(0L, "%Y-%M-%d", "1970-January-1");
         
         // epoch : unix -> string, month name and day with suffix with leading chars 
-        test(0L, "This is epoch in text %Y-%b-%D", "This is epoch in text 1969-Dec-31st");
+        test(0L, "This is epoch in text %Y-%b-%D", "This is epoch in text 1970-Jan-1st");
         
-        // 2007-11-30 11:30:19
-        test(1196440219L, "%Y %b -%D :: %r", "2007 Nov -30th :: 11:30:19");
+        // 2007-11-30 16:30:19
+        test(1196440219L, "%Y %b -%D :: %r", "2007 Nov -30th :: 04:30:19");
         
-        // 2007-11-30 11:30:19: unix -> sring with trailing chars
-        test(1196440219L, "%D %H display only day of month and hour", "30th 11 display only day of month and hour");
+        // 2007-11-30 16:30:19: unix -> sring with trailing chars
+        test(1196440219L, "%D %H display only day of month and hour", "30th 16 display only day of month and hour");
         
-        // 2007-11-30 11:30:19: unix -> string with chars in between
-        test(1196440219L, "Date part: %d/%m/%Y, Time part: %T", "Date part: 30/11/2007, Time part: 11:30:19");
+        // 2007-11-30 16:30:19: unix -> string with chars in between
+        test(1196440219L, "Date part: %d/%m/%Y, Time part: %T", "Date part: 30/11/2007, Time part: 16:30:19");
         
-        // 2007-11-30 11:30:19: unix --> string, using % to format output
+        // 2007-11-30 16:30:19: unix --> string, using % to format output
         test(1196440219L, "%Y%%%M%%%d", "2007%November%30");
         
-        // 2007-11-30 11:30:19: unix ---> week string
+        // 2007-11-30 16:30:19: unix ---> week string
         test(1196440219L, "%X %V", "2007 47");
         
-        // 2007-11-30 11:30:19: unix ---> week string
+        // 2007-11-30 16:30:19: unix ---> week string
         test(1196440219L, "%x %v", "2007 48");
         
-         // 2007-11-30 11:30:19: unix ---> week string
+         // 2007-11-30 16:30:19: unix ---> week string
         test(1196440219L, "%U", "47");
         
-        // 2007-11-30 11:30:19: unix ---> week string
+        // 2007-11-30 16:30:19: unix ---> week string
         test(1196440219L, "%u", "48");
         
         
         // unknown specifier is treated as regular char
-        test(0L, "%z %D", "z 31st");
+        test(0L, "%z %D", "z 1st");
+        
+        // reset time zone to default
+        DateTimeZone.setDefault(defaultTimezone);
     }
     
     private static void test (long unix, long expected)
