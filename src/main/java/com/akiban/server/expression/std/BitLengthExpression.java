3ff3829c75ffbf85610f1e4f0b5cdb64833d4123
rsa-key-20111018 <>
2011-12-06 22:00:19 -0500

    BitLength expression

---
+++ src/main/java/com/akiban/server/expression/std/BitLengthExpression.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.expression.Expression;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.expression.ExpressionEvaluation;
+import com.akiban.server.expression.ExpressionType;
+import com.akiban.server.service.functions.Scalar;
+import com.akiban.server.types.AkType;
+import com.akiban.server.types.NullValueSource;
+import com.akiban.server.types.ValueSource;
+import com.akiban.server.types.util.ValueHolder;
+import java.io.UnsupportedEncodingException;
+import org.slf4j.LoggerFactory;
+
+public class BitLengthExpression extends AbstractUnaryExpression
+{
+    @Scalar("bit_length")
+    public static final ExpressionComposer COMPOSER = new UnaryComposer()
+    {
+        // TODO: need a new method (probably in the ExpressionComposer)
+        // that would take an extra argument (VARCHAR) which contains
+        // the name of the CHARSET.
+        // For now compose(...) cannot take such argument
+
+        @Override
+        protected Expression compose(Expression argument)
+        {
+            return new BitLengthExpression(argument);
+        }
+
+        @Override
+        protected AkType argumentType(AkType givenType)
+        {
+            return AkType.VARCHAR;
+        }
+
+        @Override
+        protected ExpressionType composeType(ExpressionType argumentType)
+        {
+            return ExpressionTypes.LONG;
+        }
+    };
+
+    private static final class InnerEvaluation extends AbstractUnaryExpressionEvaluation
+    {
+        private final String CHARSET;
+        public InnerEvaluation (ExpressionEvaluation operand, String charset)
+        {
+            super(operand);
+            this.CHARSET = charset;
+        }
+
+        @Override
+        public ValueSource eval()
+        {
+            ValueSource source = operand();
+            if (source.isNull())
+                return NullValueSource.only();
+             else
+                if (CHARSET != null)
+                    try
+                    {
+                        return new ValueHolder(AkType.LONG, source.getString().getBytes(CHARSET).length * 8);
+                    }
+                    catch (UnsupportedEncodingException ex)
+                    {
+                        LoggerFactory.getLogger(BitLengthExpression.class).error("Un-recognised CHARSET", ex);
+                        return NullValueSource.only();
+                    }
+                else
+                    return new ValueHolder(AkType.LONG, source.getString().length() * 8);
+        }
+    }
+
+    public BitLengthExpression (Expression arg)
+    {
+        super(AkType.LONG, arg);
+        CHARSET = null;
+    }
+
+    public BitLengthExpression (Expression arg, String charset)
+    {
+        super(AkType.LONG, arg);
+        this.CHARSET = charset;
+    }
+    
+    @Override
+    protected String name()
+    {
+        return "BIT_LENGTH";
+    }
+
+    @Override
+    public ExpressionEvaluation evaluation()
+    {
+        return new InnerEvaluation(operandEvaluation(), CHARSET);
+    }
+
+    private final String CHARSET;
+}
---
+++ src/test/java/com/akiban/server/expression/std/BitLengthExpressionTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.server.expression.std;
+
+import java.io.IOException;
+import com.akiban.server.expression.Expression;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.types.AkType;
+import com.akiban.server.types.ValueSource;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BitLengthExpressionTest extends ComposedExpressionTestBase
+{
+    @Test
+    public void test() throws IOException
+    {
+        // test regular string
+        test("abc", 24, null); // w/o specifying charset, then bitlength = strlen * 8
+        test("abc", 24, "UTF-8" ); // utf-8 is used as charset
+
+        // test unicode string
+        test("♫♠", 16, null); 
+        test("♫♠", 48, "UTF-8");
+
+        // test empty string
+        test("", 0, null);
+        test("", 0, "UTF-8");
+
+        // test null
+        test (null, -1, null);
+        test (null, -1, "UTF-8");
+    }
+
+    private static void test(String input, long expected, String charset)
+    {
+        Expression ex;
+        if (input == null)
+            ex = LiteralExpression.forNull();
+        else
+            ex = new BitLengthExpression ( new LiteralExpression(AkType.VARCHAR, input), charset);
+
+        ValueSource eval = ex.evaluation().eval();
+
+        if (expected < 0)
+            assertTrue(".eval is null", eval.isNull());
+        else
+            assertEquals("bit_leng(" + input + ")",expected, eval.getLong());
+    }
+
+    @Override
+    protected CompositionTestInfo getTestInfo()
+    {
+        return new CompositionTestInfo(1, AkType.VARCHAR, true);
+    }
+
+    @Override
+    protected ExpressionComposer getComposer()
+    {
+        return BitLengthExpression.COMPOSER;
+    }
+
+    @Override
+    protected boolean alreadyExc()
+    {
+        return false;
+    }
+}
