479f389ff1c89a945665ab3188c6e5bed544dcc8
Vy Nguyen <oontvoo@hotmail.com>
2011-12-15 10:43:34 -0500

    change DateFormatExpression and WeekDayName to use it, remove ExtractorForDateTime

--- src/main/java/com/akiban/server/types/conversion/util/AbstractConverter.java
+++ src/main/java/com/akiban/server/types/conversion/util/AbstractConverter.java
@@ -22,6 +22,19 @@ import com.akiban.server.types.ValueSource;
 
 public interface AbstractConverter<T>
 {
+    /**
+     * 
+     * @param source
+     * @return a (java) object that could be created
+     *  using source's value
+     */
      T get (ValueSource source);
-     T get (String source);
+     
+     /**
+      * 
+      * @param string
+      * @return a (java) object that could be created
+      *  using string's value
+      */
+     T get (String string);
 }
