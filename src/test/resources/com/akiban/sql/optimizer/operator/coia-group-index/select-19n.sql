aa6a47570809b3e56e65f8ebce3792e560311385
Mike McMahon <mmcm@comcast.net>
2011-12-19 17:44:01 -0500

    Make tests more consistent.

--- src/test/resources/com/akiban/sql/optimizer/operator/coia-group-index/select-19n.sql
+++ src/test/resources/com/akiban/sql/optimizer/operator/coia-group-index/select-19n.sql
@@ -2,4 +2,4 @@ SELECT MIN(items.sku)
   FROM customers, orders, items
  WHERE customers.cid = orders.cid
    AND orders.oid = items.oid
-
+   AND customers.name = 'Smith'
--- src/test/resources/com/akiban/sql/optimizer/operator/coia-group-index/select-19x.sql
+++ src/test/resources/com/akiban/sql/optimizer/operator/coia-group-index/select-19x.sql
@@ -2,4 +2,4 @@ SELECT MAX(items.sku)
   FROM customers, orders, items
  WHERE customers.cid = orders.cid
    AND orders.oid = items.oid
-
+   AND customers.name = 'Smith'
