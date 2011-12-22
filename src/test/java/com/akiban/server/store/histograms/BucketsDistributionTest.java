6822121de7f80065f566d976f312a338fe02b481
Yuval Shavit <yshavit@akiban.com>
2011-12-21 20:44:08 -0500

    renaming distribution test

--- src/test/java/com/akiban/server/store/histograms/BucketsDistributionTest.java
+++ src/test/java/com/akiban/server/store/histograms/BucketsDistributionTest.java
@@ -31,7 +31,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public final class FairRandomTieBreakerTest {
+public final class BucketsDistributionTest {
     @Test
     public void evenDistribution() {
         new DistributionChecker<Integer>() {
