899c5933ba93f4d4e0362a041ea66cce3b6dc332
Yuval Shavit <eleusinian@gmail.com>
2011-12-23 14:50:05 -0500

    adding set method, and refactoring the capacity-ensuring

--- src/main/java/com/akiban/util/SparseArray.java
+++ src/main/java/com/akiban/util/SparseArray.java
@@ -17,10 +17,56 @@ package com.akiban.util;
 import java.util.BitSet;
 
 public abstract class SparseArray<T> {
-    
+
+    /**
+     * Gets the element at the specified index. If that element had not been previously defined, its initial
+     * value will be taken from {@link #initialValue()}. When this method returns, the element at the index
+     * will always be defined.
+     * @param index the index to retrieve
+     * @return the element at the specified index
+     */
     public T get(int index) {
         if (index < 0)
             throw new IndexOutOfBoundsException(Integer.toString(index));
+        if (!definedElements.get(index)) {
+            ensureCapacity(index);
+            internalArray[index] = initialValue();
+            definedElements.set(index);
+        }
+        return internalGet(index);
+    }
+
+    /**
+     * Sets the element at the specified index. This also marks that index as defined. This method returns the old
+     * value at the index, or {@code null} if it was undefined. This means that if defined methods can ever be null
+     * in your usage (either because of sets or initial values), you cannot use this method to determine whether the
+     * old value had been defined. Use {@link #isDefined(int)} instead.
+     * @param index the index to set
+     * @param item the new value
+     * @return the old element at this index, or {@code null} if it was undefined
+     */
+    public T set(int index, T item) {
+        if (index < 0)
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        ensureCapacity(index);
+        T old = internalGet(index);
+        // if old != null, this element was definitely defined before, so don't bother redefining it.
+        // if old == null, this element may or may not have been defined, but redefining it is idempotent
+        if (old == null)
+            definedElements.set(index);
+        internalArray[index] = item;
+        return old;
+    }
+    
+    public boolean isDefined(int index) {
+        return definedElements.get(index);
+    }
+
+    protected T initialValue() {
+        return null;
+    }
+
+    private void ensureCapacity(int index) {
         if (index > internalArray.length) {
             int newSize = internalArray.length * GROW_FACTOR;
             if (newSize < index)
@@ -29,17 +75,11 @@ public abstract class SparseArray<T> {
             System.arraycopy(internalArray, 0, newInternalArray, 0, internalArray.length);
             internalArray = newInternalArray;
         }
-        if (internalArray[index] == null) {
-            internalArray[index] = initialValue();
-        }
-        definedElements.set(index);
-        @SuppressWarnings("unchecked")
-        T cast = (T) internalArray[index];
-        return cast;
     }
-
-    protected T initialValue() {
-        return null;
+    
+    @SuppressWarnings("unchecked")
+    private T internalGet(int index) {
+        return (T) internalArray[index];
     }
 
     @Override
