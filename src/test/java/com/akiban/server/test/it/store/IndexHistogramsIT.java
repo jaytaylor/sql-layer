0c2097bbb650b16262583e68d38fb6687f8b0f65
Yuval Shavit <eleusinian@gmail.com>
2011-12-24 01:00:34 -0500

    adding validation to the test -- still need to use it in more tests

--- src/test/java/com/akiban/server/test/it/store/IndexHistogramsIT.java
+++ src/test/java/com/akiban/server/test/it/store/IndexHistogramsIT.java
@@ -23,21 +23,68 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 public final class IndexHistogramsIT extends ITBase {
 
+    private Set<Index> analyzedIndexes = new HashSet<Index>();
+
+    private Map<String,IndexStatistics.HistogramEntry> validateHistogram(
+            String tableName,
+            String indexName,
+            int expectedColumns,
+            int expectedEntries,
+            int expectedCount,
+            int expectedDistinct
+    ) {
+        IndexStatisticsService statsService = statsService();
+        Index index = tIndex(tableName, indexName);
+        if (analyzedIndexes.add(index))
+            statsService.updateIndexStatistics(session(), Collections.singleton(index));
+        IndexStatistics stats = statsService.getIndexStatistics(session(), index);
+        IndexStatistics.Histogram histogram = stats.getHistogram(expectedColumns);
+
+
+        assertEquals("histogram index", index, histogram.getIndex());
+        assertEquals("histogram column count", expectedColumns, histogram.getColumnCount());
+        assertEquals("histogram entries count", expectedEntries, histogram.getEntries().size());
+        
+        Map<String,IndexStatistics.HistogramEntry> result = new HashMap<String, IndexStatistics.HistogramEntry>();
+        String prev = null;
+        int actualCount = 0;
+        int actualDistinct = 0;
+        for (IndexStatistics.HistogramEntry entry : histogram.getEntries()) {
+            String entryString = entry.getKeyString();
+            assertNotNull("entry key string should not be null: " + entry, entryString);
+            if (prev != null && entryString.compareTo(prev) <= 0)
+                fail(String.format("<%s> not greater than <%s>", entryString, prev));
+            
+            actualCount += entry.getEqualCount() + entry.getLessCount();
+            actualDistinct += entry.getEqualCount() + entry.getDistinctCount();
+            
+            result.put(entryString, entry);
+            prev = entryString;
+        }
+        assertEquals("histogram rows count", expectedCount, actualCount);
+        assertEquals("histogram distinct rows", expectedDistinct, actualDistinct);
+        return result;
+    }
+    
     @Test
     public void customersPk() {
-        IndexStatistics stats = tIndex("customers", PK);
-        IndexStatistics.Histogram one = stats.getHistogram(1);
-        fail();
+        validateHistogram("customers", PK, 1, 32, CUSTOMERS_COUNT, CUSTOMERS_COUNT);
     }
 
     @Test
     public void customersNameDob() {
-        IndexStatistics stats = tIndex("customers", "name_dob");
+        IndexStatistics stats = tIndexStats("customers", "name_dob");
         IndexStatistics.Histogram one = stats.getHistogram(1);
         IndexStatistics.Histogram two = stats.getHistogram(2);
         fail();
@@ -45,27 +92,27 @@ public final class IndexHistogramsIT extends ITBase {
     
     @Test
     public void ordersPk() {
-        IndexStatistics stats = tIndex("orders", PK);
+        IndexStatistics stats = tIndexStats("orders", PK);
         IndexStatistics.Histogram one = stats.getHistogram(1);
         fail();
     }
     
     @Test
     public void ordersPlaced() {
-        IndexStatistics stats = tIndex("orders", "placed");
+        IndexStatistics stats = tIndexStats("orders", "placed");
         IndexStatistics.Histogram one = stats.getHistogram(1);
         fail();
     }
     
     @Test
     public void itemsPk() {
-        IndexStatistics stats = tIndex("items", PK);
+        IndexStatistics stats = tIndexStats("items", PK);
         IndexStatistics.Histogram one = stats.getHistogram(1);
         fail();
     }
     
     @Test
-    public void groupIndex() {
+    public void skuByPlacedGI() {
         IndexStatisticsService statsService = statsService();
         statsService.updateIndexStatistics(session(), Collections.singleton(skuByPlaced));
         IndexStatistics stats = statsService.getIndexStatistics(session(), skuByPlaced);
@@ -73,30 +120,45 @@ public final class IndexHistogramsIT extends ITBase {
         IndexStatistics.Histogram two = stats.getHistogram(2);
         fail();
     }
+
+    @Test
+    public void cidOidGI() {
+        IndexStatisticsService statsService = statsService();
+        statsService.updateIndexStatistics(session(), Collections.singleton(cidOid));
+        IndexStatistics stats = statsService.getIndexStatistics(session(), cidOid);
+        IndexStatistics.Histogram one = stats.getHistogram(1);
+        IndexStatistics.Histogram two = stats.getHistogram(2);
+        fail();
+    }
     
-    private IndexStatistics tIndex(String table, String indexName) {
-        Index index = PK.endsWith(indexName)
-                ? getUserTable(SCHEMA, table).getPrimaryKey().getIndex()
-                : getUserTable(SCHEMA, table).getIndex(indexName);
+    private IndexStatistics tIndexStats(String table, String indexName) {
+        Index index = tIndex(table, indexName);
 
         IndexStatisticsService statsService = statsService();
         statsService.updateIndexStatistics(session(), Collections.singleton(index));
         return statsService.getIndexStatistics(session(), index);
     }
 
+    private Index tIndex(String table, String indexName) {
+        return PK.endsWith(indexName)
+                    ? getUserTable(SCHEMA, table).getPrimaryKey().getIndex()
+                    : getUserTable(SCHEMA, table).getIndex(indexName);
+    }
+
     @Before
     public void createDatabase() {
         // schema: tables
-        int cTable = createTable(SCHEMA, "customers", "cid int key, name varchar(64), dob_year int",
+        int cTable = createTable(SCHEMA, "customers", "cid varchar(4) key, name varchar(64), dob_year varchar(4)",
                 "key name_dob (name, dob_year)");
-        int oTable = createTable(SCHEMA, "orders", "oid int key, cid int, placed varchar(64)",
+        int oTable = createTable(SCHEMA, "orders", "oid varchar(4) key, cid varchar(4), placed varchar(64)",
                 "key placed (placed)",
                 akibanFK("cid", "customers", "cid"));
-        int iTable = createTable(SCHEMA, "items", "iid int key, oid int, sku int",
+        int iTable = createTable(SCHEMA, "items", "iid varchar(4) key, oid varchar(4), sku varchar(4)",
                 akibanFK("oid", "orders", "oid"));
         // schema: GIs
         String groupName = getUserTable(SCHEMA, "customers").getGroup().getName();
         skuByPlaced = createGroupIndex(groupName, "skuByPlaced", "items.sku,orders.placed");
+        cidOid = createGroupIndex(groupName, "cidOid", "customers.cid,orders.oid");
         
         // data: customers
         int oid = 0;
@@ -112,32 +174,32 @@ public final class IndexHistogramsIT extends ITBase {
         }
     }
 
-    private void createCustomer(int tableId, long cid) {
-        final long dob;
+    private void createCustomer(int tableId, int cid) {
+        final int dob;
         // values counts do not include 0, which I'm counting as a special case (more convenient accounting)
         if (cid == 0 || divisibleBy(cid, 53)) // 1 + 2 values: 53, 106
-            dob = 33;
+            dob = 1933;
         else if (divisibleBy(cid, 17)) // 8 values
-            dob = 88;
+            dob = 1988;
         else if (divisibleBy(cid, 7)) // 21 - 1 values 119 is preempted by %17
-            dob = 2222;
+            dob = 2022;
         else
-            dob = 11;
+            dob = 1911;
         assert cid >= 0 && cid < 1000 : cid;
-        writeRow(tableId, cid, String.format("cust_#%03d", cid), dob);
+        writeRow(tableId, s(cid), String.format("cust_#%03d", cid), s(dob));
     }
 
-    private void createOrder(int tableId, long oid, long cid) {
-        final long placed;
+    private void createOrder(int tableId, int oid, int cid) {
+        final int placed;
         if (divisibleBy(cid, 20) && divisibleBy(oid, 12))
             placed = 1234;
         else
             placed = 5678;
-        writeRow(tableId, oid + cid, cid, placed);
+        writeRow(tableId, s(oid + cid), s(cid), s(placed));
     }
 
-    private void createItem(int tableId, long iid, long oid) {
-        writeRow(tableId, iid, oid, iid%3);
+    private void createItem(int tableId, int iid, int oid) {
+        writeRow(tableId, s(iid), s(oid), s(iid%3));
     }
 
     private boolean divisibleBy(long var, long denominator) {
@@ -147,8 +209,14 @@ public final class IndexHistogramsIT extends ITBase {
     private IndexStatisticsService statsService() {
         return serviceManager().getServiceByClass(IndexStatisticsService.class);
     }
+    
+    private String s(int intValue) {
+        assert intValue >= 0 && intValue < 10000 : intValue;
+        return String.format("%04d", intValue);
+    }
 
     private GroupIndex skuByPlaced;
+    private GroupIndex cidOid;
     
     private static final String SCHEMA = "indexes";
     private static final String PK = "PK";
