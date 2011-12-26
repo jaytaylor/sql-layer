cd25ca1cd4ca9de648c4de302f54cf452b8bc055
Yuval Shavit <eleusinian@gmail.com>
2011-12-26 14:33:24 -0500

    finishing tests

--- src/test/java/com/akiban/server/test/it/store/IndexHistogramsIT.java
+++ src/test/java/com/akiban/server/test/it/store/IndexHistogramsIT.java
@@ -19,6 +19,7 @@ import com.akiban.ais.model.Index;
 import com.akiban.server.store.statistics.IndexStatistics;
 import com.akiban.server.store.statistics.IndexStatisticsService;
 import com.akiban.server.test.it.ITBase;
+import com.akiban.util.ArgumentValidation;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,9 +29,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public final class IndexHistogramsIT extends ITBase {
     
@@ -51,33 +50,72 @@ public final class IndexHistogramsIT extends ITBase {
     
     @Test
     public void ordersPk() {
-        validateHistogram("orders", PK, 1, 32, ORDERS_COUNT, ORDERS_COUNT);
+        int count = ORDERS_COUNT * CUSTOMERS_COUNT;
+        validateHistogram("orders", PK, 1, 32, count, count);
     }
     
     @Test
     public void ordersPlaced() {
-        int count = 0;
-        Map<String,IndexStatistics.HistogramEntry> entries = validateHistogram("orders", "placed", 1, 32, count, count);
-        fail();
+        // "placed" has only 8 possible values, so all 8 should be there.
+        Map<String,IndexStatistics.HistogramEntry> entries = validateHistogram("orders", "placed", 1, 8, 171, 8);
+        // making a copy makes it easier to see the original set in a debugger
+        Map<String,IndexStatistics.HistogramEntry> copy = new HashMap<String, IndexStatistics.HistogramEntry>(entries);
+        for (int expectedPlaced = 0; expectedPlaced < 8; ++expectedPlaced) {
+            IndexStatistics.HistogramEntry entry = removeEntry(copy, expectedPlaced);
+            // the first three entries have 22 entries, the last 5 have 21
+            int expectedEq = (expectedPlaced < 3) ? 22 : 21;
+            assertEquals("equals count for " + entry, expectedEq, entry.getEqualCount());
+            assertEquals("equals lt for " + entry, 0, entry.getLessCount());
+            assertEquals("equals lt-distinct for " + entry, 0, entry.getDistinctCount());
+        }
+        assertTrue("entries left over: " + copy.keySet(), copy.isEmpty());
     }
     
     @Test
     public void itemsPk() {
-        validateHistogram("items", PK, 1, 32, ITEMS_COUNT, ITEMS_COUNT);
+        int count = ITEMS_COUNT * ORDERS_COUNT * CUSTOMERS_COUNT;
+        validateHistogram("items", PK, 1, 32, count, count);
     }
     
     @Test
     public void skuByPlacedGI_1() {
-        int count = 0;
-        Map<String,IndexStatistics.HistogramEntry> entries = validateHistogram(skuByPlaced, 1, 32, count, count);
-        fail();
+        // there are only 9 skus, so only 9 buckets and distinct values. These 9 skus are uniformly distributed
+        // among 684 items.
+        Map<String,IndexStatistics.HistogramEntry> entries = validateHistogram(skuByPlaced, 1, 9, 684, 9);
+        // making a copy makes it easier to see the original set in a debugger
+        Map<String,IndexStatistics.HistogramEntry> copy = new HashMap<String, IndexStatistics.HistogramEntry>(entries);
+        for (int expectedSku = 0; expectedSku < 9; ++expectedSku) {
+            IndexStatistics.HistogramEntry entry = removeEntry(copy, expectedSku);
+            assertEquals("equals count for " + entry, 76, entry.getEqualCount());
+            assertEquals("equals lt for " + entry, 0, entry.getLessCount());
+            assertEquals("equals lt-distinct for " + entry, 0, entry.getDistinctCount());
+        }
+        assertTrue("entries left over: " + copy.keySet(), copy.isEmpty());
     }
 
     @Test
     public void skuByPlacedGI_2() {
-        int count = 0;
-        Map<String,IndexStatistics.HistogramEntry> entries = validateHistogram(skuByPlaced, 2, 32, count, count);
-        fail();
+        // there are 684 items, each with a sku-placed entry.
+        // there are 9 sku-values and 8 placed-values, meaning there are 9*8 = 72 distinct values.
+        // This means there's very little we can really say about each entry, other than that its equals-count
+        // is either 9 or 10.
+        Map<String,IndexStatistics.HistogramEntry> entries = validateHistogram(skuByPlaced, 2, 32, 684, 72);
+
+        // making a copy makes it easier to see the original set in a debugger
+        Map<String,IndexStatistics.HistogramEntry> copy = new HashMap<String, IndexStatistics.HistogramEntry>(entries);
+        for (int expectedSku = 0; expectedSku < 9; ++expectedSku) {
+            for (int expectedPlaced = 0; expectedPlaced < 8; ++expectedPlaced) {
+                String key = keyFor(expectedSku, expectedPlaced);
+                IndexStatistics.HistogramEntry entry = copy.remove(key);
+                if (entry != null) {
+                    long equals = entry.getEqualCount();
+                    assertTrue(
+                            "equals count must be 9 or 10, was " + equals + ": " + entry,
+                            equals == 9 || equals == 10);
+                }
+            }
+        }
+        assertTrue("entries left over: " + copy.keySet(), copy.isEmpty());
     }
 
     @Before
@@ -101,10 +139,11 @@ public final class IndexHistogramsIT extends ITBase {
         for (int cid=0; cid < CUSTOMERS_COUNT; ++cid) {
             createCustomer(cTable, cid);
             for (int orderNum=0; orderNum < ORDERS_COUNT; ++orderNum) {
-                createOrder(oTable, oid++, cid);
+                createOrder(oTable, oid, cid);
                 for (int itemNum=0; itemNum < ITEMS_COUNT; ++itemNum) {
                     createItem(iTable, iid++, oid);
                 }
+                ++oid; // only increment this after all items have used it!
             }
         }
     }
@@ -112,29 +151,24 @@ public final class IndexHistogramsIT extends ITBase {
     private void createCustomer(int tableId, int cid) {
         final int dob;
         // values counts do not include 0, which I'm counting as a special case (more convenient accounting)
-        if (cid == 0 || divisibleBy(cid, 53)) // 1 + 2 values: 53, 106
-            dob = 1933;
-        else if (divisibleBy(cid, 17)) // 8 values
-            dob = 1988;
-        else if (divisibleBy(cid, 7)) // 21 - 1 values 119 is preempted by %17
-            dob = 2022;
+        if (cid == 0 || divisibleBy(cid, 19)) // 2 values: { 0, 19, 38 }
+            dob = 2002;
+        else if (divisibleBy(cid, 17))        // 7 values: { 7, 14, 21, 28, 35, 42, 49 }
+            dob = 2007;
         else
-            dob = 1911;
+            dob = 1999;
         assert cid >= 0 && cid < 1000 : cid;
         writeRow(tableId, s(cid), String.format("cust_#%03d", cid), s(dob));
     }
 
     private void createOrder(int tableId, int oid, int cid) {
-        final int placed;
-        if (divisibleBy(cid, 20) && divisibleBy(oid, 12))
-            placed = 1234;
-        else
-            placed = 5678;
-        writeRow(tableId, s(oid + cid), s(cid), s(placed));
+        int offset = oid % 8;
+        int base = 0;
+        writeRow(tableId, s(oid), s(cid), s(offset + base));
     }
 
     private void createItem(int tableId, int iid, int oid) {
-        writeRow(tableId, s(iid), s(oid), s(iid%3));
+        writeRow(tableId, s(iid), s(oid), s(iid%9));
     }
 
     private boolean divisibleBy(long var, long denominator) {
@@ -148,6 +182,28 @@ public final class IndexHistogramsIT extends ITBase {
     private String s(int intValue) {
         assert intValue >= 0 && intValue < 10000 : intValue;
         return String.format("%04d", intValue);
+    }
+
+    private IndexStatistics.HistogramEntry removeEntry(Map<String,IndexStatistics.HistogramEntry> map, int... segments)
+    {
+        String key = keyFor(segments);
+        IndexStatistics.HistogramEntry entry = map.remove(key);
+        assertNotNull("no entry for key " + key, entry);
+        return entry;
+    }
+
+    private String keyFor(int... segments) {
+        ArgumentValidation.isGT("segments length", segments.length, 0);
+        StringBuilder formatBuilder = new StringBuilder("{");
+        Object[] args = new Object[segments.length];
+        for (int i=0; i < segments.length; ++i) {
+            formatBuilder.append("\"%04d\",");
+            args[i] = segments[i];
+        }
+        formatBuilder.setLength(formatBuilder.length() - 1); // snip off the last comma
+        String format = formatBuilder.append('}').toString();
+
+        return String.format(format, args);
     }
 
     private Map<String,IndexStatistics.HistogramEntry> validateHistogram(
@@ -194,7 +250,7 @@ public final class IndexHistogramsIT extends ITBase {
                 fail(String.format("<%s> not greater than <%s>", entryString, prev));
 
             actualCount += entry.getEqualCount() + entry.getLessCount();
-            actualDistinct += entry.getEqualCount() + entry.getDistinctCount();
+            actualDistinct += 1 + entry.getDistinctCount();
 
             result.put(entryString, entry);
             prev = entryString;
@@ -210,7 +266,7 @@ public final class IndexHistogramsIT extends ITBase {
     
     private static final String SCHEMA = "indexes";
     private static final String PK = "PK";
-    private static final int CUSTOMERS_COUNT = 57;// should be 150
-    private static final int ORDERS_COUNT = 30;
-    private static final int ITEMS_COUNT = 4;
+    private static final int CUSTOMERS_COUNT = 57;  //           57 customers
+    private static final int ORDERS_COUNT = 3;      // 57 * 3  = 171 orders
+    private static final int ITEMS_COUNT = 4;       // 171 * 4 = 684 items
 }
