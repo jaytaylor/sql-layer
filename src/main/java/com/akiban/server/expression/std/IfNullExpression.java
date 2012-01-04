b1d8ec5a352910b137702b85f75b106cb23e9d31
Vy Nguyen <oontvoo@hotmail.com>
2012-01-04 18:02:03 -0500

    add IfNullExpression

---
+++ src/main/java/com/akiban/server/expression/std/IfNullExpression.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.error.WrongExpressionArityException;
+import com.akiban.server.expression.Expression;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.service.functions.Scalar;
+import java.util.List;
+
+public class IfNullExpression extends CoalesceExpression
+{
+    @Scalar ("ifnull")
+    public static final ExpressionComposer IFNULL_COMPOSER = CoalesceExpression.COMPOSER;
+    
+    protected IfNullExpression (List< ? extends Expression> children)
+    {
+        super(checkArgs(children));
+    }
+
+    private static  List<? extends Expression> checkArgs (List< ? extends Expression> children)
+    {
+        if (children.size() != 2) throw new WrongExpressionArityException(2, children.size());
+        else return children;
+    }
+
+    @Override
+    protected void describe(StringBuilder sb)
+    {
+        sb.append("IF_NULL");
+    }
+}
