8d4ada6bd6cfad777e6bf9c72d86811fe98ac3ba
rsa-key-20111018 <>
2012-01-04 19:55:31 -0500

    give IfNull its own constructor

--- src/main/java/com/akiban/server/expression/std/IfNullExpression.java
+++ src/main/java/com/akiban/server/expression/std/IfNullExpression.java
@@ -18,13 +18,31 @@ package com.akiban.server.expression.std;
 import com.akiban.server.error.WrongExpressionArityException;
 import com.akiban.server.expression.Expression;
 import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.expression.ExpressionType;
+import com.akiban.server.expression.TypesList;
 import com.akiban.server.service.functions.Scalar;
+import com.akiban.sql.StandardException;
+import java.util.Arrays;
 import java.util.List;
 
 public class IfNullExpression extends CoalesceExpression
 {
     @Scalar ("ifnull")
-    public static final ExpressionComposer IFNULL_COMPOSER = CoalesceExpression.COMPOSER;
+    public static final ExpressionComposer IFNULL_COMPOSER = new ExpressionComposer()
+    {
+        @Override
+        public ExpressionType composeType(TypesList argumentTypes) throws StandardException
+        {
+            if (argumentTypes.size() != 2) throw new WrongExpressionArityException(2, argumentTypes.size());
+            return CoalesceExpression.COMPOSER.composeType(argumentTypes);
+        }
+
+        @Override
+        public Expression compose(List<? extends Expression> arguments)
+        {
+            return new IfNullExpression(arguments);
+        }
+    };
     
     protected IfNullExpression (List< ? extends Expression> children)
     {
