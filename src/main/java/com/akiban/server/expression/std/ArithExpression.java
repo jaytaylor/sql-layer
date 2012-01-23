a1418ce2b6a83e772b7ef35bbfcc1c4ef586c485
Vy Nguyen <oontvoo@hotmail.com>
2012-01-23 15:27:45 -0500

    move output type inference of arithmetic operators from parser to expression

--- src/main/java/com/akiban/server/expression/std/ArithExpression.java
+++ src/main/java/com/akiban/server/expression/std/ArithExpression.java
@@ -188,9 +188,17 @@ public class ArithExpression extends AbstractBinaryExpression
 
     protected static boolean isNumeric (AkType type)
     {
-        return SUPPORTED_TYPES.get(type) % 2 == 1;
+        int t = SUPPORTED_TYPES.get(type);
+        if (t < 0) throw new InvalidArgumentTypeException(type + " is not supported");
+        else return t % 2 == 1;
     }
     
+    protected static boolean isDateTime (AkType type)
+    {
+        int t = SUPPORTED_TYPES.get(type);
+        if (t < 0) throw new InvalidArgumentTypeException(type + " is not supported");
+        else return t % 2 == 0;
+    }
     @Override
     protected boolean nullIsContaminating()
     {
