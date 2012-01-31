76baaa59f6fb272ff7bcc01e4075ddb28db61e4f
Yuval Shavit <yshavit@akiban.com>
2012-01-31 16:02:44 -0500

    more cleanup

--- src/main/java/com/akiban/sql/optimizer/rule/OuterJoinPromoter.java
+++ src/main/java/com/akiban/sql/optimizer/rule/OuterJoinPromoter.java
@@ -20,7 +20,6 @@ import com.akiban.server.error.UnsupportedSQLException;
 import com.akiban.sql.optimizer.plan.*;
 import com.akiban.sql.optimizer.plan.JoinNode.JoinType;
 
-import com.akiban.sql.types.DataTypeDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -94,7 +93,6 @@ public class OuterJoinPromoter extends BaseRule
 
     static class RequiredSources implements ExpressionVisitor {
         private Set<ColumnSource> required = new HashSet<ColumnSource>();
-        private Set<ColumnExpression> seenColumns = new HashSet<ColumnExpression>();
         private Deque<ExpressionNode> stack = new ArrayDeque<ExpressionNode>();
         
         public RequiredSources() {
@@ -102,10 +100,6 @@ public class OuterJoinPromoter extends BaseRule
 
         public Set<ColumnSource> getRequired() {
             return required;
-        }
-
-        public Set<ColumnExpression> getColumns() {
-            return seenColumns;
         }
 
         public void intersect(RequiredSources other) {
@@ -127,7 +121,6 @@ public class OuterJoinPromoter extends BaseRule
         @Override
         public boolean visit(ExpressionNode n) {
             if (n instanceof ColumnExpression) {
-                seenColumns.add((ColumnExpression)n);
                 if (!insideDangerousFunction())
                     required.add(((ColumnExpression)n).getTable());
             }
