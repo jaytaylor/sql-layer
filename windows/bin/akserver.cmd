d7e3f34926275aa56c741f4497c7138f25cbb5bb
Mike McMahon <mmcm@comcast.net>
2012-03-05 20:50:50 -0500

    More spaces.

--- windows/bin/akserver.cmd
+++ windows/bin/akserver.cmd
@@ -1,4 +1,4 @@
-@ECHO OFF
+rem @ECHO OFF
 
 REM Copyright (C) 2012 Akiban Technologies Inc.
 REM This program is free software: you can redistribute it and/or modify
@@ -16,9 +16,18 @@ REM along with this program.  If not, see http://www.gnu.org/licenses.
 SETLOCAL
 
 REM Defaults
-FOR %%P IN (%~dp0..) DO SET AKIBAN_HOME=%%~fP
-SET JAR_FILE=%AKIBAN_HOME%\target\akiban-server-1.1.0-SNAPSHOT-jar-with-dependencies.jar
-SET AKIBAN_CONF=%AKIBAN_HOME%\conf
+FOR %%P IN ("%~dp0..") DO SET AKIBAN_HOME=%%~fP
+SET JAR_FILE=akiban-server-1.1.0-SNAPSHOT-jar-with-dependencies.jar
+REM target is when building locally
+IF EXIST "%AKIBAN_HOME%\..\target\%JAR_FILE%" (
+  SET JAR_FILE=%AKIBAN_HOME%\..\target\%JAR_FILE%
+) ELSE (
+  SET JAR_FILE=%AKIBAN_HOME%\lib\%JAR_FILE%
+)
+SET AKIBAN_CONF=%AKIBAN_HOME%
+REM will need to be changed by installer
+SET AKIBAN_LOG=\akiban\log
+
 SET SERVICE_NAME=akserver
 SET SERVICE_DNAME=Akiban Server
 SET SERVICE_DESC=Akiban Database Server
@@ -62,43 +71,44 @@ GOTO NEXT_OPT
 IF "%VERB%"=="version" (
   FOR /F "usebackq" %%V IN (`java -cp "%JAR_FILE%" com.akiban.server.GetVersion`) DO ECHO server   : %%V
   FOR /F "usebackq" %%V IN (`java -cp "%JAR_FILE%" com.persistit.GetVersion`) DO ECHO persistit: %%V
-  %PROCRUN% //VS
+  "%PROCRUN%" //VS
   GOTO EOF
 )
 
 IF "%VERB%"=="start" (
-  %PROCRUN% //ES//%SERVICE_NAME%
+  "%PROCRUN%" //ES//%SERVICE_NAME%
   GOTO EOF
 ) ELSE IF "%VERB%"=="stop" (
-  %PROCRUN% //SS//%SERVICE_NAME%
+  "%PROCRUN%" //SS//%SERVICE_NAME%
   GOTO EOF
 ) ELSE IF "%VERB%"=="uninstall" (
-  %PROCRUN% //DS//%SERVICE_NAME%
+  "%PROCRUN%" //DS//%SERVICE_NAME%
   GOTO EOF
 ) ELSE IF "%VERB%"=="console" (
-  %PROCRUN% //TS//%SERVICE_NAME%
+  "%PROCRUN%" //TS//%SERVICE_NAME%
   GOTO EOF
 ) ELSE IF "%VERB%"=="monitor" (
-  %PROCRUN:srv=mgr% //MR//%SERVICE_NAME%
+  "%PROCRUN:srv=mgr%" //MR//%SERVICE_NAME%
   GOTO EOF
 )
 
-IF EXIST %AKIBAN_CONF%\jvm-options.cmd CALL %AKIBAN_CONF%\jvm-options.cmd
+IF EXIST "%AKIBAN_CONF%\config\jvm-options.cmd" CALL "%AKIBAN_CONF%\config\jvm-options.cmd"
 
 IF "%VERB%"=="run" GOTO RUN_CMD
 
-SET PROCRUN_ARGS=--StartMode=jvm --StartClass com.akiban.server.AkServer --StartMethod=procrunStart --StopMode=jvm --StopClass=com.akiban.server.AkServer --StopMethod=procrunStop --StdOutput="%AKIBAN_HOME%\log\stdout.log" --DisplayName="%SERVICE_DNAME%" --Description="%SERVICE_DESC%" --Startup=manual --Classpath="%JAR_FILE%"
+SET PROCRUN_ARGS=--StartMode=jvm --StartClass com.akiban.server.AkServer --StartMethod=procrunStart --StopMode=jvm --StopClass=com.akiban.server.AkServer --StopMethod=procrunStop --StdOutput="%AKIBAN_LOG%\stdout.log" --DisplayName="%SERVICE_DNAME%" --Description="%SERVICE_DESC%" --Startup=manual --Classpath="%JAR_FILE%"
 REM Each value that might have a space needs a separate ++JvmOptions.
-SET PROCRUN_ARGS=%PROCRUN_ARGS% --JvmOptions="%%JVM_OPTS: =#%" ++JvmOptions="-Dakiban.admin=%AKIBAN_CONF%" ++JvmOptions="-Dservices.config=%AKIBAN_CONF%\config\services-config.yaml" ++JvmOptions="-Dlog4j.configuration=file:%AKIBAN_CONF%\config\log4j.properties"
+SET PROCRUN_ARGS=%PROCRUN_ARGS% --JvmOptions="%JVM_OPTS: =#%" ++JvmOptions="-Dakiban.admin=%AKIBAN_CONF%" ++JvmOptions="-Dservices.config=%AKIBAN_CONF%\config\services-config.yaml" ++JvmOptions="-Dlog4j.configuration=file:%AKIBAN_CONF%\config\log4j.properties"
 IF DEFINED MAX_HEAP_SIZE SET PROCRUN_ARGS=%PROCRUN_ARGS% --JvmMs=%MAX_HEAP_SIZE% --JvmMx=%MAX_HEAP_SIZE%
 
 IF "%VERB%"=="install" (
-  %PROCRUN% //IS//%SERVICE_NAME% %PROCRUN_ARGS%
+  "%PROCRUN%" //IS//%SERVICE_NAME% %PROCRUN_ARGS%
   GOTO EOF
 )
 
 :USAGE
 ECHO Usage: {install,uninstall,start,stop,run,monitor,version} [-j jarfile] [-c confdir] [-g]
+GOTO EOF
 
 :RUN_CMD
 SET JVM_OPTS=%JVM_OPTS% -Dakiban.admin="%AKIBAN_CONF%"
