cd61b862a9d246c88731dfab79001607a7d216cd
Louis Li <louisli@akiban.com>
2012-03-14 11:59:31 -0400

    changed Affero license to 2012

--- src/test/java/com/akiban/server/expression/std/SqrtExpressionTest.java
+++ src/test/java/com/akiban/server/expression/std/SqrtExpressionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Akiban Technologies Inc.
+ * Copyright (C) 2012 Akiban Technologies Inc.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License, version 3,
  * as published by the Free Software Foundation.
