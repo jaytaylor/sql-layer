6b71232d088b41bd9c645d6b3fdbb6bc363a0b19
Yuval Shavit <yshavit@akiban.com>
2012-03-15 16:07:19 -0400

    updating test to use correct logic (already there in the IndexScan impl)

--- src/test/java/com/akiban/sql/optimizer/MultiIndexEnumeratorTest.java
+++ src/test/java/com/akiban/sql/optimizer/MultiIndexEnumeratorTest.java
@@ -258,8 +258,10 @@ public final class MultiIndexEnumeratorTest {
         public boolean removeCoveredConditions(Set<? super String> conditions, List<? super String> removeTo) {
             boolean removedAny = false;
             for (String cond : pegged) {
-                removedAny |= conditions.remove(cond);
-                removeTo.add(cond);
+                if(conditions.remove(cond)) {
+                    removeTo.add(cond);
+                    removedAny = true;
+                }
             }
             return removedAny;
         }
