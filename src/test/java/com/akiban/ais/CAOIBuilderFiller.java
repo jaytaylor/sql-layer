4c38322375ca1e4e32a7aae5800ae6587df8cb8c
Nathan Williams <nwilliams@akiban.com>
2012-03-21 13:31:15 -0400

    Split out the CAOI and builder work from DataDictionaryDDL into ais.test, will be more generally useful

---
+++ src/test/java/com/akiban/ais/CAOIBuilderFiller.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2012 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.akiban.ais;
+
+import com.akiban.ais.model.AkibanInformationSchema;
+import com.akiban.ais.model.aisb2.AISBBasedBuilder;
+import com.akiban.ais.model.aisb2.NewAISBuilder;
+
+public class CAOIBuilderFiller {
+    public final static String CUSTOMER_TABLE = "customer";
+    public final static String ADDRESS_TABLE = "address";
+    public final static String ORDER_TABLE = "order";
+    public final static String ITEM_TABLE = "item";
+    public final static String COMPONENT_TABLE = "component";
+
+    public static NewAISBuilder createAndFillBuilder(String schema) {
+        return fillBuilder(AISBBasedBuilder.create(schema));
+    }
+
+    public static NewAISBuilder createAndFillBuilder(AkibanInformationSchema ais, String schema) {
+        return fillBuilder(AISBBasedBuilder.create(ais, schema));
+    }
+
+    private static NewAISBuilder fillBuilder(NewAISBuilder builder) {
+        builder.userTable(CUSTOMER_TABLE).
+                colBigInt("customer_id", false).
+                colString("customer_name", 100, false).
+                pk("customer_id");
+
+        builder.userTable(ADDRESS_TABLE).
+                colBigInt("customer_id", false).
+                colLong("instance_id", false).
+                colString("address_line1", 60, false).
+                colString("address_line2", 60, false).
+                colString("address_line3", 60, false).
+                pk("customer_id", "instance_id").
+                joinTo("customer").on("customer_id", "customer_id");
+
+        builder.userTable(ORDER_TABLE).
+                colBigInt("order_id", false).
+                colBigInt("customer_id", false).
+                colLong("order_date", false).
+                pk("order_id").
+                joinTo("customer").on("customer_id", "customer_id");
+
+        builder.userTable(ITEM_TABLE).
+                colBigInt("order_id", false).
+                colBigInt("part_id", false).
+                colLong("quantity", false).
+                colLong("unit_price", false).
+                pk("part_id").
+                joinTo("order").on("order_id", "order_id");
+
+        builder.userTable(COMPONENT_TABLE).
+                colBigInt("part_id", false).
+                colBigInt("component_id", false).
+                colLong("supplier_id", false).
+                colLong("unique_id", false).
+                colString("description", 50, true).
+                pk("component_id").
+                uniqueKey("uk", "unique_id").
+                key("xk", "supplier_id").
+                joinTo("item").on("part_id", "part_id");
+
+        return builder;
+    }
+}
