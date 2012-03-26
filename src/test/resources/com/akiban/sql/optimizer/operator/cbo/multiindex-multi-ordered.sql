bb08cb91fe609a2de7866ac7824520a455059318
Mike McMahon <mmcm@comcast.net>
2012-03-26 19:24:54 -0400

    Update unit tests to something that works now.

--- src/test/resources/com/akiban/sql/optimizer/operator/cbo/multiindex-multi-ordered.sql
+++ src/test/resources/com/akiban/sql/optimizer/operator/cbo/multiindex-multi-ordered.sql
@@ -2,4 +2,4 @@ SELECT i1.quan, i2.quan
   FROM customers, orders o1, orders o2, items i1, items i2
  WHERE customers.cid = o1.cid AND customers.cid = o2.cid AND o1.oid = i1.oid AND o2.oid = i2.oid
    AND i1.sku = '1234' AND i2.sku = '4567'
- ORDER BY o1.order_date DESC
+ ORDER BY customers.cid DESC
