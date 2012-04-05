948cd5b97b44209809cdab45edfce1d4e9f24ef1
Jack Orenstein <jao@jao>
2012-04-05 11:12:05 -0400

    Replace Cursor.close() by Cursor.destroy() for cursors that will never be used again.

--- src/main/java/com/akiban/qp/persistitadapter/OperatorStore.java
+++ src/main/java/com/akiban/qp/persistitadapter/OperatorStore.java
@@ -326,7 +326,7 @@ public class OperatorStore extends DelegatingStore<PersistitStore> {
                 }
             }
         } finally {
-            cursor.close();
+            cursor.destroy();
         }
     }
 
