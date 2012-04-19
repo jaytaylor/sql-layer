1cde3dc85b4306a2f17e3499acd4baab2db6e925
Mike McMahon <mmcm@comcast.net>
2012-04-19 15:26:28 -0400

    Simpler test.

--- src/test/resources/com/akiban/sql/optimizer/rule/fold-maps/semi.sql
+++ src/test/resources/com/akiban/sql/optimizer/rule/fold-maps/semi.sql
@@ -2,5 +2,6 @@ SELECT order_date FROM orders
  WHERE EXISTS (SELECT * FROM items, child
                 WHERE items.oid = orders.oid 
                   AND child.id = items.iid
-                  AND sku in ('1234', '4567'))
+                  AND sku = '1234'
+                  AND child.name <> 'Astro')
            OR order_date = '0000-00-00'
