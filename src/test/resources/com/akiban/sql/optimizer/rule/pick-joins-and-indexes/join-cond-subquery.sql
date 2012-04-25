bcf6c238287a1f04f03f7039bcc66ef04665bcbd
Mike McMahon <mmcm@comcast.net>
2012-04-25 15:37:14 -0400

    Unit test now uses primary key index, and thereby prefers a good join order.

--- src/test/resources/com/akiban/sql/optimizer/rule/pick-joins-and-indexes/join-cond-subquery.sql
+++ src/test/resources/com/akiban/sql/optimizer/rule/pick-joins-and-indexes/join-cond-subquery.sql
@@ -1,6 +1,5 @@
 SELECT customers.cid, order_date
- FROM customers
- INNER JOIN orders 
-    ON oid = (SELECT MAX(oid) FROM orders o2 WHERE o2.cid = customers.cid
+  FROM customers INNER JOIN orders 
+    ON oid = (SELECT MIN(oid) FROM orders o2 WHERE o2.cid = customers.cid
                  AND o2.order_date > '2010-01-01')
  WHERE name = 'Smith'
