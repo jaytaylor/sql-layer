e984114c85c61b7d86df2f9746cdb912a06f4f16
Vy Nguyen <oontvoo@hotmail.com>
2012-06-01 18:42:03 -0400

    modify Matcher class to be used in SubstringIndex

--- src/main/java/com/akiban/server/expression/std/AbstractTernaryExpression.java
+++ src/main/java/com/akiban/server/expression/std/AbstractTernaryExpression.java
@@ -29,9 +29,17 @@ package com.akiban.server.expression.std;
 import com.akiban.server.error.WrongExpressionArityException;
 import com.akiban.server.expression.Expression;
 import com.akiban.server.types.AkType;
+import java.util.List;
 
 public abstract class AbstractTernaryExpression extends AbstractCompositeExpression
 {
+    public AbstractTernaryExpression (AkType type, List<? extends Expression> args)
+    {
+        super(type, args);
+        if (args.size() != 3)
+            throw new WrongExpressionArityException(3, args.size());
+    }
+    
     public AbstractTernaryExpression (AkType type, Expression first, Expression second, Expression third)
     {
         super(type, first, second, third);
