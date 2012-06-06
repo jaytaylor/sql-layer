a07d33b6113fd64ecddf36cd90106a53e78b2f50
Nohemi Fernandez <nfernandez@akiban.com>
2012-06-06 13:15:32 -0400

    Finish adding arithmetic functions

--- src/main/java/com/akiban/server/types3/mcompat/mfuncs/MArithmetic.java
+++ src/main/java/com/akiban/server/types3/mcompat/mfuncs/MArithmetic.java
@@ -31,7 +31,7 @@ import com.akiban.server.types3.TExecutionContext;
 import com.akiban.server.types3.mcompat.mtypes.MNumeric;
 import com.akiban.server.types3.pvalue.PValueSource;
 import com.akiban.server.types3.pvalue.PValueTarget;
-import com.akiban.server.types3.texpressions.TArithmetic;
+import com.akiban.server.types3.common.TArithmetic;
 import java.math.BigDecimal;
 
 public class MArithmetic {   
@@ -149,6 +149,73 @@ public class MArithmetic {
             BigDecimal dec1 = (BigDecimal) inputs.get(1).getObject();
             BigDecimal result = dec0.subtract(dec1);
             output.putObject(result);
+        }
+    };
+    
+    // Divide functions
+    TArithmetic DIVIDE_DECIMAL = new TArithmetic("/", MNumeric.DECIMAL, MNumeric.DECIMAL) {
+        @Override 
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            // TODO: Make this faster
+            BigDecimal dec0 = (BigDecimal) inputs.get(0).getObject();
+            BigDecimal dec1 = (BigDecimal) inputs.get(1).getObject();
+            BigDecimal result = dec0.divide(dec1);
+            output.putObject(result);
+        }
+    };
+    
+    // Multiply functions
+    TArithmetic MULTIPLY_TINYINT = new TArithmetic("*", MNumeric.TINYINT, MNumeric.INT) {
+        @Override
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            int a0 = inputs.get(0).getInt8();
+            int a1 = inputs.get(1).getInt8();
+            output.putInt32(a0 * a1);
+        }
+    };
+    
+    TArithmetic MULTIPLY_SMALLINT = new TArithmetic("*", MNumeric.SMALLINT, MNumeric.INT) {
+        @Override
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            int a0 = inputs.get(0).getInt16();
+            int a1 = inputs.get(1).getInt16();
+            output.putInt32(a0 * a1);
+        }
+    };
+    
+    TArithmetic MULTIPLY_MEDIUMINT = new TArithmetic("*", MNumeric.MEDIUMINT, MNumeric.BIGINT) {
+        @Override
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            long a0 = inputs.get(0).getInt32();
+            long a1 = inputs.get(1).getInt32();
+            output.putInt64(a0 * a1);
+        }
+    };
+    
+    TArithmetic MULTIPLY_INT = new TArithmetic("*", MNumeric.INT, MNumeric.BIGINT) {
+        @Override
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            long a0 = inputs.get(0).getInt32();
+            long a1 = inputs.get(1).getInt32();
+            output.putInt64(a0 * a1);
+        }
+    };
+    
+    TArithmetic MULTIPLY_BIGINT = new TArithmetic("*", MNumeric.BIGINT, MNumeric.BIGINT) {
+        @Override
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            long a0 = inputs.get(0).getInt64();
+            long a1 = inputs.get(1).getInt64();
+            output.putInt64(a0 * a1);
+        }
+    };
+    
+    TArithmetic MULTIPLY_DECIMAL = new TArithmetic("*", MNumeric.DECIMAL, MNumeric.DECIMAL) {
+        @Override
+        protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
+            long a0 = inputs.get(0).getInt64();
+            long a1 = inputs.get(1).getInt64();
+            output.putInt64(a0 * a1);
         }
     };
 }
