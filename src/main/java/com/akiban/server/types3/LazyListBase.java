da32e1ae1e0cce548886a6ef3be0ff1abfc9f53f
Yuval Shavit <yshavit@akiban.com>
2012-06-08 11:11:52 -0400

    manually copying over changes from lp:~oontvoo/akiban-server/types3_alpha_string_funcs

--- src/main/java/com/akiban/server/types3/LazyListBase.java
+++ src/main/java/com/akiban/server/types3/LazyListBase.java
@@ -26,7 +26,40 @@
 
 package com.akiban.server.types3;
 
-public interface LazyList<T> extends Iterable<T> {
-    public T get(int i);
-    public int size();
+import java.util.Iterator;
+
+public abstract class LazyListBase<T> implements LazyList<T>
+{
+    private class LazyIterator implements Iterator<T>
+    {
+        private int index;
+        LazyIterator()
+        {
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return index < size();
+        }
+
+        @Override
+        public T next()
+        {
+            return get(index++);
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator()
+    {
+        return new LazyIterator();
+    }
 }
