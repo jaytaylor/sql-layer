e9255c58f10df74dc346076324644bae8ca52264
Vy Nguyen <oontvoo@hotmail.com>
2012-06-13 13:34:23 -0400

    define SPACE functions

---
+++ src/main/java/com/akiban/server/types3/common/funcs/Space.java
@@ -0,0 +1,103 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.types3.common.funcs;
+
+import com.akiban.server.types3.LazyList;
+import com.akiban.server.types3.TClass;
+import com.akiban.server.types3.TCustomOverloadResult;
+import com.akiban.server.types3.TExecutionContext;
+import com.akiban.server.types3.TInstance;
+import com.akiban.server.types3.TOverload;
+import com.akiban.server.types3.TOverloadResult;
+import com.akiban.server.types3.TPreptimeContext;
+import com.akiban.server.types3.TPreptimeValue;
+import com.akiban.server.types3.pvalue.PValueSource;
+import com.akiban.server.types3.pvalue.PValueTarget;
+import com.akiban.server.types3.texpressions.TInputSetBuilder;
+import com.akiban.server.types3.texpressions.TOverloadBase;
+import java.util.Arrays;
+import java.util.List;
+
+public class Space extends TOverloadBase
+{
+    public static TOverload create (TClass stringType, TClass intType)
+    {
+        return new Space(stringType, intType);
+    }
+
+    private final TClass stringType;
+    private final TClass intType;
+    
+    Space(TClass stringType, TClass intType)
+    {
+        this.stringType = stringType;
+        this.intType = intType;
+    }
+    
+    @Override
+    protected void buildInputSets(TInputSetBuilder builder)
+    {
+        builder.covers(stringType, 0);
+    }
+
+    @Override
+    protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output)
+    {
+        char ret[] = new char[inputs.get(0).getInt32()];
+        Arrays.fill(ret, ' ');
+        
+        output.putObject(new String(ret));
+    }
+
+    @Override
+    public String overloadName()
+    {
+        return "SPACE";
+    }
+
+    @Override
+    public TOverloadResult resultType()
+    {
+        return TOverloadResult.custom(new TCustomOverloadResult()
+        {
+            @Override
+            public TInstance resultInstance(List<TPreptimeValue> inputs, TPreptimeContext context)
+            {
+                PValueSource length = inputs.get(0).value();
+                
+                // TODO:
+                // if length operand is not available,
+                // the default return type is LONGTEXT
+                //, which is not defined yet, so we'd return the stringType instead
+                if (length == null || length.isNull())
+                    return stringType.instance();
+                else
+                    return stringType.instance(length.getInt32());
+            }
+        });
+    }
+}
