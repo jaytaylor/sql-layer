f16c57692cb4b072fad21765f353e50ab340c66f
Nohemi Fernandez <nfernandez@akiban.com>
2012-06-18 11:15:36 -0400

    Clean-up headers and unused imports

--- src/main/java/com/akiban/server/types3/common/funcs/Sqrt.java
+++ src/main/java/com/akiban/server/types3/common/funcs/Sqrt.java
@@ -26,14 +26,8 @@
 
 package com.akiban.server.types3.common.funcs;
 
-import com.akiban.server.types3.LazyList;
 import com.akiban.server.types3.TClass;
-import com.akiban.server.types3.TExecutionContext;
 import com.akiban.server.types3.TOverloadResult;
-import com.akiban.server.types3.mcompat.mtypes.MDouble;
-import com.akiban.server.types3.mcompat.mtypes.MNumeric;
-import com.akiban.server.types3.pvalue.PValueSource;
-import com.akiban.server.types3.pvalue.PValueTarget;
 import com.akiban.server.types3.texpressions.TInputSetBuilder;
 import com.akiban.server.types3.texpressions.TOverloadBase;
 
