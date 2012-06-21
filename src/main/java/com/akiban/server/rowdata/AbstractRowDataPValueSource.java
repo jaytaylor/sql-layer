0ad465eca9ca3524eadcbfa7d0f9bff4fadf1935
Nohemi Fernandez <nfernandez@akiban.com>
2012-06-21 15:12:19 -0400

    add skeleton for new types to rowdata

---
+++ src/main/java/com/akiban/server/rowdata/AbstractRowDataPValueSource.java
@@ -0,0 +1,196 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.rowdata;
+
+import com.akiban.server.AkServerUtil;
+import com.akiban.server.Quote;
+import com.akiban.server.encoding.EncodingException;
+import com.akiban.server.types.*;
+import com.akiban.server.types3.pvalue.PUnderlying;
+import com.akiban.server.types3.pvalue.PValueSource;
+import com.akiban.util.AkibanAppender;
+import com.akiban.util.WrappingByteSource;
+
+import java.nio.ByteBuffer;
+
+abstract class AbstractRowDataPValueSource implements PValueSource {
+
+    // ValueSource interface
+
+    @Override
+    public PUnderlying getUnderlyingType() {
+        return fieldDef().getType().pUnderlying();
+    }
+
+    @Override
+    public boolean hasAnyValue() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean hasRawValue() {
+        throw new UnsupportedOperationException("Not supported yet.");      
+    }
+
+    @Override
+    public boolean hasCacheValue() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean isNull() { 
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean getBoolean() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean getBoolean(boolean defaultValue) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public byte getInt8() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public short getInt16() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public char getUInt16() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getInt32() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public long getInt64() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public float getFloat() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double getDouble() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public byte[] getBytes() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object getObject() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+    // for subclasses
+    protected abstract long getRawOffsetAndWidth();
+    protected abstract byte[] bytes();
+    protected abstract FieldDef fieldDef();
+
+    // for use within this class
+
+    private void appendStringField(AkibanAppender appender, Quote quote) {
+        try {
+            final long location = getCheckedOffsetAndWidth();
+            if (appender.canAppendBytes()) {
+                ByteBuffer buff = location == 0
+                        ? null
+                        : AkServerUtil.byteBufferForMySQLString(bytes(), (int)location, (int) (location >>> 32), fieldDef());
+                quote.append(appender, buff, fieldDef().column().getCharsetAndCollation().charset());
+            }
+            else {
+                String s = location == 0
+                        ? null
+                        : AkServerUtil.decodeMySQLString(bytes(), (int)location, (int) (location >>> 32), fieldDef());
+                quote.append(appender, s);
+            }
+        } catch (EncodingException e) {
+            quote.append(appender, "<encoding exception! " + e.getMessage() + '>');
+        }
+    }
+
+    private double doGetDouble() {
+        long asLong = extractLong(Signage.SIGNED);
+        return Double.longBitsToDouble(asLong);
+    }
+
+    private float doGetFloat() {
+        long asLong = extractLong(Signage.SIGNED);
+        int asInt = (int) asLong;
+        return Float.intBitsToFloat(asInt);
+    }
+
+    private String doGetString() {
+        final long location = getRawOffsetAndWidth();
+        return location == 0
+                ? null
+                : AkServerUtil.decodeMySQLString(bytes(), (int) location, (int) (location >>> 32), fieldDef());
+    }
+
+    private long extractLong(Signage signage) {
+        long offsetAndWidth = getCheckedOffsetAndWidth();
+        final int offset = (int)offsetAndWidth;
+        final int width = (int)(offsetAndWidth >>> 32);
+        if (signage == Signage.SIGNED) {
+            return AkServerUtil.getSignedIntegerByWidth(bytes(), offset, width);
+        } else {
+            assert signage == Signage.UNSIGNED;
+            return AkServerUtil.getUnsignedIntegerByWidth(bytes(), offset, width);
+        }
+    }
+
+    private long getCheckedOffsetAndWidth() {
+        long offsetAndWidth = getRawOffsetAndWidth();
+        if (offsetAndWidth == 0) {
+            throw new ValueSourceIsNullException();
+        }
+        return offsetAndWidth;
+    }
+
+    // object state
+    private final WrappingByteSource byteSource = new WrappingByteSource();
+
+    private enum Signage {
+        SIGNED, UNSIGNED
+    }
+}
