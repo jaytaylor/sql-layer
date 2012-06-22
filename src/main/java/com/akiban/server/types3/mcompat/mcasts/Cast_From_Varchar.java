ed79aeb4e35b86106cb2a091c30470d7c2a5fef6
Vy Nguyen <oontvoo@hotmail.com>
2012-06-22 15:31:34 -0400

    add Cast_From_<xyz>

---
+++ src/main/java/com/akiban/server/types3/mcompat/mcasts/Cast_From_Varchar.java
@@ -0,0 +1,104 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.types3.mcompat.mcasts;
+
+import com.akiban.server.error.StringTruncationException;
+import com.akiban.server.types3.TCast;
+import com.akiban.server.types3.TCastBase;
+import com.akiban.server.types3.TExecutionContext;
+import com.akiban.server.types3.TInstance;
+import com.akiban.server.types3.TPreptimeContext;
+import com.akiban.server.types3.TPreptimeValue;
+import com.akiban.server.types3.mcompat.mtypes.MNumeric;
+import com.akiban.server.types3.mcompat.mtypes.MString;
+import com.akiban.server.types3.pvalue.PValueSource;
+import com.akiban.server.types3.pvalue.PValueTarget;
+import com.akiban.server.types3.texpressions.Constantness;
+
+/**
+ * 
+ * This implements a MySQL compatible cast from STRING.
+ * ('Compatible' in that it the String is parsed as much as possible.)
+ * 
+ */
+public class Cast_From_Varchar
+{
+    public static final TCast TO_TINYINT = new TCastBase(MString.VARCHAR, MNumeric.TINYINT, true, Constantness.UNKNOWN)
+    {
+        @Override
+        public TInstance targetInstance(TPreptimeContext context, TPreptimeValue preptimeInput)
+        {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void evaluate(TExecutionContext context, PValueSource source, PValueTarget target)
+        {
+            String st = (String) source.getObject();
+            String truncated;
+            
+            // first attempt
+            try
+            {
+                target.putInt8(Byte.parseByte(st));
+                return;
+            }
+            catch (NumberFormatException e)
+            {
+                context.warnClient(new StringTruncationException(st, truncated = truncate(st)));
+            }
+            
+            // TODO: deal with Overflow? or let it propogate up?
+            // second attempt
+            target.putInt8(Byte.parseByte(truncated));
+        }
+    };
+    
+        /**
+     * Truncate non-digits part
+     * @param st
+     * @return 
+     */
+    static String truncate(String st)
+    {
+        int n = 0;
+        st = st.trim();
+        
+        // if the string starts with non-digits, return 0
+        if (st.isEmpty() || !Character.isDigit(st.charAt(n++)) 
+                && (n == st.length() || n < st.length() && !Character.isDigit(st.charAt(n++))))
+            return "0";
+        
+        char ch;
+        boolean sawDot = false;
+        for (; n < st.length(); ++n)
+            if ((!Character.isDigit(ch = st.charAt(n)))
+                    && (sawDot || !(sawDot = ch == '.')))
+                return st.substring(0, n);
+        return st;
+    }
+}
