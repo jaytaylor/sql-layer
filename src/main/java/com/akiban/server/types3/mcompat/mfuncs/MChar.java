17afef8ee769c21e99ea929bb456df39e43d08ba
Vy Nguyen <oontvoo@hotmail.com>
2012-06-23 19:15:42 -0400

    check point

---
+++ src/main/java/com/akiban/server/types3/mcompat/mfuncs/MChar.java
@@ -0,0 +1,95 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.types3.mcompat.mfuncs;
+
+import com.akiban.server.types3.LazyList;
+import com.akiban.server.types3.TExecutionContext;
+import com.akiban.server.types3.TOverloadResult;
+import com.akiban.server.types3.mcompat.mtypes.MNumeric;
+import com.akiban.server.types3.mcompat.mtypes.MString;
+import com.akiban.server.types3.pvalue.PValueSource;
+import com.akiban.server.types3.pvalue.PValueTarget;
+import com.akiban.server.types3.texpressions.TInputSetBuilder;
+import com.akiban.server.types3.texpressions.TOverloadBase;
+
+public class MChar extends TOverloadBase
+{
+
+    @Override
+    protected void buildInputSets(TInputSetBuilder builder)
+    {
+        builder.vararg(MNumeric.BIGINT);
+    }
+
+    @Override
+    protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output)
+    {
+        // length of the varbin string
+        int length = 0;
+        
+        for(PValueSource num : inputs)
+            length += Math.log(num.getInt64()) / DIV + 1;
+        
+        byte ret 
+    }
+
+    @Override
+    public String overloadName()
+    {
+        return "CHAR";
+    }
+
+    @Override
+    public TOverloadResult resultType()
+    {
+        // TODO: calculate the length of varbinary
+        return TOverloadResult.fixed(MString.VARBINARY.instance());
+    }
+    
+    private static final long MASK = 0xff;
+    private static final double DIV = Math.log(256);
+    
+    /**
+     * TODO: byte is too small ==> causes overflow, but this is what underlies VARBINARY
+     * @param num
+     * @return 
+     */
+    static byte[] parse(long num)
+    {
+        byte ret[] = new byte[(int)(Math.log(num) / DIV) + 1];
+        
+        int n = ret.length -1;
+        
+        while (num > 0)
+        {
+            ret[n] = (byte)(num & MASK);
+            num = num >> 8;
+            --n;
+        }
+        return ret;
+    }
+}
