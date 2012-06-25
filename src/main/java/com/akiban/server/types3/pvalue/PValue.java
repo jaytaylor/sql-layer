2171fbc21ab979f8680f71f9e410487db8a290db
Vy Nguyen <oontvoo@hotmail.com>
2012-06-25 15:08:00 -0400

    clean up

--- src/main/java/com/akiban/server/types3/pvalue/PValue.java
+++ src/main/java/com/akiban/server/types3/pvalue/PValue.java
@@ -310,6 +310,12 @@ public final class PValue implements PValueSource, PValueTarget {
         this(underlying, null);
     }
     
+    public PValue(double val)
+    {
+        this(PUnderlying.DOUBLE);
+        putDouble(val);
+    }
+    
     public PValue(PUnderlying underlying, PValueCacher<?> cacher) {
         this.underlying = underlying;
         this.state = State.UNSET;
