d9207f9845968f5e64e17cc935ad13fba1b0b158
Akiba <akiba@localhost.localdomain>
2012-06-29 12:08:13 -0400

    added ctor

--- src/main/java/com/akiban/server/types3/mcompat/mfuncs/MDay.java
+++ src/main/java/com/akiban/server/types3/mcompat/mfuncs/MDay.java
@@ -9,25 +9,25 @@
  * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
  *
  * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
- * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
- * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
- * YOUR INITIAL PURCHASE.
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A)
+ * DO NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS
+ * OF YOUR INITIAL PURCHASE.
  *
  * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
- * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
- * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
- * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
- * BY SUCH AUTHORIZED PERSONNEL.
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO
+ * SIGN FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT.
+ * THE LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON
+ * ACCEPTANCE BY SUCH AUTHORIZED PERSONNEL.
  *
  * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
  * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
  * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
  */
-
 package com.akiban.server.types3.mcompat.mfuncs;
 
 import com.akiban.server.types3.LazyList;
 import com.akiban.server.types3.TExecutionContext;
+import com.akiban.server.types3.TOverload;
 import com.akiban.server.types3.TOverloadResult;
 import com.akiban.server.types3.mcompat.mtypes.MDatetimes;
 import com.akiban.server.types3.mcompat.mtypes.MNumeric;
@@ -37,57 +37,62 @@ import com.akiban.server.types3.texpressions.TInputSetBuilder;
 import com.akiban.server.types3.texpressions.TOverloadBase;
 import org.joda.time.MutableDateTime;
 
+public class MDay extends TOverloadBase {
 
+    public static final TOverload[] INSTANCE = {
+        new MDay(FuncType.DAY),
+        new MDay(FuncType.DAYOFMONTH),
+        new MDay(FuncType.DAYOFWEEK),
+        new MDay(FuncType.DAYOFYEAR),
+        new MDay(FuncType.WEEKDAY)
+    };
 
+    protected static enum FuncType {
 
-public class MDay extends TOverloadBase{
-    
-     protected static enum DateType {
         DAY {
+
             @Override
-            long evaluate(MutableDateTime cal, long[] input)
-            {
+            long evaluate(MutableDateTime cal, long[] input) {
                 return input[MDatetimes.DAY_INDEX];
-            }            
+            }
         },
         DAYOFMONTH {
+
             @Override
-            long evaluate(MutableDateTime cal, long[] input)
-            {
+            long evaluate(MutableDateTime cal, long[] input) {
                 return input[MDatetimes.DAY_INDEX];
             }
         },
         DAYOFWEEK {
+
             @Override
-            long evaluate(MutableDateTime cal, long[] input)
-            {
+            long evaluate(MutableDateTime cal, long[] input) {
                 return (cal.getDayOfWeek() % 7) + 1;
             }
         },
         DAYOFYEAR {
+
             @Override
-            long evaluate(MutableDateTime cal, long[] input)
-            {
+            long evaluate(MutableDateTime cal, long[] input) {
                 return cal.getDayOfYear();
             }
         },
         WEEKDAY {
+
             @Override
-            long evaluate(MutableDateTime cal, long[] input)
-            {
+            long evaluate(MutableDateTime cal, long[] input) {
                 return cal.getDayOfWeek() - 1;
             }
         };
-        
+
         abstract long evaluate(MutableDateTime cal, long[] input);
     }
-    
-    private final DateType dateType;
-    
-    MDay(DateType dateType) {
-        this.dateType = dateType;
+    private final FuncType funcType;
+
+    MDay(FuncType dateType) {
+        this.funcType = dateType;
     }
-    
+
     @Override
     protected void buildInputSets(TInputSetBuilder builder) {
         builder.covers(MDatetimes.DATETIME, 0);
@@ -97,13 +102,16 @@ public class MDay extends TOverloadBase{
     protected void doEvaluate(TExecutionContext context, LazyList<? extends PValueSource> inputs, PValueTarget output) {
         long[] datetime = MDatetimes.decodeDatetime(inputs.get(0).getInt64());
         MutableDateTime cal = MDatetimes.toJodaDatetime(datetime, context.getCurrentTimezone());
-        if (!MDatetimes.isValidDatetime(datetime)) output.putNull();
-        else output.putInt32((int)(dateType.evaluate(cal, datetime)));
+        if (!MDatetimes.isValidDatetime(datetime)) {
+            output.putNull();
+        } else {
+            output.putInt32((int) (funcType.evaluate(cal, datetime)));
+        }
     }
 
     @Override
     public String overloadName() {
-        return dateType.name();
+        return funcType.name();
     }
 
     @Override
