c1741cf3eb6e449905e67087b22c3bec6524bcc7
Yuval Shavit <yshavit@akiban.com>
2012-07-12 12:00:07 -0400

    creating a test BaseRule which delegates to OverloadAndTInstanceResolver, but only when running with Types3

---
+++ src/test/java/com/akiban/sql/optimizer/rule/ConditionalOverloadAndTInstanceResolver.java
@@ -0,0 +1,56 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.sql.optimizer.rule;
+
+import com.akiban.server.types3.Types3Switch;
+import com.akiban.sql.optimizer.plan.PlanContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ConditionalOverloadAndTInstanceResolver extends BaseRule {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConditionalOverloadAndTInstanceResolver.class);
+
+    @Override
+    protected Logger getLogger() {
+        return logger;
+    }
+
+    @Override
+    public void apply(PlanContext plan) {
+        if (delegate != null)
+            delegate.apply(plan);
+    }
+
+    public ConditionalOverloadAndTInstanceResolver() {
+        delegate = Types3Switch.ON
+                ? new OverloadAndTInstanceResolver()
+                : null;
+    }
+
+    private final BaseRule delegate;
+}
