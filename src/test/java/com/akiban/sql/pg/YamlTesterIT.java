1b90e9fbdbb49e6e01502e5b83cb0436c6ac259d
Tyler Laprade <tlaprade@akiban.com>
2012-07-16 17:29:35 -0400

    fixed ITs

--- src/test/java/com/akiban/sql/pg/YamlTesterIT.java
+++ src/test/java/com/akiban/sql/pg/YamlTesterIT.java
@@ -1338,7 +1338,7 @@ public class YamlTesterIT extends PostgresServerYamlITBase {
 	    "---\n" +
 	    "- Statement: SELECT * FROM c\n" +
 	    "- explain: |\n" +
-	    "    project([Field(0), Field(1)])\n" +
+	    "    project(Field(0), Field(1))\n" +
 	    "      Filter_Default(["+PostgresServerITBase.SCHEMA_NAME+".c])\n" +
 	    "        GroupScan_Default(full scan on _akiban_c)\n");
     }
