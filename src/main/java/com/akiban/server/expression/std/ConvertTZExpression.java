3763b5d08e5b625f59660b6c685ce05dd75b1e44
Vy Nguyen <oontvoo@hotmail.com>
2012-07-25 18:04:50 -0400

    support CONVERT_TZ function

---
+++ src/main/java/com/akiban/server/expression/std/ConvertTZExpression.java
@@ -0,0 +1,141 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.error.WrongExpressionArityException;
+import com.akiban.server.expression.Expression;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.expression.ExpressionEvaluation;
+import com.akiban.server.expression.ExpressionType;
+import com.akiban.server.expression.TypesList;
+import com.akiban.server.service.functions.Scalar;
+import com.akiban.server.types.AkType;
+import com.akiban.server.types.NullValueSource;
+import com.akiban.server.types.ValueSource;
+import com.akiban.server.types.extract.Extractors;
+import com.akiban.sql.StandardException;
+import java.util.List;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+public class ConvertTZExpression extends AbstractTernaryExpression
+{
+    @Scalar("convert_tz")
+    public static ExpressionComposer COMPOSER = new TernaryComposer()
+    {
+        @Override
+        protected Expression doCompose(List<? extends Expression> arguments, List<ExpressionType> typesList)
+        {
+            return new ConvertTZExpression(arguments);
+        }
+
+        @Override
+        public ExpressionType composeType(TypesList argumentTypes) throws StandardException
+        {
+            if (argumentTypes.size() != 3)
+                throw new WrongExpressionArityException(3, 2);
+            
+            argumentTypes.setType(0, AkType.DATETIME);
+            argumentTypes.setType(1, AkType.VARCHAR);
+            argumentTypes.setType(2, AkType.VARCHAR);
+            
+            return ExpressionTypes.DATETIME;
+        }
+        
+    };
+
+    private static class InnerEvaluation extends AbstractThreeArgExpressionEvaluation
+    {
+        public InnerEvaluation(List<? extends ExpressionEvaluation> args)
+        {
+            super(args);
+        }
+        
+        @Override
+        public ValueSource eval()
+        {
+            ValueSource dt, from, to;
+            
+            if ((dt = first()).isNull()
+                    || (from = second()).isNull()
+                    || (to = third()).isNull())
+                return NullValueSource.only();
+
+            if (dt.getDateTime() / 1000000 == 0L) // zero date
+                return NullValueSource.only();
+
+            long ymd[] = Extractors.getLongExtractor(AkType.DATETIME).getYearMonthDayHourMinuteSecond(dt.getDateTime());
+
+            String fromTz = from.getString();
+            String toTz = from.getString();
+            
+            try
+            {
+                DateTime date = new DateTime((int)ymd[0], (int)ymd[1], (int)ymd[2],
+                                             (int)ymd[3], (int)ymd[4], (int)ymd[5], 0,
+                                             DateTimeZone.forID(fromTz));
+            
+                valueHolder().putDateTime(date.withZone(DateTimeZone.forID(toTz)));
+            }
+            catch (IllegalArgumentException e)
+            {
+                return NullValueSource.only();
+            }
+            return valueHolder();
+        }
+    }
+
+    protected ConvertTZExpression(List<? extends Expression> args)
+    {
+        super(AkType.DATETIME, args);
+    }
+
+    @Override
+    protected void describe(StringBuilder sb)
+    {
+        sb.append("CONVERT_TZ");
+    }
+
+    @Override
+    public boolean nullIsContaminating()
+    {
+        return true;
+    }
+
+    @Override
+    public ExpressionEvaluation evaluation()
+    {
+        return new InnerEvaluation(childrenEvaluations());
+    }
+
+    @Override
+    public String name()
+    {
+        return "CONVERT_TZ";
+    }
+    
+}
---
+++ src/test/java/com/akiban/server/expression/std/ConvertTZExpressionTest.java
@@ -0,0 +1,120 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.expression.std;
+
+import com.akiban.server.types.ValueSource;
+import com.akiban.server.expression.Expression;
+import com.akiban.junit.NamedParameterizedRunner;
+import com.akiban.junit.NamedParameterizedRunner.TestParameters;
+import com.akiban.junit.Parameterization;
+import com.akiban.junit.ParameterizationBuilder;
+import com.akiban.server.expression.ExpressionComposer;
+import com.akiban.server.types.AkType;
+import com.akiban.server.types.extract.Extractors;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(NamedParameterizedRunner.class)
+public class ConvertTZExpressionTest extends ComposedExpressionTestBase
+{
+    private static boolean alreadyExc = false;
+    
+    private final String dt;
+    private final String from;
+    private final String to;
+    
+    private final Long expected;
+    public ConvertTZExpressionTest (String dt, String from, String to, Long exp)
+    {
+        this.dt = dt;
+        this.from = from;
+        this.to = to;
+        expected = exp;
+    }
+    
+    @TestParameters
+    public static Collection<Parameterization> params()
+    {
+        ParameterizationBuilder b = new ParameterizationBuilder();
+        
+        param(b, "2009-12-12", "-6:00", "+5:00", 20091212110000L);
+        param(b, "2004-01-01 12:00:00", "GMT", "MET", 20040101130000L);
+
+        return b.asList();
+    }
+    
+    private static void param(ParameterizationBuilder bd, String dt, String from, String to, Long exp)
+    {
+        bd.add("CONVET_TZ(" + dt + ", " + from + ", " + to + ")", dt, from, to, exp);
+    }
+
+    @Test
+    public void test()
+    {
+        Expression date = dt == null
+                          ? LiteralExpression.forNull()
+                          : new LiteralExpression(AkType.DATETIME, 
+                                                  Extractors.getLongExtractor(AkType.DATETIME).getLong(dt));
+        
+        Expression frm = from == null
+                          ? LiteralExpression.forNull()
+                          : new LiteralExpression(AkType.VARCHAR, from);
+        
+        Expression t = to == null
+                       ? LiteralExpression.forNull()
+                       : new LiteralExpression(AkType.VARCHAR, to);
+        
+        ValueSource top = new ConvertTZExpression(Arrays.asList(date, frm, t)).evaluation().eval();
+        
+        if (expected == null)
+            assertTrue("Top should be NULL ", top.isNull());
+        else
+            assertEquals(expected.longValue(), top.getDateTime());
+    }
+    
+    @Override
+    protected CompositionTestInfo getTestInfo()
+    {
+        return new CompositionTestInfo(3, AkType.DATETIME, true);
+    }
+
+    @Override
+    protected ExpressionComposer getComposer()
+    {
+        return ConvertTZExpression.COMPOSER;
+    }
+
+    @Override
+    public boolean alreadyExc()
+    {
+        return alreadyExc;
+    }
+}
