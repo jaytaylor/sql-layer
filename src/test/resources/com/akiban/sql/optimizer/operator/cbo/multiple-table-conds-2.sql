79189d2631d65c2a7b1502bb68782342ee973dee
Mike McMahon <mmcm@comcast.net>
2012-07-26 21:49:21 -0400

    Another test that needs tweaking.

--- src/test/resources/com/akiban/sql/optimizer/operator/cbo/multiple-table-conds-2.sql
+++ src/test/resources/com/akiban/sql/optimizer/operator/cbo/multiple-table-conds-2.sql
@@ -1,6 +1,6 @@
 SELECT customers.name
   FROM customers, addresses, parent
- WHERE customers.name > 'M' AND addresses.state > 'M'
+ WHERE customers.name LIKE 'M%' AND addresses.state > 'M'
    AND parent.name = customers.name
    AND customers.name <> addresses.state AND addresses.state <> 'WY'
    AND parent.name <> addresses.city
