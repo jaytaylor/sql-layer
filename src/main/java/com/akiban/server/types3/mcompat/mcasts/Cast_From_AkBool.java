4f9b69dccaebde6503b737fec6885494a437fe09
Yuval Shavit <eleusinian@gmail.com>
2012-07-29 23:21:40 -0400

    adding various casts

--- src/main/java/com/akiban/server/types3/mcompat/mcasts/Cast_From_AkBool.java
+++ src/main/java/com/akiban/server/types3/mcompat/mcasts/Cast_From_AkBool.java
@@ -23,42 +23,22 @@
  * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
  * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
  */
-
 package com.akiban.server.types3.mcompat.mcasts;
 
 import com.akiban.server.types3.TCast;
 import com.akiban.server.types3.TCastBase;
-import com.akiban.server.types3.TClassBase;
 import com.akiban.server.types3.TExecutionContext;
-import com.akiban.server.types3.mcompat.mtypes.MApproximateNumber;
+import com.akiban.server.types3.aksql.aktypes.AkBool;
+import com.akiban.server.types3.mcompat.mtypes.MString;
 import com.akiban.server.types3.pvalue.PValueSource;
 import com.akiban.server.types3.pvalue.PValueTarget;
-import com.akiban.server.types3.texpressions.Constantness;
 
-public final class Cast_From_Double {
-
-    public static final TCast TO_FLOAT = new TCastBase(MApproximateNumber.DOUBLE, MApproximateNumber.FLOAT) {
+public final class Cast_From_AkBool {
+    public static final TCast TO_VARCHAR = new TCastBase(AkBool.INSTANCE, MString.VARCHAR) {
         @Override
         public void evaluate(TExecutionContext context, PValueSource source, PValueTarget target) {
-            double orig = source.getDouble();
-            double positive = Math.abs(orig);
-            if (positive > Float.MAX_VALUE) {
-                context.reportTruncate(Double.toString(orig), Float.toString(Float.MAX_VALUE));
-                orig = Float.MAX_VALUE; // TODO or is it null?
-            }
-            target.putFloat((float)orig);
-        }
-    };
-
-    public static final TCast TO_DOUBLE_UNSIGNED = new TCastBase(MApproximateNumber.DOUBLE, MApproximateNumber.DOUBLE_UNSIGNED, true) {
-        @Override
-        public void evaluate(TExecutionContext context, PValueSource source, PValueTarget target) {
-            double orig = source.getDouble();
-            if (orig < 0) {
-                context.reportTruncate(Double.toString(orig), "0");
-                orig = 0; // TODO or is it null?
-            }
-            target.putDouble(orig);
+            String boolStr = Boolean.toString(source.getBoolean());
+            target.putString(boolStr, null);
         }
     };
 }
