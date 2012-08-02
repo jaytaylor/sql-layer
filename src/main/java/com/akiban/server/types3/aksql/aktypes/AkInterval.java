fb619f6c7112881901219ee46ffbf989138f3059
Yuval Shavit <yshavit@akiban.com>
2012-08-01 20:02:55 -0400

    consolidating the two enums and their helpers.

--- src/main/java/com/akiban/server/types3/aksql/aktypes/AkInterval.java
+++ src/main/java/com/akiban/server/types3/aksql/aktypes/AkInterval.java
@@ -26,6 +26,7 @@
 
 package com.akiban.server.types3.aksql.aktypes;
 
+import com.akiban.server.error.AkibanInternalException;
 import com.akiban.server.types3.Attribute;
 import com.akiban.server.types3.IllegalNameException;
 import com.akiban.server.types3.TBundleID;
@@ -43,11 +44,16 @@ import com.akiban.server.types3.pvalue.PValueTarget;
 import com.akiban.sql.types.DataTypeDescriptor;
 import com.akiban.sql.types.TypeId;
 import com.akiban.util.AkibanAppender;
+import com.google.common.math.LongMath;
 
+import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AkInterval extends TClassBase {
 
@@ -215,6 +221,12 @@ public class AkInterval extends TClassBase {
     private final Attribute formatAttribute;
     private final Map<TypeId,IntervalFormat> typeIdToFormat;
 
+    private interface IntervalFormat {
+        long parse(String string);
+        TypeId getTypeId();
+        int ordinal();
+    }
+
     private static <F extends IntervalFormat> TParser createParser(final Attribute formatAttribute,
                                                                    final F[] formatters)
     {
@@ -236,5 +248,238 @@ public class AkInterval extends TClassBase {
         for (F literalFormat : values)
             map.put(literalFormat.getTypeId(), literalFormat);
         return map;
+    }
+
+    static enum AkIntervalMonthsFormat implements IntervalFormat {
+        YEAR("Y", TypeId.INTERVAL_YEAR_ID),
+        MONTH("M", TypeId.INTERVAL_MONTH_ID),
+        YEAR_MONTH("Y-M", TypeId.INTERVAL_YEAR_MONTH_ID)
+        ;
+
+        @Override
+        public TypeId getTypeId() {
+            return typeId;
+        }
+
+        @Override
+        public long parse(String string) {
+            return parser.parse(string);
+        }
+
+        AkIntervalMonthsFormat(String pattern, TypeId typeId) {
+            this.parser = new MonthsParser(this, pattern);
+            this.typeId = typeId;
+
+        }
+
+        private final AkIntervalParser<?> parser;
+        private final TypeId typeId;
+
+        private static class MonthsParser extends AkIntervalParser<Boolean> {
+
+            private MonthsParser(Enum<?> onBehalfOf, String pattern) {
+                super(onBehalfOf, pattern);
+            }
+
+            @Override
+            protected void buildChar(char c, ParseCompilation<? super Boolean> result) {
+                switch (c) {
+                case 'Y':
+                    result.addGroupingDigits();
+                    result.addUnit(Boolean.TRUE);
+                    break;
+                case 'M':
+                    result.addUnit(Boolean.FALSE);
+                    result.addGroupingDigits();
+                    break;
+                case '-':
+                    result.addPattern(c);
+                    break;
+                default:
+                    throw new IllegalArgumentException("illegal pattern: " + result.inputPattern());
+                }
+            }
+
+            @Override
+            protected long parseLong(String asString, Boolean isYear) {
+                long parsed = Long.parseLong(asString);
+                if (isYear)
+                    parsed = LongMath.checkedMultiply(parsed, 12);
+                return parsed;
+            }
+        }
+    }
+
+    static enum AkIntervalSecondsFormat implements IntervalFormat {
+
+        DAY("D", TypeId.INTERVAL_DAY_ID),
+        HOUR("H", TypeId.INTERVAL_HOUR_ID),
+        MINUTE("M", TypeId.INTERVAL_MINUTE_ID),
+        SECOND("S", TypeId.INTERVAL_SECOND_ID),
+        DAY_HOUR("D H", TypeId.INTERVAL_DAY_HOUR_ID),
+        DAY_MINUTE("D H:M", TypeId.INTERVAL_DAY_MINUTE_ID),
+        DAY_SECOND("D H:M:S", TypeId.INTERVAL_DAY_SECOND_ID),
+        HOUR_MINUTE("H:M", TypeId.INTERVAL_HOUR_MINUTE_ID),
+        HOUR_SECOND("H:M:S", TypeId.INTERVAL_HOUR_SECOND_ID),
+        MINUTE_SECOND("M:S", TypeId.INTERVAL_MINUTE_SECOND_ID)
+        ;
+
+        static TimeUnit UNDERLYING_UNIT = TimeUnit.MICROSECONDS;
+
+        @Override
+        public TypeId getTypeId() {
+            return typeId;
+        }
+
+        @Override
+        public long parse(String string) {
+            return parser.parse(string);
+        }
+
+        AkIntervalSecondsFormat(String pattern, TypeId typeId) {
+            this.parser = new SecondsParser(this, pattern);
+            this.typeId = typeId;
+        }
+
+        private final AkIntervalParser<?> parser;
+        private final TypeId typeId;
+
+        private static class SecondsParser extends AkIntervalParser<TimeUnit> {
+            private SecondsParser(Enum<?> onBehalfOf, String pattern) {
+                super(onBehalfOf, pattern);
+            }
+
+            @Override
+            protected void buildChar(char c, ParseCompilation<? super TimeUnit> result) {
+                switch (c) {
+                case 'D':
+                    result.addGroupingDigits();
+                    result.addUnit(TimeUnit.DAYS);
+                    break;
+                case 'H':
+                    result.addGroupingDigits();
+                    result.addUnit(TimeUnit.HOURS);
+                    break;
+                case 'M':
+                    result.addGroupingDigits();
+                    result.addUnit(TimeUnit.MINUTES);
+                    break;
+                case 'S':
+                    result.addPattern("(\\d+)(?:\\.(\\d+))?");
+                    result.addUnit(TimeUnit.SECONDS);
+                    result.addUnit(null); // fractional component
+                    break;
+                case ' ':
+                case ':':
+                    result.addPattern(c);
+                    break;
+                default:
+                    throw new IllegalArgumentException("illegal pattern: " + result.inputPattern());
+                }
+            }
+
+            @Override
+            protected long parseLong(String asString, TimeUnit parsedUnit) {
+                long parsed;
+                if (parsedUnit != null) {
+                    parsed = Long.parseLong(asString);
+                }
+                else {
+                    // Fractional seconds component. Need to be careful about how many digits were given.
+                    // We'll normalize to nanoseconds, then convert to what we need. This isn't the most efficient,
+                    // but it means we can change the underlying scale without having to remember this code.
+                    // It's just a couple multiplications and one division, anyway.
+                    if (asString.length() > 8)
+                        asString = asString.substring(0, 9);
+                    parsed = Long.parseLong(asString);
+                    // how many digits short of the full 8 are we? e.g., "123" is 5 short. Need to multiply it
+                    // by shortBy*10 to get to nanos
+                    for (int shortBy= (8 - asString.length()); shortBy > 0; --shortBy)
+                        parsed = LongMath.checkedMultiply(parsed, 10L);
+                    parsedUnit = TimeUnit.NANOSECONDS;
+                }
+                return UNDERLYING_UNIT.convert(parsed, parsedUnit);
+            }
+        }
+    }
+
+    static abstract class AkIntervalParser<U> {
+
+        public long parse(String string) {
+            boolean isNegative;
+            if (string.charAt(0) == '-') {
+                isNegative = true;
+                string = string.substring(1);
+            }
+            else {
+                isNegative = false;
+            }
+            Matcher matcher = regex.matcher(string);
+            if (!matcher.matches())
+                throw new AkibanInternalException("couldn't parse string as " + onBehalfOf.name() + ": " + string);
+            long result = 0;
+            for (int i = 0, len = matcher.groupCount(); i < len; ++i) {
+                String group = matcher.group(i+1);
+                @SuppressWarnings("unchecked")
+                U unit = (U) units;
+                long parsed = parseLong(group, unit);
+                result = LongMath.checkedAdd(result, parsed);
+            }
+            return isNegative ? -result : result;
+        }
+
+        protected abstract void buildChar(char c, ParseCompilation<? super U> result);
+        protected abstract long parseLong(String asString, U unit);
+
+        protected AkIntervalParser(Enum<?> onBehalfOf, String pattern) {
+            ParseCompilation<U> built = compile(pattern);
+            this.regex = Pattern.compile(built.patternBuilder.toString());
+            this.units = built.unitsList.toArray();
+            this.onBehalfOf = onBehalfOf;
+        }
+
+        private final Enum<?> onBehalfOf;
+        private final Pattern regex;
+        private final Object[] units;
+
+        private ParseCompilation<U> compile(String pattern) {
+            ParseCompilation<U> result = new ParseCompilation<U>(pattern);
+            for (int i = 0, len = pattern.length(); i < len; ++i) {
+                char c = pattern.charAt(i);
+                buildChar(c, result);
+            }
+            return result;
+        }
+
+        static class ParseCompilation<U> {
+
+            public void addUnit(U unit) {
+                unitsList.add(unit);
+            }
+
+            public void addPattern(String pattern) {
+                patternBuilder.append(pattern);
+            }
+
+            public void addPattern(char pattern) {
+                patternBuilder.append(pattern);
+            }
+
+            public void addGroupingDigits() {
+                addPattern("(\\d+)");
+            }
+
+            public String inputPattern() {
+                return inputPattern;
+            }
+
+            ParseCompilation(String inputPattern) {
+                this.inputPattern = inputPattern;
+            }
+
+            private String inputPattern;
+            private StringBuilder patternBuilder = new StringBuilder();
+            private List<U> unitsList = new ArrayList<U>();
+        }
     }
 }
