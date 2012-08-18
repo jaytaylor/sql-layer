46139d4bddd590285b8c0a08457762874b868e5f
Jack Orenstein <jao@jao>
2012-08-18 11:59:15 -0400

    Reminder to unignore tests

--- src/test/java/com/akiban/server/test/it/qp/UniqueIndexScanJumpBoundedWithNulls2IT.java
+++ src/test/java/com/akiban/server/test/it/qp/UniqueIndexScanJumpBoundedWithNulls2IT.java
@@ -424,6 +424,7 @@ public class UniqueIndexScanJumpBoundedWithNulls2IT extends OperatorITBase
         }
 
         // 5
+        // TODO: Don't forget to unignore - Vy will be adding @Ignore tags
         @Test
         public void testDAAA_b_1013()
         {
