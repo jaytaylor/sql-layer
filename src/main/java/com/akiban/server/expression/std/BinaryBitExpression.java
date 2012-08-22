de2645cadaefb77780a6458fded82b984dce7258
Yuval Shavit <yshavit@akiban.com>
2012-08-22 19:44:16 -0400

    fixing left and right shifts to return 0 when the rhs is 0. this is a types2 fix as well as types3

--- src/main/java/com/akiban/server/expression/std/BinaryBitExpression.java
+++ src/main/java/com/akiban/server/expression/std/BinaryBitExpression.java
@@ -42,6 +42,9 @@ import com.akiban.server.types.extract.LongExtractor;
 import com.akiban.server.types.extract.ObjectExtractor;
 import com.akiban.server.expression.TypesList;
 import com.akiban.sql.StandardException;
+import com.google.common.primitives.Longs;
+import com.google.common.primitives.UnsignedLongs;
+
 import java.math.BigInteger;
 import java.util.List;
 
@@ -84,7 +87,10 @@ public class BinaryBitExpression extends AbstractBinaryExpression
             @Override
             protected BigInteger exc (ValueSource left, ValueSource right)
             {
-                return bIntExtractor.getObject(left).shiftLeft((int)lExtractor.getLong(right));
+                long shiftBy = lExtractor.getLong(right);
+                if (shiftBy < 0)
+                    return BigInteger.ZERO;
+                return bIntExtractor.getObject(left).shiftLeft((int) shiftBy);
             }
         },
         RIGHT_SHIFT
@@ -92,7 +98,10 @@ public class BinaryBitExpression extends AbstractBinaryExpression
             @Override
             protected BigInteger exc (ValueSource left, ValueSource right)
             {
-                return bIntExtractor.getObject(left).shiftRight((int)lExtractor.getLong(right));
+                long shiftBy = lExtractor.getLong(right);
+                if (shiftBy < 0)
+                    return BigInteger.ZERO;
+                return bIntExtractor.getObject(left).shiftRight((int) shiftBy);
             }
         };
 
