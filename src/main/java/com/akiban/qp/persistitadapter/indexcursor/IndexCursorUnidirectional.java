07c86076b7f2825b0fa2d1d13016b63c3ccccbe0
Jack Orenstein <jao@jao>
2012-08-23 21:38:13 -0400

    Fixing use of cursors in sorting.

--- src/main/java/com/akiban/qp/persistitadapter/indexcursor/IndexCursorUnidirectional.java
+++ src/main/java/com/akiban/qp/persistitadapter/indexcursor/IndexCursorUnidirectional.java
@@ -105,10 +105,21 @@ class IndexCursorUnidirectional<S> extends IndexCursor
     {
         super.close();
         if (startKey != null) {
+            clearStart();
+        }
+    }
+
+    @Override
+    public void destroy()
+    {
+        super.destroy();
+        if (startKey != null) {
             adapter.returnIndexRow(startKey);
+            startKey = null;
         }
         if (endKey != null) {
             adapter.returnIndexRow(endKey);
+            endKey = null;
         }
     }
 
@@ -383,7 +394,7 @@ class IndexCursorUnidirectional<S> extends IndexCursor
             this.startInclusive = keyRange.loInclusive();
             this.end = this.hi;
             this.endInclusive = keyRange.hiInclusive();
-            this.keyComparison = startInclusive ? Key.GTEQ : Key.GT;
+            this.initialKeyComparison = startInclusive ? Key.GTEQ : Key.GT;
             this.subsequentKeyComparison = Key.GT;
             this.startBoundary = Key.BEFORE;
         } else if (ordering.allDescending()) {
@@ -392,7 +403,7 @@ class IndexCursorUnidirectional<S> extends IndexCursor
             this.startInclusive = keyRange.hiInclusive();
             this.end = this.lo;
             this.endInclusive = keyRange.loInclusive();
-            this.keyComparison = startInclusive ? Key.LTEQ : Key.LT;
+            this.initialKeyComparison = startInclusive ? Key.LTEQ : Key.LT;
             this.subsequentKeyComparison = Key.LT;
             this.startBoundary = Key.AFTER;
         } else {
@@ -448,6 +459,7 @@ class IndexCursorUnidirectional<S> extends IndexCursor
             startKey.copyPersistitKeyTo(exchange().getKey());
             pastStart = false;
         }
+        keyComparison = initialKeyComparison;
     }
 
     private Index index()
@@ -465,11 +477,11 @@ class IndexCursorUnidirectional<S> extends IndexCursor
         this.ordering = ordering;
         if (ordering.allAscending()) {
             this.startBoundary = Key.BEFORE;
-            this.keyComparison = Key.GT;
+            this.initialKeyComparison = Key.GT;
             this.subsequentKeyComparison = Key.GT;
         } else if (ordering.allDescending()) {
             this.startBoundary = Key.AFTER;
-            this.keyComparison = Key.LT;
+            this.initialKeyComparison = Key.LT;
             this.subsequentKeyComparison = Key.LT;
         } else {
             assert false : ordering;
@@ -492,6 +504,7 @@ class IndexCursorUnidirectional<S> extends IndexCursor
     private API.Ordering ordering;
     protected int direction; // +1 = ascending, -1 = descending
     protected Key.Direction keyComparison;
+    protected Key.Direction initialKeyComparison;
     protected Key.Direction subsequentKeyComparison;
     protected Key.EdgeValue startBoundary; // Start of a scan that is unbounded at the start
     // start/endBoundColumns is the number of index fields with restrictions. They start out having the same value.
