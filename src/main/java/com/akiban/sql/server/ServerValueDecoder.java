a0a3f167f30156e31c1714f31c5f164cceabbad8
Mike McMahon <mmcm@comcast.net>
2012-08-31 22:58:40 -0400

    Sketch new integration.

--- src/main/java/com/akiban/sql/server/ServerValueDecoder.java
+++ src/main/java/com/akiban/sql/server/ServerValueDecoder.java
@@ -26,13 +26,17 @@
 
 package com.akiban.sql.server;
 
+import com.akiban.server.error.AkibanInternalException;
 import com.akiban.server.error.UnsupportedCharsetException;
 import com.akiban.server.types.AkType;
 import com.akiban.server.types.FromObjectValueSource;
 import com.akiban.server.types.ValueSource;
 import com.akiban.server.types3.pvalue.PValueSource;
 import com.akiban.server.types3.pvalue.PValueSources;
-import java.io.UnsupportedEncodingException;
+
+import org.joda.time.DateTimeZone;
+import java.math.BigDecimal;
+import java.io.*;
 
 public class ServerValueDecoder
 {
@@ -54,15 +58,73 @@ public class ServerValueDecoder
         if (akType == null)
             akType = AkType.VARCHAR;
         Object value;
-        if (binary || (encoded == null)) {
-            value = encoded;
+        if (encoded == null) {
+            value = null;
         }
-        else {
+        else if (!binary) {
             try {
                 value = new String(encoded, encoding);
             }
             catch (UnsupportedEncodingException ex) {
                 throw new UnsupportedCharsetException("", "", encoding);
+            }
+        }
+        else {
+            try {
+                switch (type.getBinaryEncoding()) {
+                case BINARY_OCTAL_TEXT:
+                default:
+                    value = encoded;
+                    break;
+                case INT_8:
+                    value = getDataStream(encoded).read();
+                    break;
+                case INT_16:
+                    value = getDataStream(encoded).readShort();
+                    break;
+                case INT_32:
+                    value = getDataStream(encoded).readInt();
+                    break;
+                case INT_64:
+                    value = getDataStream(encoded).readLong();
+                    break;
+                case FLOAT_32:
+                    value = getDataStream(encoded).readFloat();
+                    break;
+                case FLOAT_64:
+                    value = getDataStream(encoded).readDouble();
+                    break;
+                case STRING_BYTES:
+                    value = new String(encoded, encoding);
+                    break;
+                case BOOLEAN_C:
+                    value = (encoded[0] != 0);
+                    break;
+                case TIMESTAMP_FLOAT64_SECS_2000_NOTZ:
+                    value = seconds2000NoTZ((long)getDataStream(encoded).readDouble());
+                    break;
+                case TIMESTAMP_INT64_MICROS_2000_NOTZ:
+                    value = seconds2000NoTZ(getDataStream(encoded).readLong() / 1000000L);
+                    break;
+                case DECIMAL_PG_NUMERIC_VAR:
+                    {
+                        DataInputStream dstr = getDataStream(encoded);
+                        short ndigits = dstr.readShort();
+                        short[] digits = new short[ndigits + 4];
+                        digits[0] = ndigits;
+                        for (int i = 1; i < digits.length; i++) {
+                            digits[i] = dstr.readShort();
+                        }
+                        value = pgNumericVar(digits);
+                    }
+                    break;
+                }
+            }
+            catch (UnsupportedEncodingException ex) {
+                throw new UnsupportedCharsetException("", "", encoding);
+            }
+            catch (IOException ex) {
+                throw new AkibanInternalException("IO error reading from byte array", ex);
             }
         }
         objectSource.setReflectively(value);
@@ -90,6 +152,25 @@ public class ServerValueDecoder
         }
         PValueSource source = PValueSources.fromObject(value, akType).value();
         context.setPValue(index, source);
+    }
+
+    private static DataInputStream getDataStream(byte[] bytes) {
+        return new DataInputStream(new ByteArrayInputStream(bytes));
+    }
+
+    private static long seconds2000NoTZ(long secs) {
+        long unixtime = secs + 946702800L; // 2000-01-01 00:00:00-UTC.
+        DateTimeZone dtz = DateTimeZone.getDefault();
+        unixtime -= (dtz.getOffset(unixtime * 1000) - dtz.getStandardOffset(unixtime * 1000)) / 1000;
+        return unixtime;
+    }
+
+    private static final short NUMERIC_POS = 0x0000;
+    private static final short NUMERIC_NEG = 0x4000;
+    private static final short NUMERIC_NAN = (short)0xC000;
+
+    private static BigDecimal pgNumericVar(short[] digits) {
+        return null;
     }
 
 }
