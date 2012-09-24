0c724727a9211b00756f5e074146daf8a5e75ee5
Yuval Shavit <yshavit@akiban.com>
2012-09-24 12:59:10 -0400

    strengthing TOverloadBase.constness

--- src/main/java/com/akiban/server/types3/common/funcs/Elt.java
+++ src/main/java/com/akiban/server/types3/common/funcs/Elt.java
@@ -30,9 +30,11 @@ import com.akiban.server.types3.LazyList;
 import com.akiban.server.types3.TClass;
 import com.akiban.server.types3.TExecutionContext;
 import com.akiban.server.types3.TOverloadResult;
+import com.akiban.server.types3.TPreptimeValue;
 import com.akiban.server.types3.pvalue.PValueSource;
 import com.akiban.server.types3.pvalue.PValueTarget;
 import com.akiban.server.types3.pvalue.PValueTargets;
+import com.akiban.server.types3.texpressions.Constantness;
 import com.akiban.server.types3.texpressions.TInputSetBuilder;
 import com.akiban.server.types3.texpressions.TOverloadBase;
 
@@ -66,14 +68,28 @@ public class Elt extends TOverloadBase
         else
             PValueTargets.copyFrom(inputs.get(index), output);
     }
-       
      
     @Override
     protected boolean constnessMatters(int inputIndex) 
     {
         return true;
     }
-    
+
+    @Override
+    protected Constantness constness(int inputIndex, LazyList<? extends TPreptimeValue> values) {
+        assert inputIndex == 0 : inputIndex + " for " + values; // 0 should be enough to fully answer the question
+        PValueSource indexVal = constSource(values, 0);
+        if (indexVal == null)
+            return Constantness.NOT_CONST;
+        if (indexVal.isNull())
+            return Constantness.CONST;
+        int answerIndex = indexVal.getInt32();
+        if (answerIndex < 1 || answerIndex >= values.size())
+            return Constantness.CONST; // answer is null
+        PValueSource answer = constSource(values, answerIndex);
+        return answer == null ? Constantness.NOT_CONST : Constantness.CONST;
+    }
+
     @Override
     protected boolean nullContaminates(int inputIndex) 
     {
