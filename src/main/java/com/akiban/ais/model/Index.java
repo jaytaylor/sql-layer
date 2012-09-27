666a5ec38fa59f827e8c7786bcb434085887e40c
Mike McMahon <mmcm@comcast.net>
2012-09-27 00:31:58 -0400

    Update predicate.

--- src/main/java/com/akiban/ais/model/Index.java
+++ src/main/java/com/akiban/ais/model/Index.java
@@ -32,7 +32,9 @@ import com.akiban.server.collation.AkCollator;
 import com.akiban.server.rowdata.IndexDef;
 import com.akiban.server.service.tree.TreeService;
 import com.akiban.server.types.AkType;
+import com.akiban.server.types3.TInstance;
 import com.akiban.server.types3.Types3Switch;
+import com.akiban.server.types3.mcompat.mtypes.MBigDecimal;
 import com.persistit.Tree;
 
 import java.util.*;
@@ -369,10 +371,11 @@ public abstract class Index implements Traversable
 
     private static boolean isFixedDecimal(Column column) {
         if (Types3Switch.ON) {
-            throw new IllegalStateException("Types3 not implemented");
+            TInstance instance = column.tInstance();
+            return (instance.typeClass() instanceof MBigDecimal);
         } else {
             AkType type = column.getType().akType();
-            return type == AkType.DECIMAL;
+            return (type == AkType.DECIMAL);
         }
     }
 
