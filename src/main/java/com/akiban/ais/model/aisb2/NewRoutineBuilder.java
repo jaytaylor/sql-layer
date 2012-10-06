c4f5a77c7d3122a306bb87080e202defba5a2a31
Mike McMahon <mmcm@comcast.net>
2012-10-05 21:11:43 -0400

    Another variant.

--- src/main/java/com/akiban/ais/model/aisb2/NewRoutineBuilder.java
+++ src/main/java/com/akiban/ais/model/aisb2/NewRoutineBuilder.java
@@ -43,6 +43,8 @@ public interface NewRoutineBuilder {
 
     NewRoutineBuilder paramDoubleOut(String name);
     
+    NewRoutineBuilder externalName(String className);
+
     NewRoutineBuilder externalName(String className, String methodName);
 
     NewRoutineBuilder externalName(String jarName, String className, String methodName);
