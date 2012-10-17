4ccb32f0cf6ff123b7d1f029bf05c70c02067fd1
Nathan Williams <nwilliams@akiban.com>
2012-10-17 11:12:18 -0400

    Release 1.4.2

--- bin/akserver.cmd
+++ bin/akserver.cmd
@@ -28,7 +28,7 @@ REM
 
 SETLOCAL
 
-SET SERVER_JAR=akiban-server-1.4.2-SNAPSHOT-jar-with-dependencies.jar
+SET SERVER_JAR=akiban-server-1.4.2-jar-with-dependencies.jar
 SET SERVICE_NAME=akserver
 SET SERVICE_DNAME=Akiban Server
 SET SERVICE_DESC=Akiban Database Server
