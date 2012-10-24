fbba963149222209cb629025ad93ec7d834895de
Nathan Williams <nwilliams@akiban.com>
2012-10-24 14:40:41 -0400

    More tests

--- src/test/java/com/akiban/server/test/it/store/PersistitStoreSchemaManagerIT.java
+++ src/test/java/com/akiban/server/test/it/store/PersistitStoreSchemaManagerIT.java
@@ -26,6 +26,8 @@
 
 package com.akiban.server.test.it.store;
 
+import com.akiban.ais.model.TableName;
+import com.akiban.server.api.dml.scan.NewRow;
 import org.junit.Test;
 
 import java.util.Set;
@@ -35,31 +37,40 @@ import static com.akiban.server.store.PersistitStoreSchemaManager.SerializationT
 import static org.junit.Assert.assertEquals;
 
 public class PersistitStoreSchemaManagerIT extends PersistitStoreSchemaManagerITBase {
+    private static final TableName TABLE_NAME = new TableName(SCHEMA, T1_NAME);
+    private static final int ROW_COUNT = 10;
+
+    private int tid;
+    private NewRow[] rows = new NewRow[ROW_COUNT];
+
+    private void createAndLoad() {
+        tid = createTable(SCHEMA, T1_NAME, T1_DDL);
+        for(int i = 0; i < ROW_COUNT; ++i) {
+            rows[i] = createNewRow(tid, i+1L);
+        }
+        writeRows(rows);
+    }
+
+
     @Test
     public void newDataSetReadAndSavedAsProtobuf() throws Exception {
         createTable(SCHEMA, T1_NAME, T1_DDL);
         assertEquals("Saved as PROTOBUF", SerializationType.PROTOBUF, pssm.getSerializationType());
-
         safeRestart();
-
         assertEquals("Saw PROTOBUF on load", SerializationType.PROTOBUF, pssm.getSerializationType());
     }
 
     @Test
-    public void delayedTreeRemoval() throws Exception {
-        int tid = createTable(SCHEMA, T1_NAME, T1_DDL);
-        for(int i = 0; i < 5; ++i) {
-            writeRow(tid, i);
-        }
+    public void groupAndIndexTreeDelayedRemoval() throws Exception {
+        createAndLoad();
 
         String groupTreeName = getUserTable(tid).getGroup().getTreeName();
         String pkTreeName = getUserTable(tid).getPrimaryKey().getIndex().getTreeName();
-
         Set<String> treeNames = pssm.getTreeNames();
         assertEquals("Group tree is in set before drop", true, treeNames.contains(groupTreeName));
         assertEquals("PK tree is in set before drop", true, treeNames.contains(pkTreeName));
 
-        ddl().dropTable(session(), tableName(SCHEMA, T1_NAME));
+        ddl().dropTable(session(), TABLE_NAME);
 
         treeNames = pssm.getTreeNames();
         assertEquals("Group tree is in set after drop", true, treeNames.contains(groupTreeName));
@@ -70,8 +81,34 @@ public class PersistitStoreSchemaManagerIT extends PersistitStoreSchemaManagerIT
         treeNames = pssm.getTreeNames();
         assertEquals("Group tree is in set after restart", false, treeNames.contains(groupTreeName));
         assertEquals("PK tree is in set after restart", false, treeNames.contains(pkTreeName));
-
         assertEquals("Group tree exist after restart", false, treeService().treeExists(SCHEMA, groupTreeName));
         assertEquals("PK tree exists after restart", false, treeService().treeExists(SCHEMA, pkTreeName));
+    }
+
+    @Test
+    public void createDropCreateRestart() throws Exception {
+        createAndLoad();
+        expectFullRows(tid, rows);
+        ddl().dropTable(session(), TABLE_NAME);
+
+        // Make sure second table gets new trees that don't get removed on restart
+        createAndLoad();
+        expectFullRows(tid, rows);
+        safeRestart();
+        expectFullRows(tid, rows);
+    }
+
+    @Test
+    public void delayedTreeRemovalRollbackSafe() throws Exception {
+        createAndLoad();
+
+        // Start a transaction for this thread so it can be aborted manually. Less invasive than hooks or similar.
+        treeService().getDb().getTransaction().begin();
+        ddl().dropTable(session(), TABLE_NAME);
+        treeService().getDb().getTransaction().rollback();
+        treeService().getDb().getTransaction().end();
+
+        safeRestart();
+        expectFullRows(tid, rows);
     }
 }
