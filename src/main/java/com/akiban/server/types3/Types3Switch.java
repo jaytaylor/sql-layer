5e0738fd759167c1b922581abdd5dc9829fafd98
Yuval Shavit <yshavit@akiban.com>
2012-11-01 17:21:33 -0400

    defaulting newtypes to on

--- src/main/java/com/akiban/server/types3/Types3Switch.java
+++ src/main/java/com/akiban/server/types3/Types3Switch.java
@@ -27,6 +27,7 @@
 package com.akiban.server.types3;
 
 public class Types3Switch {
-    public static final boolean DEFAULT = Boolean.getBoolean("newtypes");
+    public static final boolean DEFAULT = Boolean.parseBoolean(System.getProperty("newtypes", "true"));
+
     public static volatile boolean ON = DEFAULT;
 }
