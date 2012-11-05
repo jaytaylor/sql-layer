5f5c920baf07b12cb63b9ab3551f0fdca7dd256e
Nathan Williams <nwilliams@akiban.com>
2012-11-05 14:02:30 -0500

    Document waiting

--- src/test/java/com/akiban/server/test/ApiTestBase.java
+++ src/test/java/com/akiban/server/test/ApiTestBase.java
@@ -488,6 +488,14 @@ public class ApiTestBase {
     }
 
     protected void waitForStableAIS() {
+        /*
+         * This is a bit unfortunate, but many ITs will do DDL, grab and AIS and hold
+         * onto contents. If they later do identity comparisons, like OperatorITs do,
+         * then they are vulnerable to AIS changes (e.g. aisMap is cleared and then
+         * latest cache gets updated, causing different (identity) AIS for the same
+         * generation). Waiting for no outstanding queue items avoids that and is
+         * generally very quick.
+         */
         SchemaManager sm = serviceManager().getSchemaManager();
         if(sm instanceof PersistitStoreSchemaManager) {
             PersistitStoreSchemaManager pssm = (PersistitStoreSchemaManager) sm;
