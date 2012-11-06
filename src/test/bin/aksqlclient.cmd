5c61a10671fcfc75cf3baab158f6acac44245c0f
Mike McMahon <mmcm@comcast.net>
2012-11-06 17:24:04 -0500

    Fix classpath in test scripts.

--- src/test/bin/aksqlclient.cmd
+++ src/test/bin/aksqlclient.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal
-for %%f in (%~dp0..\..\..\target\akiban-server-*.*.*-SNAPSHOT.jar) do set BASEJAR=%%~dpnf
-java -cp %BASEJAR%-jar-with-dependencies.jar;%BASEJAR%-tests.jar com.akiban.sql.test.SQLClient %*
+for %%f in (%~dp0..\..\..\target) do set TARGET=%%~dpnf
+for %%f in (%TARGET%\akiban-server-*.*.*-SNAPSHOT.jar) do set BASEJAR=%%~dpnf
+java -cp %BASEJAR%.jar;%BASEJAR%-tests.jar;%TARGET%\dependency\* com.akiban.sql.test.SQLClient %*
 endlocal
