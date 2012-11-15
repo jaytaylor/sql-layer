784d734ce484ac53e0ca4583316941f2f21e0ba7
Mike McMahon <mmcm@comcast.net>
2012-11-15 18:00:30 -0500

    Put the inside join condition on the inside join.

--- src/main/java/com/akiban/sql/optimizer/rule/InConditionReverser.java
+++ src/main/java/com/akiban/sql/optimizer/rule/InConditionReverser.java
@@ -120,12 +120,25 @@ public class InConditionReverser extends BaseRule
                                         joinConditions, hasDistinct))
                 return;
         }
+        ConditionList insideJoinConditions = null;
         if (input instanceof Select) {
             Select inselect = (Select)input;
-            joinConditions.addAll(inselect.getConditions());
+            insideJoinConditions = inselect.getConditions();
             input = inselect.getInput();
         }
         if (input instanceof Joinable) {
+            if (insideJoinConditions != null) {
+                if (input instanceof JoinNode) {
+                    JoinNode insideJoin = (JoinNode)input;
+                    if (insideJoin.getJoinConditions() != null)
+                        insideJoin.getJoinConditions().addAll(insideJoinConditions);
+                    else
+                        insideJoin.setJoinConditions(insideJoinConditions);
+                }
+                else {
+                    joinConditions.addAll(insideJoinConditions);
+                }
+            }
             convertToSemiJoin(select, selectElement, selectInput, 
                               (Joinable)input, joinConditions, 
                               (negated) ? JoinType.ANTI : JoinType.SEMI);
