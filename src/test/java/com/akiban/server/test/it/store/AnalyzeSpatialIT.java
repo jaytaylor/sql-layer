5c5b0be5f2811fb3cc549b0098d5e130c55fe741
Yuval Shavit <yshavit@akiban.com>
2012-11-20 19:07:17 -0500

    fixing the bug by ignoring spatial indexes in indexStatisticsToHistogram

--- src/test/java/com/akiban/server/test/it/store/AnalyzeSpatialIT.java
+++ src/test/java/com/akiban/server/test/it/store/AnalyzeSpatialIT.java
@@ -26,6 +26,7 @@
 
 package com.akiban.server.test.it.store;
 
+import com.akiban.ais.model.Index;
 import com.akiban.ais.model.TableName;
 import com.akiban.server.geophile.Space;
 import com.akiban.server.test.it.ITBase;
@@ -45,7 +46,7 @@ public final class AnalyzeSpatialIT extends ITBase {
                 new TableName("schem", "tab"), Collections.singleton("idxgeo"));
         dml().getTableStatistics(session(), cid, false);
     }
-    
+
     @Test
     public void geoAndOther() {
         int cid = createTable("schem", "tab", "id int not null primary key", "lat decimal(11,07)", "lon decimal(11,7)",
@@ -57,5 +58,25 @@ public final class AnalyzeSpatialIT extends ITBase {
         serviceManager().getDXL().ddlFunctions().updateTableStatistics(session(),
                 new TableName("schem", "tab"), Collections.singleton("idxgeo"));
         dml().getTableStatistics(session(), cid, false);
+    }
+
+    @Test
+    public void geoGroup() {
+        int cid = createTable("schem", "cust", "id int not null primary key", "lat decimal(11,07)", "lon decimal(11,7)",
+                "name varchar(32)");
+        int oid = createTable("schem", "orders", "id int not null primary key", "cid int not null", "colour varchar(3)",
+                akibanFK("cid", "cust", "id"));
+
+        TableName groupName = ais().getUserTable("schem", "cust").getGroup().getName();
+
+        createSpatialGroupIndex(groupName, "idxgeogrp", 0, Space.LAT_LON_DIMENSIONS,
+                "cust.lat, cust.lon, orders.colour", Index.JoinType.LEFT);
+        writeRow(cid, 10L, "10", "11", "foo");
+        writeRow(oid, 20L, 10L, "red");
+        dml().getTableStatistics(session(), cid, false);
+        serviceManager().getDXL().ddlFunctions().updateTableStatistics(session(),
+                new TableName("schem", "orders"), Collections.singleton("idxgeogrp"));
+        dml().getTableStatistics(session(), cid, false);
+        dml().getTableStatistics(session(), oid, false);
     }
 }
