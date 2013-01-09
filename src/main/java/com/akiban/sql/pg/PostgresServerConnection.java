27d1bb632e3fd406e4b5affd024f077494a0b681
Yuval Shavit <yshavit@akiban.com>
2013-01-08 19:55:02 -0500

    adding tests to disallow certain behaviors like ddl, etc

--- src/main/java/com/akiban/sql/pg/PostgresServerConnection.java
+++ src/main/java/com/akiban/sql/pg/PostgresServerConnection.java
@@ -733,6 +733,7 @@ public class PostgresServerConnection extends ServerSessionBase
                 sessionMonitor.leaveStage();
             }
             pstmt = generateStatementStub(sql, stmt, params, paramTypes);
+            checkStatementIsAllowed(pstmt);
             ServerTransaction local = beforeExecute(pstmt);
             boolean success = false;
             try {
