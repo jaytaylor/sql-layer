d8f5f1e0c3896aabf6f2520c672899dd7f133483
Nathan Williams <nwilliams@akiban.com>
2013-01-30 09:27:45 -0500

    Expose http port

--- src/main/java/com/akiban/http/HttpConductor.java
+++ src/main/java/com/akiban/http/HttpConductor.java
@@ -31,4 +31,5 @@ import org.eclipse.jetty.server.handler.ContextHandler;
 public interface HttpConductor {
     void registerHandler(ContextHandler handler);
     void unregisterHandler(ContextHandler handler);
+    int getPort();
 }
