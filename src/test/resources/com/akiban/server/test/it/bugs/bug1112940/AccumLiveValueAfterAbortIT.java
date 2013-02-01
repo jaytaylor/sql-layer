2f6983132a882013637f88356d9d6bc598573ddb
Nathan Williams <nwilliams@akiban.com>
2013-02-01 18:27:38 -0500

    Test case for REPLACE behavior

--- src/test/resources/com/akiban/server/test/it/bugs/bug1112940/AccumLiveValueAfterAbortIT.java
+++ src/test/resources/com/akiban/server/test/it/bugs/bug1112940/AccumLiveValueAfterAbortIT.java
@@ -26,9 +26,9 @@
 
 package com.akiban.server.test.it.bugs.bug1112940;
 
-import com.akiban.ais.model.UserTable;
 import com.akiban.server.TableStatus;
 import com.akiban.server.api.dml.ConstantColumnSelector;
+import com.akiban.server.api.dml.scan.NewRow;
 import com.akiban.server.error.DuplicateKeyException;
 import com.akiban.server.test.it.ITBase;
 import org.junit.Test;
@@ -36,55 +36,78 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+/**
+ * The cost estimator uses the live value from the row count accumulator
+ * as it is much cheaper than a (transactional) snapshot value. However, the
+ * live value is not adjusted if a modification is aborted.
+ *
+ * For no particular reason, the row count was bumped before we did any
+ * uniqueness checks in the write row processing. This caused an increasingly
+ * inaccurate gap for replication streams that use ON DUPLICATION UPDATE
+ * processing, which the server sees as INSERT, (error), UPDATE
+ */
 public class AccumLiveValueAfterAbortIT extends ITBase {
     private static final String SCHEMA = "test";
     private static final String TABLE = "t";
-
     private static final int ROW_COUNT = 2;
     private static final int UPDATE_COUNT = 10;
+    private static enum Op { ON_DUP_KEY_UPDATE, REPLACE }
 
-    /**
-     * The cost estimator uses the live value from the row count
-     * accumulator for making decisions as it is much cheaper
-     * than a (transactional) snapshot value. However, the live
-     * value is not adjusted if a modification is aborted.
-     *
-     * For no particular reason, the row count was bumped before
-     * we did any uniqueness checks in the write row processing.
-     * This caused an increasingly inaccurate gap for replication
-     * streams that use ON DUPLICATION UPDATE processing, which
-     * the server sees as INSERT (violation), UPDATE
-     */
-    @Test
-    public void doTest() {
-        int tid = createTable(SCHEMA, TABLE, "id int not null primary key, x int");
+    private int tid = -1;
 
+    private void createAndLoad() {
+        tid = createTable(SCHEMA, TABLE, "id int not null primary key, x int");
         for(int i = 0; i < ROW_COUNT; ++i) {
             writeRow(tid, i, 0);
         }
-
         expectRowCount(tid, ROW_COUNT);
+    }
 
-        final int UPDATE_ID = 1;
-        for(int i = 1; i <= UPDATE_COUNT; ++i) {
-            try {
-                writeRow(tid, UPDATE_ID, i);
-                fail("Expected DuplicateKeyException, loop " + i);
-            } catch(DuplicateKeyException e) {
-                // Expected
-            }
-
-            dml().updateRow(session(),
-                            createNewRow(tid, UPDATE_ID, i - 1),
-                            createNewRow(tid, UPDATE_ID, i - 1),
-                            ConstantColumnSelector.ALL_ON);
-        }
-
+    private void postCheck() {
         expectRowCount(tid, ROW_COUNT);
-
         // Approximate count doesn't have to match in general, but there
-        // is no reason for it to be off in this simple scenario
+        // is no reason for it to be off in these simple scenarios
         TableStatus tableStatus = getUserTable(tid).rowDef().getTableStatus();
         assertEquals("ApproximateRowCount", ROW_COUNT, tableStatus.getApproximateRowCount());
+    }
+
+    private void insertAs(Op op, int id, int x) {
+        NewRow newRow = createNewRow(tid, id, x);
+        try {
+            dml().writeRow(session(), newRow);
+            fail("Expected DuplicateKeyException");
+        } catch(DuplicateKeyException e) {
+            // Expected
+        }
+
+        NewRow oldRow = createNewRow(tid, id, x - 1);
+        if(op == Op.ON_DUP_KEY_UPDATE) {
+            dml().updateRow(session(), oldRow, newRow, ConstantColumnSelector.ALL_ON);
+        } else if(op == Op.REPLACE) {
+            dml().deleteRow(session(), oldRow);
+            dml().writeRow(session(), newRow);
+        } else {
+            fail("Unknown op: " + op);
+        }
+    }
+
+    @Test
+    public void onDuplicateKeyUpdate() {
+        createAndLoad();
+        final int UPDATE_ID = 1;
+        for(int i = 1; i <= UPDATE_COUNT; ++i) {
+            insertAs(Op.ON_DUP_KEY_UPDATE, UPDATE_ID, i);
+        }
+        postCheck();
+    }
+
+    @Test
+    public void replace() {
+        createAndLoad();
+        final int UPDATE_ID = 1;
+        for(int i = 1; i <= UPDATE_COUNT; ++i) {
+            insertAs(Op.REPLACE, UPDATE_ID, i);
+        }
+        postCheck();
     }
 }
