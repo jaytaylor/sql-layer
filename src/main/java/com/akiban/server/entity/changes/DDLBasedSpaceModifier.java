9ca47ed8ae46ee7b614af5316756409a32d8c4c1
Nathan Williams <nwilliams@akiban.com>
2013-02-13 15:25:15 -0500

    DDLFunctions based space modifier with add entity support

---
+++ src/main/java/com/akiban/server/entity/changes/DDLBasedSpaceModifier.java
@@ -0,0 +1,151 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.entity.changes;
+
+import com.akiban.ais.model.GroupIndex;
+import com.akiban.ais.model.NopVisitor;
+import com.akiban.ais.model.TableName;
+import com.akiban.ais.model.UserTable;
+import com.akiban.server.api.DDLFunctions;
+import com.akiban.server.entity.model.Attribute;
+import com.akiban.server.entity.model.Entity;
+import com.akiban.server.entity.model.EntityIndex;
+import com.akiban.server.entity.model.Space;
+import com.akiban.server.entity.model.Validation;
+import com.akiban.server.service.dxl.DXLService;
+import com.akiban.server.service.session.Session;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class DDLBasedSpaceModifier implements SpaceModificationHandler {
+    private final DDLFunctions ddlFunctions;
+    private final Session session;
+    private final String schemaName;
+    private final Space space;
+    private final SpaceLookups spaceLookups;
+
+    public DDLBasedSpaceModifier(DDLFunctions ddlFunctions, Session session, String schemaName, Space space) {
+        this.ddlFunctions = ddlFunctions;
+        this.session = session;
+        this.schemaName = schemaName;
+        this.space = space;
+        this.spaceLookups = new SpaceLookups(space);
+    }
+
+    @Override
+    public void addEntity(UUID entityUuid) {
+        Entity entity = spaceLookups.getEntity(entityUuid);
+        String entityName = spaceLookups.getName(entityUuid);
+
+        EntityToAIS visitor = new EntityToAIS(schemaName);
+        entity.accept(entityName, visitor);
+
+        UserTable root = visitor.getAIS().getUserTable(schemaName, entityName);
+        root.traverseTableAndDescendants(new NopVisitor() {
+            @Override
+            public void visitUserTable(UserTable table) {
+                ddlFunctions.createTable(session, table);
+            }
+        });
+
+        Collection<GroupIndex> groupIndexes = root.getGroup().getIndexes();
+        if(!groupIndexes.isEmpty()) {
+            ddlFunctions.createIndexes(session, groupIndexes);
+        }
+    }
+
+    @Override
+    public void dropEntity(Entity dropped) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void renameEntity(UUID entityUuid, String oldName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addAttribute(UUID attributeUuid) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void dropAttribute(Attribute dropped) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void renameAttribute(UUID attributeUuid, String oldName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void changeScalarType(UUID scalarUuid, Attribute afterChange) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void changeScalarValidations(UUID scalarUuid, Attribute afterChange) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void changeScalarProperties(UUID scalarUuid, Attribute afterChange) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addEntityValidation(Validation validation) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void dropEntityValidation(Validation validation) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addIndex(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void dropIndex(String name, EntityIndex index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void renameIndex(EntityIndex index, String oldName, String newName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void error(String message) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/com/akiban/server/entity/changes/DDLBasedSpaceModifierIT.java
@@ -0,0 +1,102 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.server.entity.changes;
+
+import com.akiban.junit.NamedParameterizedRunner;
+import com.akiban.junit.Parameterization;
+import com.akiban.server.entity.model.Space;
+import com.akiban.server.test.it.ITBase;
+import com.akiban.util.JUnitUtils;
+import com.akiban.util.Strings;
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Load a space diff, execute the DDLBasedSpaceModifier, and compare the resulting AIS.
+ *
+ * Input files: x-orig.json, x-update.json
+ * Expected files: x-update.ais
+ */
+@RunWith(NamedParameterizedRunner.class)
+public final class DDLBasedSpaceModifierIT extends ITBase {
+    private static final String SCHEMA = "test";
+    private static final String ORIG_SUFFIX = "-orig.json";
+    private static final String UPDATE_SUFFIX = "-update.json";
+    private static final String EXPECTED_SUFFIX = "-update.ais";
+
+    private static String getShortName(String testName) {
+        return testName.substring(0, testName.length() - ORIG_SUFFIX.length());
+    }
+
+    @NamedParameterizedRunner.TestParameters
+    public static Collection<Parameterization> params() throws IOException {
+        String[] testNames = JUnitUtils.getContainingFile(DDLBasedSpaceModifierIT.class).list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(ORIG_SUFFIX) &&
+                       new File(dir, getShortName(name) + EXPECTED_SUFFIX).exists();
+            }
+        });
+        return Collections2.transform(Arrays.asList(testNames), new Function<String, Parameterization>() {
+            @Override
+            public Parameterization apply(String testName) {
+                String shortName = getShortName(testName);
+                return new Parameterization(shortName, true, shortName);
+            }
+        });
+    }
+
+    @Test
+    public void test() throws IOException {
+        Space origSpace = Space.readSpace(testName + ORIG_SUFFIX, DDLBasedSpaceModifierIT.class);
+        Space updateSpace = Space.readSpace(testName + UPDATE_SUFFIX, DDLBasedSpaceModifierIT.class);
+        SpaceDiff diff = new SpaceDiff(origSpace, updateSpace);
+
+        diff.apply(new DDLBasedSpaceModifier(ddl(), session(), SCHEMA, updateSpace));
+
+        String expected = Strings.dumpFileToString(new File(dir, testName + EXPECTED_SUFFIX));
+        String actual = AISDumper.dumpDeterministicAIS(ais(), SCHEMA);
+        assertEquals("Generated AIS", expected.trim(), actual.trim());
+    }
+
+    public DDLBasedSpaceModifierIT(String testName) {
+        this.testName = testName;
+    }
+
+    private final String testName;
+    private static final File dir = JUnitUtils.getContainingFile(DDLBasedSpaceModifierIT.class);
+}
