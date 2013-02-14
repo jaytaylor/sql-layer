d59e2252356e445564a56f76ae71c888c9d49347
Yuval Shavit <yshavit@akiban.com>
2013-02-14 17:34:25 -0500

    fixing TParser for DECIMAL

--- src/main/java/com/akiban/server/types3/TParsers.java
+++ src/main/java/com/akiban/server/types3/TParsers.java
@@ -27,7 +27,9 @@
 package com.akiban.server.types3;
 
 import com.akiban.server.error.InvalidDateFormatException;
+import com.akiban.server.types3.common.types.TString;
 import com.akiban.server.types3.mcompat.mcasts.CastUtils;
+import com.akiban.server.types3.mcompat.mtypes.MBigDecimal;
 import com.akiban.server.types3.mcompat.mtypes.MBigDecimalWrapper;
 import com.akiban.server.types3.mcompat.mtypes.MDatetimes;
 import com.akiban.server.types3.pvalue.PValueSource;
@@ -213,9 +215,19 @@ public class TParsers
         @Override
         public void parse(TExecutionContext context, PValueSource source, PValueTarget target)
         {
-            CastUtils.doCastDecimal(context,
-                                    CastUtils.parseDecimalString(source.getString(),context),
-                                    target);
+            TInstance tInstance = source.tInstance();
+            if (tInstance.typeClass() instanceof TString) {
+                CastUtils.doCastDecimal(
+                        context, CastUtils.parseDecimalString(source.getString(),context), target);
+            }
+            else if (tInstance.typeClass() instanceof MBigDecimal) {
+                if (source.hasCacheValue())
+                    target.putObject(source.getObject());
+                else
+                    target.putBytes(source.getBytes());
+            }
+            else
+                throw new AssertionError("wrong class for DECIMAL type: " + tInstance);
         }   
     };
 
