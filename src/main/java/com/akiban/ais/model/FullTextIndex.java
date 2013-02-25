6f6949f831cb69dcda90f8848f77c0f22a64ced3
Mike McMahon <mmcm@comcast.net>
2013-02-24 19:53:02 -0500

    Begin new Index subclass.

---
+++ src/main/java/com/akiban/ais/model/FullTextIndex.java
@@ -0,0 +1,107 @@
+/**
+ * END USER LICENSE AGREEMENT (“EULA”)
+ *
+ * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
+ * http://www.akiban.com/licensing/20110913
+ *
+ * BY INSTALLING OR USING ALL OR ANY PORTION OF THE SOFTWARE, YOU ARE ACCEPTING
+ * ALL OF THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU AGREE THAT THIS
+ * AGREEMENT IS ENFORCEABLE LIKE ANY WRITTEN AGREEMENT SIGNED BY YOU.
+ *
+ * IF YOU HAVE PAID A LICENSE FEE FOR USE OF THE SOFTWARE AND DO NOT AGREE TO
+ * THESE TERMS, YOU MAY RETURN THE SOFTWARE FOR A FULL REFUND PROVIDED YOU (A) DO
+ * NOT USE THE SOFTWARE AND (B) RETURN THE SOFTWARE WITHIN THIRTY (30) DAYS OF
+ * YOUR INITIAL PURCHASE.
+ *
+ * IF YOU WISH TO USE THE SOFTWARE AS AN EMPLOYEE, CONTRACTOR, OR AGENT OF A
+ * CORPORATION, PARTNERSHIP OR SIMILAR ENTITY, THEN YOU MUST BE AUTHORIZED TO SIGN
+ * FOR AND BIND THE ENTITY IN ORDER TO ACCEPT THE TERMS OF THIS AGREEMENT. THE
+ * LICENSES GRANTED UNDER THIS AGREEMENT ARE EXPRESSLY CONDITIONED UPON ACCEPTANCE
+ * BY SUCH AUTHORIZED PERSONNEL.
+ *
+ * IF YOU HAVE ENTERED INTO A SEPARATE WRITTEN LICENSE AGREEMENT WITH AKIBAN FOR
+ * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
+ * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
+ */
+
+package com.akiban.ais.model;
+
+import com.akiban.ais.model.validation.AISInvariants;
+import com.akiban.server.error.BranchingGroupIndexException;
+import com.akiban.server.error.IndexColNotInGroupException;
+
+import java.util.*;
+
+public class FullTextIndex extends Index
+{
+    /* Index */
+
+    @Override
+    public HKey hKey() {
+        return indexedTable.hKey();
+    }
+
+    @Override
+    public boolean isTableIndex() {
+        return false;
+    }
+
+    @Override
+    public void computeFieldAssociations(Map<Table,Integer> ordinalMap) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Table leafMostTable() {
+        // This is not entirely well-defined, since more than one
+        // descendant to the same depth can be indexed.
+        UserTable deepest = null;
+        for (IndexColumn indexColumn : keyColumns) {
+            if ((deepest == null) || 
+                (indexColumn.getColumn().getUserTable().getDepth() > deepest.getDepth())) {
+                deepest = indexColumn.getColumn().getUserTable();
+            }
+        }
+        return deepest;
+    }
+
+    @Override
+    public Table rootMostTable() {
+        UserTable shallowest = null;
+        for (IndexColumn indexColumn : keyColumns) {
+            if ((shallowest == null) || 
+                (indexColumn.getColumn().getUserTable().getDepth() < shallowest.getDepth())) {
+                shallowest = indexColumn.getColumn().getUserTable();
+            }
+        }
+        return shallowest;
+    }
+
+    @Override
+    public void checkMutability() {
+        indexedTable.checkMutability();
+    }
+
+    @Override
+    public Collection<Integer> getAllTableIDs() {
+        Set<Integer> ids = new HashSet<>();
+        for (IndexColumn indexColumn : keyColumns) {
+            ids.add(indexColumn.getColumn().getTable().getTableId());
+        }
+        return ids;
+    }
+
+    /* FullTextIndex */
+
+    public UserTable getIndexedTable() {
+        return indexedTable;
+    }
+
+    public FullTextIndex(UserTable indexedTable, String indexName, Integer indexId, Boolean isUnique, String constraint)
+    {
+        super(indexedTable.getName(), indexName, indexId, isUnique, constraint);
+        this.indexedTable = indexedTable;
+    }
+    
+    private final UserTable indexedTable;
+}
