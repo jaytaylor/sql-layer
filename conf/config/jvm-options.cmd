8c1fdd9cb64e7d7fcb1e67b893e50780d74daf76
Nathan Williams <nwilliams@akiban.com>
2013-03-22 16:05:57 -0400

    Add new license headers

--- conf/config/jvm-options.cmd
+++ conf/config/jvm-options.cmd
@@ -1,3 +1,20 @@
+REM 
+REM Copyright (C) 2009-2013 Akiban Technologies, Inc.
+REM 
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU Affero General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM 
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Affero General Public License for more details.
+REM 
+REM You should have received a copy of the GNU Affero General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+REM 
+
 REM This file is for Windows. For Unix-like systems, look in jvm.options.
 REM It is important that none of the property values have spaces in them.
 
