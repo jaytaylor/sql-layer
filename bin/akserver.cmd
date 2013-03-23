b48c73dd79ccbec9cfc0f46c2124eba7af1698c1
Nathan Williams <nwilliams@akiban.com>
2013-03-23 16:29:20 -0400

    Release 1.6.0

--- bin/akserver.cmd
+++ bin/akserver.cmd
@@ -19,7 +19,7 @@
 
 SETLOCAL
 
-SET SERVER_JAR=akiban-server-1.5.2-SNAPSHOT.jar
+SET SERVER_JAR=akiban-server-1.6.0.jar
 SET SERVICE_NAME=akserver
 SET SERVICE_DNAME=Akiban Server
 SET SERVICE_DESC=Akiban Database Server
