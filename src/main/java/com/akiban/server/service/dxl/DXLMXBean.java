7f47d506849c6cd81a8bd1fe639cbc4e58e5a3d2
Nathan Williams <nwilliams@akiban.com>
2013-06-11 10:33:30 -0400

    Remove DDL JMX helpers

--- src/main/java/com/akiban/server/service/dxl/DXLMXBean.java
+++ src/main/java/com/akiban/server/service/dxl/DXLMXBean.java
@@ -17,36 +17,11 @@
 
 package com.akiban.server.service.dxl;
 
-import com.akiban.ais.model.Index;
-import com.akiban.ais.model.TableName;
-
-import java.util.List;
-
 @SuppressWarnings("unused") // jmx
 public interface DXLMXBean {
-    String getUsingSchema();
-
-    void setUsingSchema(String schema);
-
-    void createGroupIndex(String schemaName, String groupName, String indexName, String tableColumnList, Index.JoinType joinType);
-
-    void dropTable(String tableName) ;
-
-    void dropGroupIndex(String schemaName, String groupName, String indexName) ;
-
     void dropGroup(String schemaName, String groupName) ;
     
-    void dropGroupBySchema(String schemaName) ;
-
     void dropAllGroups() ;
-
-    void writeRow(String table, String fields) ;
-
-    List<String> getGrouping();
-
-    List<String> getGroupIndexDDLs();
-
-    TableName getGroupNameFromTableName(String schema, String table);
 
     String printAIS();
 }
