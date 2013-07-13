3498c741ff1cdf74705599f133b35311c43ca193
Nathan Williams <nwilliams@akiban.com>
2013-07-13 14:22:40 -0400

    Add treeExists() to Store interface and use for DropTreesIT

--- src/main/java/com/akiban/server/store/PersistitStore.java
+++ src/main/java/com/akiban/server/store/PersistitStore.java
@@ -552,6 +552,11 @@ public class PersistitStore extends AbstractStore<Exchange> implements Service
     }
 
     @Override
+    public boolean treeExists(Session session, String schemaName, String treeName) {
+        return treeService.treeExists(schemaName, treeName);
+    }
+
+    @Override
     public void truncateTree(Session session, TreeLink treeLink) {
         Exchange iEx = treeService.getExchange(session, treeLink);
         try {
--- src/main/java/com/akiban/server/store/Store.java
+++ src/main/java/com/akiban/server/store/Store.java
@@ -130,6 +130,8 @@ public interface Store extends KeyCreator {
 
     StoreAdapter createAdapter(Session session, Schema schema);
 
+    boolean treeExists(Session session, String schemaName, String treeName);
+
     // TODO: Better abstraction
     void traverse(Session session, Group group, TreeRecordVisitor visitor);
     <V extends IndexVisitor<Key,Value>> V traverse(Session session, Index index, V visitor);
