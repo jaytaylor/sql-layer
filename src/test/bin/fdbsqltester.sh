97223b249fefa3c7e25f7b3b8464afadf4d6d0d7
Nathan Williams <nathan.williams@foundationdb.com>
2013-10-01 15:41:33 -0400

    Release 1.9.0

--- src/test/bin/fdbsqltester.sh
+++ src/test/bin/fdbsqltester.sh
@@ -17,5 +17,5 @@
 #
 
 TARGET=$(ls -d $(dirname $0)/../../../target)
-BASEJAR=$(ls ${TARGET}/fdb-sql-layer-*.*.*-SNAPSHOT.jar)
+BASEJAR=$(ls ${TARGET}/fdb-sql-layer-*.*.*.jar)
 java -cp "${BASEJAR}:${BASEJAR%.jar}-tests.jar:${TARGET}/dependency/*" com.foundationdb.sql.test.Tester "$@"
