23388f52f1c22dafea315f8e013023bd4cf2fc96
tjoneslo <thomas.jones-low@foundationdb.com>
2013-10-23 12:26:12 -0400

    Remove out-dated TODO comment.

--- src/main/java/com/foundationdb/sql/optimizer/rule/OperatorAssembler.java
+++ src/main/java/com/foundationdb/sql/optimizer/rule/OperatorAssembler.java
@@ -1391,10 +1391,6 @@ public class OperatorAssembler extends BaseRule
             RowStream leftStream = assembleStream (left);
             RowStream rightStream = assembleStream (right);
             
-            // TOOD: Replace the expressions in the projectDefault at the top 
-            // of both streams with casts to the common row type.
-            
-            
             if (union.isAll()) {
                 leftStream.operator = 
                     API.unionAll_Default(leftStream.operator, leftStream.rowType, 
