e50a335a35495eb20e8f872cf31523e3c8b33954
Thomas Jones-Low <tjoneslow@gmail.com>
2013-11-11 17:51:23 -0500

    Update the read from statementCache to be sensitive to the parameter OID values requested by the client. That is, sending the same statement text with different parameter values results in a different statement in the cache.

--- src/main/java/com/foundationdb/sql/pg/PostgresServerConnection.java
+++ src/main/java/com/foundationdb/sql/pg/PostgresServerConnection.java
@@ -718,6 +718,27 @@ public class PostgresServerConnection extends ServerSessionBase
         PostgresStatement pstmt = null;
         if (statementCache != null)
             pstmt = statementCache.get(sql);
+
+        // Verify the parameter types from the parse request match the
+        // parameter requests from our potential cached statement
+        // if they don't match, assume the statement isn't a match
+        if (pstmt != null) {
+            if (pstmt.getParameterTypes() == null) {
+                if (nparams > 0) {
+                    pstmt = null;
+                }
+            } else  if (pstmt.getParameterTypes().length != nparams) {
+                pstmt = null;
+            } else {
+                for (int i = 0; i < nparams; i++ ) {
+                    if (pstmt.getParameterTypes()[i].getOid() != paramTypes[i]) {
+                        pstmt = null;
+                        break;
+                    }
+                }
+            }
+        }
+
         if (pstmt == null) {
             for (PostgresStatementParser parser : unparsedGenerators) {
                 pstmt = parser.parse(this, sql, null);
@@ -727,6 +748,8 @@ public class PostgresServerConnection extends ServerSessionBase
                 }
             }
         }
+        
+        
         if (pstmt == null) {
             StatementNode stmt;
             List<ParameterNode> params;
