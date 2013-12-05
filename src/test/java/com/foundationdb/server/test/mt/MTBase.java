8bc5107595017f5cc708a26e5cc2ee3e42d2977f
Nathan Williams <nathan.williams@foundationdb.com>
2013-12-04 22:43:11 -0500

    Move common helpers to base

--- src/test/java/com/foundationdb/server/test/mt/MTBase.java
+++ src/test/java/com/foundationdb/server/test/mt/MTBase.java
@@ -17,6 +17,8 @@
 
 package com.foundationdb.server.test.mt;
 
+import com.foundationdb.ais.model.Index;
+import com.foundationdb.qp.expression.IndexKeyRange;
 import com.foundationdb.qp.operator.API;
 import com.foundationdb.qp.operator.Cursor;
 import com.foundationdb.qp.operator.Operator;
@@ -24,9 +26,14 @@ import com.foundationdb.qp.operator.QueryBindings;
 import com.foundationdb.qp.operator.QueryContext;
 import com.foundationdb.qp.operator.SimpleQueryContext;
 import com.foundationdb.qp.operator.StoreAdapter;
+import com.foundationdb.qp.operator.UpdateFunction;
 import com.foundationdb.qp.row.Row;
+import com.foundationdb.qp.rowtype.IndexRowType;
+import com.foundationdb.qp.rowtype.RowType;
 import com.foundationdb.qp.rowtype.Schema;
+import com.foundationdb.qp.rowtype.TableRowType;
 import com.foundationdb.qp.util.SchemaCache;
+import com.foundationdb.server.api.DDLFunctions;
 import com.foundationdb.server.service.session.Session;
 import com.foundationdb.server.service.transaction.TransactionService;
 import com.foundationdb.server.store.SchemaManager;
@@ -38,6 +45,12 @@ import com.foundationdb.server.test.mt.util.ServiceHolder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+
+import static com.foundationdb.qp.operator.API.delete_Returning;
+import static com.foundationdb.qp.operator.API.groupScan_Default;
+import static com.foundationdb.qp.operator.API.insert_Returning;
+import static com.foundationdb.qp.operator.API.update_Returning;
+import static com.foundationdb.qp.operator.API.valuesScan_Default;
 
 // Extend ITBase for the miscellaneous Row/Operator test helpers
 public abstract class MTBase extends ITBase implements ServiceHolder
@@ -57,7 +70,7 @@ public abstract class MTBase extends ITBase implements ServiceHolder
                 Schema schema = SchemaCache.globalSchema(ais());
                 Operator plan = creator.create(schema);
                 StoreAdapter adapter = store().createAdapter(session(), schema);
-                QueryContext context = new SimpleQueryContext(adapter);
+                QueryContext context = new SimpleQueryContext(adapter, serviceManager());
                 QueryBindings bindings = context.createBindings();
                 List<Row> rows = new ArrayList<>();
                 Cursor cursor = API.cursor(plan, context, bindings);
@@ -73,6 +86,117 @@ public abstract class MTBase extends ITBase implements ServiceHolder
                 return rows;
             }
         });
+    }
+
+    protected static List<Row> remove(List<Row> rows, int index) {
+        return remove(rows, rows.get(index));
+    }
+
+    protected static List<Row> remove(List<Row> rows, Row row) {
+        List<Row> newRows = new ArrayList<>(rows);
+        newRows.remove(row);
+        return newRows;
+    }
+
+    protected static List<Row> combine(List<Row> rows, Row newRow) {
+        List<Row> newRows = new ArrayList<>(rows);
+        newRows.add(newRow);
+        return newRows;
+    }
+
+    protected static List<Row> insert(List<Row> rows, int index, Row newRow) {
+        List<Row> newRows = new ArrayList<>(rows);
+        newRows.add(index, newRow);
+        return newRows;
+    }
+
+    protected static List<Row> replace(List<Row> rows, int index, Row newRow) {
+        List<Row> newRows = new ArrayList<>(rows);
+        newRows.set(index, newRow);
+        return newRows;
+    }
+
+    protected static OperatorCreator groupScanCreator(final int tID) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                return groupScan_Default(schema.tableRowType(tID).table().getGroup());
+            }
+        };
+    }
+
+    protected static OperatorCreator indexScanCreator(final int tID, final int iID) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                return API.indexScan_Default(schema.tableRowType(tID).indexRowType(iID));
+            }
+        };
+    }
+
+    protected static OperatorCreator indexScanCreator(final int tID, final String indexName) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                TableRowType tableRowType = schema.tableRowType(tID);
+                Index index = tableRowType.table().getIndex(indexName);
+                return API.indexScan_Default(tableRowType.indexRowType(index));
+            }
+        };
+    }
+
+    protected static OperatorCreator groupIndexScanCreator(final int tID, final String indexName) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                TableRowType tableType = schema.tableRowType(tID);
+                Index index = schema.tableRowType(tID).table().getGroup().getIndex(indexName);
+                IndexRowType indexType = schema.indexRowType(index);
+                return API.indexScan_Default(indexType, false, IndexKeyRange.unbounded(indexType), tableType);
+            }
+        };
+    }
+
+    protected static OperatorCreator insertCreator(final int tID, final Row newRow) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                RowType rowType = schema.tableRowType(tID);
+                return insert_Returning(valuesScan_Default(bindableRows(newRow), rowType));
+            }
+        };
+    }
+
+    protected static OperatorCreator updateCreator(final int tID, final Row oldRow, final Row newRow) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                RowType rowType = schema.tableRowType(tID);
+                return update_Returning(
+                    valuesScan_Default(bindableRows(oldRow), rowType),
+                    new UpdateFunction() {
+                        @Override
+                        public Row evaluate(Row original, QueryContext context, QueryBindings bindings) {
+                            return newRow;
+                        }
+
+                        @Override
+                        public boolean rowIsSelected(Row row) {
+                            return row.value(0).getInt32() == oldRow.value(0).getInt32();
+                        }
+                    });
+            }
+        };
+    }
+
+    protected static OperatorCreator deleteCreator(final int tID, final Row row) {
+        return new OperatorCreator() {
+            @Override
+            public Operator create(Schema schema) {
+                RowType rowType = schema.tableRowType(tID);
+                return delete_Returning(valuesScan_Default(bindableRows(row), rowType), false);
+            }
+        };
     }
 
 
@@ -93,6 +217,11 @@ public abstract class MTBase extends ITBase implements ServiceHolder
     @Override
     public Store getStore() {
         return store();
+    }
+
+    @Override
+    public DDLFunctions getDDLFunctions() {
+        return ddl();
     }
 
     @Override
