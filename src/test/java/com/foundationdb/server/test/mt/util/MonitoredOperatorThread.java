7cf2896be10eadb96dff4bd68dc60201184faec4
Nathan Williams <nathan.williams@foundationdb.com>
2013-12-04 22:40:00 -0500

    Factor out common base for monitored thread, add DDL thread

--- src/test/java/com/foundationdb/server/test/mt/util/MonitoredOperatorThread.java
+++ src/test/java/com/foundationdb/server/test/mt/util/MonitoredOperatorThread.java
@@ -29,26 +29,14 @@ import com.foundationdb.qp.rowtype.Schema;
 import com.foundationdb.qp.util.SchemaCache;
 import com.foundationdb.server.service.session.Session;
 import com.foundationdb.server.test.mt.util.ThreadMonitor.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class MonitoredOperatorThread extends Thread implements HasTimeMarker
+public class MonitoredOperatorThread extends MonitoredThread
 {
-    private static final Logger LOG = LoggerFactory.getLogger(MonitoredOperatorThread.class);
-
-    private final ServiceHolder services;
-    private final ThreadMonitor monitor;
     private final OperatorCreator operatorCreator;
-    private final Set<Stage> stageMarks;
-
-    private final List<Row> scannedRows;
-    private final TimeMarker timeMarker;
-    private Stage curStage = null;
-    private Throwable failure = null;
 
 
     public MonitoredOperatorThread(String name,
@@ -56,126 +44,65 @@ public class MonitoredOperatorThread extends Thread implements HasTimeMarker
                                    OperatorCreator operatorCreator,
                                    ThreadMonitor monitor,
                                    Set<Stage> stageMarks) {
-        super(name);
-        this.services = services;
+        super(name, services, monitor, stageMarks);
         this.operatorCreator = operatorCreator;
-        this.monitor = monitor;
-        this.stageMarks = stageMarks;
-        this.scannedRows = new ArrayList<>();
-        this.timeMarker = new TimeMarker();
     }
 
     //
-    // MonitoredPlanRunable
-    //
-
-    public List<Row> getScannedRows() {
-        return scannedRows;
-    }
-
-    public boolean hadFailure() {
-        return (failure != null);
-    }
-
-    public Throwable getFailure() {
-        return failure;
-    }
-
-    //
-    // HasTimeMarker
+    // MonitoredThread
     //
 
     @Override
-    public TimeMarker getTimeMarker() {
-        return timeMarker;
+    protected boolean doRetryOnRollback() {
+        return true;
     }
-
-    //
-    // Runnable
-    //
 
     @Override
-    public void run() {
-        to(Stage.START);
-        try(Session session = services.createSession()) {
-            runInternal(session);
-        } catch(Throwable t) {
-            wrap(t);
-        } finally {
-            to(Stage.FINISH);
-        }
+    protected boolean doRetryOnTableVersionChange() {
+        return false;
     }
 
-    //
-    // Internal
-    //
-
-    private void runInternal(Session session) {
-
+    @Override
+    protected void runInternal(Session session) {
         to(Stage.PRE_BEGIN);
         boolean success = false;
-        services.getTransactionService().beginTransaction(session);
+        getServiceHolder().getTransactionService().beginTransaction(session);
         to(Stage.POST_BEGIN);
-
         try {
-            to(Stage.PRE_SCAN);
-            Schema schema = SchemaCache.globalSchema(services.getSchemaManager().getAis(session));
-            Operator plan = operatorCreator.create(schema);
-            StoreAdapter adapter = services.getStore().createAdapter(session, schema);
-            QueryContext context = new SimpleQueryContext(adapter);
-            QueryBindings bindings = context.createBindings();
-            Cursor cursor = API.cursor(plan, context, bindings);
-            cursor.openTopLevel();
-            try {
-                Row row;
-                while((row = cursor.next()) != null) {
-                    scannedRows.add(row);
-                    switch(scannedRows.size()) {
-                        case 1: to(Stage.SCAN_FIRST_ROW); break;
-                        case 2: to(Stage.SCAN_SECOND_ROW); break;
+            while(!success) {
+                to(Stage.PRE_SCAN);
+                Schema schema = SchemaCache.globalSchema(getServiceHolder().getSchemaManager().getAis(session));
+                Operator plan = operatorCreator.create(schema);
+                StoreAdapter adapter = getServiceHolder().getStore().createAdapter(session, schema);
+                QueryContext context = new SimpleQueryContext(adapter);
+                QueryBindings bindings = context.createBindings();
+                Cursor cursor = API.cursor(plan, context, bindings);
+                cursor.openTopLevel();
+                try {
+                    Row row;
+                    while((row = cursor.next()) != null) {
+                        getScannedRows().add(row);
+                        switch(getScannedRows().size()) {
+                            case 1: to(Stage.SCAN_FIRST_ROW); break;
+                            case 2: to(Stage.SCAN_SECOND_ROW); break;
+                        }
                     }
+                } finally {
+                    cursor.closeTopLevel();
                 }
-            } finally {
-                cursor.closeTopLevel();
-            }
-            to(Stage.POST_SCAN);
+                to(Stage.POST_SCAN);
 
-            to(Stage.PRE_COMMIT);
-            services.getTransactionService().commitTransaction(session);
-            success = true;
-            to(Stage.POST_COMMIT);
+                to(Stage.PRE_COMMIT);
+                getServiceHolder().getTransactionService().commitTransaction(session);
+                success = true;
+                to(Stage.POST_COMMIT);
+            }
         } finally {
             if(!success) {
                 to(Stage.PRE_ROLLBACK);
-                services.getTransactionService().rollbackTransaction(session);
+                getServiceHolder().getTransactionService().rollbackTransactionIfOpen(session);
                 to(Stage.POST_ROLLBACK);
             }
         }
-    }
-
-    private void to(Stage stage) {
-        try {
-            monitor.at(stage);
-            LOG.trace("to: {}", stage);
-            if(stageMarks.contains(stage)) {
-                timeMarker.mark(getName() + ":" + stage.name());
-            }
-            curStage = stage;
-        } catch(Throwable t) {
-            wrap(t);
-        }
-    }
-
-    private void wrap(Throwable t) {
-        LOG.debug("wrapping", t);
-        assert (failure == null) : failure;
-        failure = t;
-        if(t instanceof Error) {
-            throw (Error)t;
-        }
-        if(t instanceof RuntimeException) {
-            throw (RuntimeException)t;
-        }
-        throw new RuntimeException(t);
     }
 }
