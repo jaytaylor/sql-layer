c6cad7fa9e5f203e054b538e87dd849704facce0
Mike McMahon <mmcm@comcast.net>
2013-12-13 18:38:06 -0500

    Get rid of AkType in Type.

--- src/main/java/com/foundationdb/ais/protobuf/ProtobufReader.java
+++ src/main/java/com/foundationdb/ais/protobuf/ProtobufReader.java
@@ -113,7 +113,7 @@ public class ProtobufReader {
                     pbType.getMaxSizeBytes(),
                     null,
                     null,
-                    null
+                    false
             );
         }
     }
