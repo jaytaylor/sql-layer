bc8372541d328c34ce351355cf5bb0427dd5e0c7
Nathan Williams <nathan.williams@foundationdb.com>
2013-12-29 19:13:58 -0500

    Move tree methods to PersistitNameGenerator and remove Synchronized wrapper

--- src/main/java/com/foundationdb/ais/model/NameGenerator.java
+++ src/main/java/com/foundationdb/ais/model/NameGenerator.java
@@ -20,31 +20,23 @@ package com.foundationdb.ais.model;
 import java.util.List;
 import java.util.Set;
 
+/** NB: Used concurrently, synchronize implementations as appropriate. */
 public interface NameGenerator
 {
-    // To allow downcasting.
-    NameGenerator unwrap();
-
     // Generation
     int generateTableID(TableName name);
     int generateIndexID(int rootTableID);
     TableName generateIdentitySequenceName(TableName table);
     String generateJoinName(TableName parentTable, TableName childTable, List<JoinColumn> joinIndex);
     String generateJoinName(TableName parentTable, TableName childTable, List<String> pkColNames, List<String> fkColNames);
-    String generateIndexTreeName(Index index);
-    String generateGroupTreeName(String schemaName, String groupName);
-    String generateSequenceTreeName(Sequence sequence);
-
-    // External generation. Still required to be unique.
-    void generatedTreeName(String treeName);
+    String generateFullTextIndexPath(FullTextIndex index);
 
     // Bulk add
     void mergeAIS(AkibanInformationSchema ais);
 
     // Removal
     void removeTableID(int tableID);
-    void removeTreeName(String treeName);
 
     // View only (debug/testing)
-    Set<String> getTreeNames();
+    Set<String> getStorageNames();
 }
