6b8359b76caad7a50ece263d11848972d25b352e
SytzeH <sytze.harkema@foundationdb.com>
2014-04-17 15:45:29 -0400

    Alligned behavior from functions: space(), repeat(), lpad, and rpad

--- src/main/java/com/foundationdb/server/types/mcompat/mfuncs/MPad.java
+++ src/main/java/com/foundationdb/server/types/mcompat/mfuncs/MPad.java
@@ -25,5 +25,5 @@ import com.foundationdb.server.types.mcompat.mtypes.MString;
 public class MPad
 {
     public static final TScalar[] INSTANCES
-            = Pad.create(MString.VARCHAR, MNumeric.INT, MString.LONGTEXT);
+            = Pad.create(MString.VARCHAR, MNumeric.INT);
 }
