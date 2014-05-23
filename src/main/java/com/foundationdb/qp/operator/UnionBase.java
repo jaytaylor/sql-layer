c310b2f5ef94994d3104646cb99c0e616e3842c2
jerett <jerett.sierad@foundationdb.com>
2014-05-23 17:14:18 -0400

    SetOperatorBase Class
    Added base class for all set operations which UnionBase now depends on
    This new base class will be used for the forthcoming Intersect and
    Except operators

--- src/main/java/com/foundationdb/qp/operator/UnionBase.java
+++ src/main/java/com/foundationdb/qp/operator/UnionBase.java
@@ -17,69 +17,16 @@
 
 package com.foundationdb.qp.operator;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
 import com.foundationdb.qp.rowtype.RowType;
-import com.foundationdb.server.error.SetWrongNumColumns;
-import com.foundationdb.server.error.SetWrongTypeColumns;
-import com.foundationdb.server.types.TInstance;
-import com.foundationdb.util.ArgumentValidation;
 import com.foundationdb.util.Strings;
-import com.google.common.base.Objects;
 
 
-public abstract class UnionBase extends Operator {
+public abstract class UnionBase extends SetOperatorBase {
 
-    UnionBase (Operator left, RowType leftType, Operator right, RowType rightType) {
-        ArgumentValidation.notNull("left", left);
-        ArgumentValidation.notNull("right", right);
-        ArgumentValidation.notNull("leftRowType", leftType);
-        ArgumentValidation.notNull("rightRowType", rightType);
-        
-        if (leftType.nFields() != rightType.nFields()) {
-            throw new SetWrongNumColumns (leftType.nFields(), rightType.nFields());
-        }
-        this.outputRowType = rowType(leftType, rightType);
-        overlayRow = !(outputRowType == leftType);
-        
-        this.inputs = Arrays.asList(left, right);
-        this.inputTypes = Arrays.asList(leftType, rightType);
-        ArgumentValidation.isEQ("inputs.size", inputs.size(), "inputTypes.size", inputTypes.size());
-    }
-    
-    private RowType outputRowType;
-    private boolean overlayRow = false;
-    private final List<? extends Operator> inputs;
-    private final List<? extends RowType> inputTypes;
-
-    @Override
-    public RowType rowType() {
-        return outputRowType;
+    UnionBase(Operator left, RowType leftType, Operator right, RowType rightType){
+        super( left,  leftType,  right,  rightType);
     }
 
-    @Override
-    public List<Operator> getInputOperators() {
-        return Collections.unmodifiableList(inputs);
-    }
-    
-    public List<RowType> getInputTypes() {
-        return Collections.unmodifiableList(inputTypes);
-    }
-    @Override
-    public void findDerivedTypes(Set<RowType> derivedTypes)
-    {
-        for (Operator oper : inputs) {
-            oper.findDerivedTypes(derivedTypes);
-        }
-    }
-
-    public boolean useOverlayRow() {
-        return overlayRow;
-    }
-    
     @Override
     public String describePlan() {
         StringBuilder sb = new StringBuilder();
@@ -90,59 +37,5 @@ public abstract class UnionBase extends Operator {
                 sb.append(Strings.nl()).append("UNION").append(Strings.nl());
         }
         return sb.toString();
-    }
-
-    
-    Operator left() { 
-        return inputs.get(0); 
-    }
-    
-    Operator right() { 
-        return inputs.get(1); 
-    }
-    
-    Operator operator(int i) { 
-        return inputs.get(i);
-    }
-
-    RowType inputRowType (int i) {
-        return inputTypes.get(i);
-    }
-    
-    int getInputSize() {
-        return inputs.size();
-    }
-    // for use in this package (in ctor and unit tests)
-
-    public static RowType rowType(RowType rowType1, RowType rowType2) {
-        if (rowType1 == rowType2)
-            return rowType1;
-        if (rowType1.nFields() != rowType2.nFields())
-            throw notSameShape(rowType1, rowType2);
-        return rowTypeNew(rowType1, rowType2);
-    }
-
-    private static RowType rowTypeNew(RowType rowType1, RowType rowType2) {
-        TInstance[] types = new TInstance[rowType1.nFields()];
-        for(int i=0; i<types.length; ++i) {
-            TInstance type1 = rowType1.typeAt(i);
-            TInstance type2 = rowType2.typeAt(i);
-            if (Objects.equal(type1, type2))
-                types[i] = type1;
-            else if (type1 == null)
-                types[i] = type2;
-            else if (type2 == null)
-                types[i] = type1;
-            else if (type1.equalsExcludingNullable(type2)) {
-                types[i] = type1.nullability() ? type1 : type2;
-            }
-            else
-                throw notSameShape(rowType1, rowType2);
-        }
-        return rowType1.schema().newValuesType(types);
-    }
-
-    private static SetWrongTypeColumns notSameShape(RowType rt1, RowType rt2) {
-        return new SetWrongTypeColumns (rt1, rt2);
     }
 }
