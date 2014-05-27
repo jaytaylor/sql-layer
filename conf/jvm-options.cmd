26a679d69c118632d18e2acb07d203b1cc15731c
Nathan Williams <nathan.williams@foundationdb.com>
2014-05-27 16:23:26 -0400

    More jvm.options cleanup and commenting

--- conf/jvm-options.cmd
+++ conf/jvm-options.cmd
@@ -1,37 +1,31 @@
+REM
 REM This file is for Windows. For Unix-like systems, look in jvm.options.
+REM
+REM This file is loaded into the environment before starting the SQL Layer.
+REM The value of JVM_OPTS will be passed as-is on the Java command line.
+REM
 REM It is important that none of the property values have spaces in them.
+REM
 
-REM The amount of memory, in megabytes, to allocate to the JVM at startup.
-REM You almost certainly want to adjust this for your environment.
-REM Do not include unit suffix.
-REM SET MAX_HEAP_MB=1024
+REM Memory to dedicated to the SQL Layer process. Used for both Xmx and Xms.
+SET MAX_HEAP_SIZE=512M
 
-REM min and max heap sizes should be set to the same value to avoid
-REM stop-the-world GC pauses during resize, and so that we can lock the
-REM heap in memory on startup to prevent any of it from being swapped
-REM out.
+REM Assertions
+SET JVM_OPTS=%JVM_OPTS% -ea
+
+REM Set min and max at the same time to avoid resizing the heap while running.
+SET JVM_OPTS=%JVM_OPTS% -Xms%MAX_HEAP_SIZE%
+SET JVM_OPTS=%JVM_OPTS% -Xmx%MAX_HEAP_SIZE%
 SET JVM_OPTS=%JVM_OPTS% -XX:+HeapDumpOnOutOfMemoryError
 
-REM GC logging options -- uncomment to enable
-REM SET JVM_OPTS=%JVM_OPTS% -XX:+PrintGCDetails
-REM SET JVM_OPTS=%JVM_OPTS% -XX:+PrintGCTimeStamps
-REM SET JVM_OPTS=%JVM_OPTS% -XX:+PrintClassHistogram
-REM SET JVM_OPTS=%JVM_OPTS% -XX:+PrintTenuringDistribution
-REM SET JVM_OPTS=%JVM_OPTS% -XX:+PrintGCApplicationStoppedTime
-REM SET JVM_OPTS=%JVM_OPTS% -Xloggc:gc.log
+REM Prefer IPv4 even if IPv6 is available.
+SET JVM_OPTS=%JVM_OPTS% -Djava.net.preferIPv4Stack=true
 
-REM JMX: metrics and administration interface
-REM 
-REM add this if you're having trouble connecting:
-REM SET JVM_OPTS=%JVM_OPTS% -Djava.rmi.server.hostname=<public name>
-REM 
-REM see 
-REM http://blogs.sun.com/jmxetc/entry/troubleshooting_connection_problems_in_jconsole
-REM for more on configuring JMX through firewalls, etc. (Short version:
-REM get it working with no firewall first.)
-SET JMX_PORT=8082
-SET JVM_OPTS=%JVM_OPTS% -Dcom.sun.management.jmxremote.port=%JMX_PORT%
+REM JMX
+SET JVM_OPTS=%JVM_OPTS% -Dcom.sun.management.jmxremote.port=8082
 SET JVM_OPTS=%JVM_OPTS% -Dcom.sun.management.jmxremote.ssl=false
 SET JVM_OPTS=%JVM_OPTS% -Dcom.sun.management.jmxremote.authenticate=false
 
+REM Debugging
 SET JVM_OPTS=%JVM_OPTS% -Xrunjdwp:transport=dt_socket,address=8000,suspend=n,server=y
+
