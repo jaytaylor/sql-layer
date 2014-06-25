295717c7ec3a75ad6058a98c6501fa51246ae891
Scott Dugas <scott.dugas@foundationdb.com>
2014-06-25 14:53:30 -0400

    Removed bad test
    this isn't really the behavior we want, probably, eventually, and
    it only passed by happenstance

--- src/test/java/com/foundationdb/server/collation/AkCollatorFactoryTest.java
+++ src/test/java/com/foundationdb/server/collation/AkCollatorFactoryTest.java
@@ -165,20 +165,4 @@ public class AkCollatorFactoryTest {
             AkCollatorFactory.setCollationMode(saveMode);
         }
     }
-
-    @Test
-    public void fromCacheDifferentName() throws Exception {
-        AkCollatorFactory.Mode saveMode = AkCollatorFactory.getCollationMode();
-        try {
-            AkCollatorFactory.setCollationMode(DEFAULT_MODE);
-            AkCollator c = AkCollatorFactory.getAkCollator("en_ci");
-            assertEquals("en_ci", c.getName());
-            c = AkCollatorFactory.getAkCollator("en_us_ci");
-            assertEquals("en_us_ci", c.getName());
-            c = AkCollatorFactory.getAkCollator("en_ci");
-            assertEquals("en_ci", c.getName());
-        } finally {
-            AkCollatorFactory.setCollationMode(saveMode);
-        }
-    }
 }
