9144e510070f94fb34523959f28fb07de6abb841
Thomas Jones-Low <thomas.jones-low@foundationdb.com>
2014-07-01 09:11:41 -0400

    can -> can't

--- src/main/java/com/foundationdb/sql/aisddl/AlterTableDDL.java
+++ src/main/java/com/foundationdb/sql/aisddl/AlterTableDDL.java
@@ -350,7 +350,7 @@ public class AlterTableDDL {
                     
                     // Check if the column was added in the same alter as creating the index: 
                     // ALTER TABLE c ADD COLUMN n SERIAL PRIMARY KEY
-                    // You can add and modify the column, so assume the add does the correct thing.
+                    // You can't add and modify the column, so assume the add does the correct thing.
                     boolean columnAdded = false;
                     for (TableChange change : columnChanges) {
                         if (change.getChangeType() ==  ChangeType.ADD && columnName.equals(change.getNewName())) 
