73ab726efd8be129fa0c2dcb44a64ff513e73337
Harrison Chandler <harrison.chandler@foundationdb.com>
2014-07-02 10:52:11 -0400

    Throw exception for AggregateWindowFunction

--- src/main/java/com/foundationdb/sql/optimizer/rule/ASTStatementLoader.java
+++ src/main/java/com/foundationdb/sql/optimizer/rule/ASTStatementLoader.java
@@ -1651,6 +1651,9 @@ public class ASTStatementLoader extends BaseRule
                 return toCondition(valueNode, projects);
             }
             else if (valueNode instanceof UnaryOperatorNode) {
+                if (valueNode instanceof AggregateWindowFunctionNode) {
+                    throw new UnsupportedSQLException("AggregateWindowFunction");
+                }
                 UnaryOperatorNode unary = (UnaryOperatorNode)valueNode;
                 List<ExpressionNode> operands = new ArrayList<>(1);
                 operands.add(toExpression(unary.getOperand(), projects));
