8aa32140bd493fbfb3efeb25575f64f6ce4ec307
Harrison Chandler <harrison.chandler@foundationdb.com>
2014-07-29 15:41:11 -0400

    formatting

--- src/main/java/com/foundationdb/sql/optimizer/rule/cost/CostEstimator.java
+++ src/main/java/com/foundationdb/sql/optimizer/rule/cost/CostEstimator.java
@@ -41,7 +41,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
-
 import static java.lang.Math.round;
 
 public abstract class CostEstimator implements TableRowCounts
