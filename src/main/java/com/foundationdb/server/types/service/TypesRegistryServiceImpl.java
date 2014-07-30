7c7a8edc9b26c67c22b7a10d8c3aec222dc367fb
Thomas Jones-Low <thomas.jones-low@foundationdb.com>
2014-07-30 15:24:32 -0400

    Make the TypesRegistrySchemaTablesService independent of the TypesRegistryService, ensure they are loaded via the services config.

--- src/main/java/com/foundationdb/server/types/service/TypesRegistryServiceImpl.java
+++ src/main/java/com/foundationdb/server/types/service/TypesRegistryServiceImpl.java
@@ -17,28 +17,15 @@
 
 package com.foundationdb.server.types.service;
 
-import com.foundationdb.ais.model.Table;
-import com.foundationdb.ais.model.TableName;
-import com.foundationdb.ais.model.aisb2.AISBBasedBuilder;
-import com.foundationdb.ais.model.aisb2.NewAISBuilder;
-import com.foundationdb.ais.model.aisb2.NewTableBuilder;
-import com.foundationdb.qp.memoryadapter.BasicFactoryBase;
-import com.foundationdb.qp.memoryadapter.MemoryAdapter;
-import com.foundationdb.qp.memoryadapter.MemoryGroupCursor;
-import com.foundationdb.qp.memoryadapter.SimpleMemoryGroupScan;
 import com.foundationdb.server.error.ServiceStartupException;
 import com.foundationdb.server.service.Service;
-import com.foundationdb.server.service.is.SchemaTablesService;
 import com.foundationdb.server.service.jmx.JmxManageable;
-import com.foundationdb.server.service.session.Session;
-import com.foundationdb.server.store.SchemaManager;
 import com.foundationdb.server.types.TAggregator;
 import com.foundationdb.server.types.TCast;
 import com.foundationdb.server.types.TClass;
 import com.foundationdb.server.types.TKeyComparable;
 import com.foundationdb.server.types.TScalar;
 import com.foundationdb.server.types.TOverload;
-import com.foundationdb.server.types.common.types.TypesTranslator;
 import com.foundationdb.server.types.texpressions.TValidatedAggregator;
 import com.foundationdb.server.types.texpressions.TValidatedOverload;
 import com.foundationdb.server.types.texpressions.TValidatedScalar;
@@ -46,9 +33,7 @@ import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Iterators;
 import com.google.common.collect.Multimap;
-import com.google.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -219,40 +204,6 @@ public final class TypesRegistryServiceImpl
 
     // inner classes
     
-    public class TypesRegistrySchemaTablesServiceImpl
-    extends SchemaTablesService
-    implements Service {
-
-        @Inject
-        public TypesRegistrySchemaTablesServiceImpl(SchemaManager schemaManager) {
-            super(schemaManager);
-        }
-
-        @Override
-        public void start() {
-            TableName overloadsName = TableName.create(TableName.INFORMATION_SCHEMA, "ak_overloads");
-            OverloadsTableFactory overloadsFactory = new OverloadsTableFactory(overloadsName);
-            attach(overloadsFactory.table(schemaManager.getTypesTranslator()),  overloadsFactory);
-            
-            TableName castsName = TableName.create(TableName.INFORMATION_SCHEMA, "ak_casts");
-            CastsTableFactory castsFactory = new CastsTableFactory(castsName);
-            attach (castsFactory.table(schemaManager.getTypesTranslator()), castsFactory);
-        }
-
-        @Override
-        public void stop() {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void crash() {
-            // TODO Auto-generated method stub
-            
-        }
-        
-    }
-
     private class Bean implements TypesRegistryMXBean {
 
         @Override
@@ -421,119 +372,6 @@ public final class TypesRegistryServiceImpl
         for (int i = 1; i < ints.length; ++i)
             result = Math.min(result, ints[i]);
         return result;
-    }
-
-    private abstract class MemTableBase extends BasicFactoryBase {
-
-        protected abstract void buildTable(NewTableBuilder builder);
-
-        public Table table(TypesTranslator typesTranslator) {
-            NewAISBuilder builder = AISBBasedBuilder.create(typesTranslator);
-            buildTable(builder.table(getName()));
-            return builder.ais(false).getTable(getName());
-        }
-
-        protected MemTableBase(TableName tableName) {
-            super(tableName);
-        }
-    }
-
-    private class CastsTableFactory extends MemTableBase {
-
-        @Override
-        protected void buildTable(NewTableBuilder builder) {
-            builder.colString("source_bundle", 64, false)
-                    .colString("source_type", 64, false)
-                    .colString("target_bundle", 64, false)
-                    .colString("target_type", 64, false)
-                    .colString("is_strong", 3, false)
-                    .colString("is_derived", 3, false);
-        }
-
-        @Override
-        public MemoryGroupCursor.GroupScan getGroupScan(MemoryAdapter adapter) {
-            Collection<Map<TClass, TCast>> castsBySource = castsResolver.castsBySource();
-            Collection<TCast> castsCollections = new ArrayList<>(castsBySource.size());
-            for (Map<?, TCast> castMap : castsBySource) {
-                castsCollections.addAll(castMap.values());
-            }
-            return new SimpleMemoryGroupScan<TCast>(adapter, getName(), castsCollections.iterator()) {
-                @Override
-                protected Object[] createRow(TCast data, int hiddenPk) {
-                    return new Object[] {
-                            data.sourceClass().name().bundleId().name(),
-                            data.sourceClass().name().unqualifiedName(),
-                            data.targetClass().name().bundleId().name(),
-                            data.targetClass().name().unqualifiedName(),
-                            boolResult(castsResolver.isStrong(data)),
-                            boolResult(data instanceof TCastsRegistry.ChainedCast),
-                            hiddenPk
-                    };
-                }
-            };
-        }
-
-        @Override
-        public long rowCount(Session session) {
-            long count = 0;
-            for (Map<?,?> castsBySource : castsResolver.castsBySource()) {
-                count += castsBySource.size();
-            }
-            return count;
-        }
-
-        private CastsTableFactory(TableName tableName) {
-            super(tableName);
-        }
-    }
-
-    private class OverloadsTableFactory extends MemTableBase {
-
-        @Override
-        protected void buildTable(NewTableBuilder builder) {
-            builder.colString("name", 128, false)
-                   .colBigInt("priority_order", false)
-                   .colString("inputs", 256, false)
-                   .colString("output", 256, false)
-                   .colString("internal_impl", 256, false);
-        }
-
-        @Override
-        public MemoryGroupCursor.GroupScan getGroupScan(MemoryAdapter adapter) {
-            Iterator<? extends TValidatedOverload> allOverloads = Iterators.concat(
-                    scalarsRegistry.iterator(),
-                    aggreatorsRegistry.iterator());
-            return new SimpleMemoryGroupScan<TValidatedOverload>(adapter, getName(), allOverloads) {
-                @Override
-                protected Object[] createRow(TValidatedOverload data, int hiddenPk) {
-                    return new Object[] {
-                            data.displayName().toLowerCase(),
-                            lowest(data.getPriorities()),
-                            data.describeInputs(),
-                            data.resultStrategy().toString(true),
-                            data.id(),
-                            hiddenPk
-                    };
-                }
-            };
-        }
-
-        @Override
-        public long rowCount(Session session) {
-            return countOverloads(aggreatorsRegistry) + countOverloads(scalarsRegistry);
-        }
-
-        private long countOverloads(ResolvablesRegistry<?> registry) {
-            long count = 0;
-            for (ScalarsGroup<?> group : registry.allScalarsGroups()) {
-                count += group.getOverloads().size();
-            }
-            return count;
-        }
-
-        public OverloadsTableFactory(TableName tableName) {
-            super(tableName);
-        }
     }
 
 }
