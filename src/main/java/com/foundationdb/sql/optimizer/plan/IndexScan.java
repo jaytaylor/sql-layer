bc48d98da3bf2a57f4d868848b5f0c4b554fe3c2
Scott Dugas <scott.dugas@foundationdb.com>
2014-07-31 11:54:16 -0400

    Revert "added getNUnions, which will hopefully help"
    This reverts commit 402a99932100bc22dd9a3d98ea87392ca00840c7.

--- src/main/java/com/foundationdb/sql/optimizer/plan/IndexScan.java
+++ src/main/java/com/foundationdb/sql/optimizer/plan/IndexScan.java
@@ -24,7 +24,6 @@ import java.util.*;
 
 public abstract class IndexScan extends BaseScan implements IndexIntersectionNode<ConditionExpression,IndexScan>, JoinTreeScan
 {
-
     public static enum OrderEffectiveness {
         NONE, PARTIAL_GROUPED, GROUPED, SORTED, FOR_MIN_MAX
     }
@@ -117,7 +116,7 @@ public abstract class IndexScan extends BaseScan implements IndexIntersectionNod
 
     @Override
     public int getPeggedCount() {
-        return getNEquality() + getNUnions();
+        return getNEquality();
     }
 
     public abstract List<OrderByExpression> getOrdering();
@@ -141,10 +140,6 @@ public abstract class IndexScan extends BaseScan implements IndexIntersectionNod
      * The index of the first column that may have more than one result
      */
     public abstract int getNEquality();
-    /**
-     * The number of UNION columns after the initial Equality indexes
-     */
-    public abstract int getNUnions();
     public abstract boolean isAscendingAt(int index);
     public abstract boolean isRecoverableAt(int index);
     
