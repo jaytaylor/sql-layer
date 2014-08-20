dcf8091f12cebd73e9952151484f54f95e491b55
Scott Dugas <scott.dugas@foundationdb.com>
2014-08-20 12:04:29 -0400

    Updated RulesTests

--- src/test/resources/com/foundationdb/sql/optimizer/rule/reverse-ins/in-subquery-left-cond.sql
+++ src/test/resources/com/foundationdb/sql/optimizer/rule/reverse-ins/in-subquery-left-cond.sql
@@ -1,3 +1,5 @@
+-- NOTE: if we fix ANTI join to correctly handle null values this can be switched back to an anti join
+-- instead of a subquery.
 SELECT name FROM customers
  WHERE cid NOT IN (SELECT cid FROM orders LEFT OUTER JOIN child ON orders.oid = child.pid
                     WHERE child.id IS NULL)
