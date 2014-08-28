a18cee78248beadb79084bd1887fa2b27053b56e
Thomas Jones-Low <thomas.jones-low@foundationdb.com>
2014-08-27 22:09:40 -0400

    Update new test to functional, passing tests.

--- src/test/resources/com/foundationdb/sql/pg/pipeline-select/select-5.sql
+++ src/test/resources/com/foundationdb/sql/pg/pipeline-select/select-5.sql
@@ -1,7 +1,7 @@
 SELECT * FROM artists WHERE (
  (artists.id NOT IN
-    (SELECT albums_artists.artist_id FROM albums_artists
-        INNER JOIN albums ON (albums.id = albums_artists.album_id)
-        INNER JOIN albums_artists AS albums_artists_0 ON (albums_artists_0.album_id = albums.id)
-        WHERE ((albums_artists_0.artist_id = 1) AND (albums_artists.artist_id IS NOT NULL))))
+    (SELECT join_albums_artists.artist_id FROM join_albums_artists
+        INNER JOIN albums ON (albums.id = join_albums_artists.album_id)
+        INNER JOIN join_albums_artists AS albums_artists_0 ON (albums_artists_0.album_id = albums.id)
+        WHERE ((albums_artists_0.artist_id = 1) AND (join_albums_artists.artist_id IS NOT NULL))))
  OR (artists.id IS NULL))
