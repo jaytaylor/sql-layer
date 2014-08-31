0ae0e30fcb9b6a321b329f2ec9a1613a870292eb
Nathan Williams <nathan.williams@foundationdb.com>
2014-08-31 14:15:36 -0400

    Flip pipeline test to check off

--- src/test/java/com/foundationdb/sql/pg/PostgresServerMiscYamlPipelineOffDT.java
+++ src/test/java/com/foundationdb/sql/pg/PostgresServerMiscYamlPipelineOffDT.java
@@ -21,22 +21,23 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PostgresServerMiscYamlPipelineDT extends PostgresServerMiscYamlIT {
+/** Intended to test the opposite of default settings */
+public class PostgresServerMiscYamlPipelineOffDT extends PostgresServerMiscYamlIT {
 
-    public final static String[] PIPELINE_PROPERTIES = {"fdbsql.pipeline.map.enabled=true",
-                                                        "fdbsql.pipeline.indexScan.lookaheadQuantum=10",
-                                                        "fdbsql.pipeline.groupLookup.lookaheadQuantum=10",
-                                                        "fdbsql.pipeline.unionAll.openBoth=true",
-                                                        "fdbsql.pipeline.selectBloomFilter.enabled=true"}; 
+    public final static String[] PIPELINE_PROPERTIES = {"fdbsql.pipeline.map.enabled=false",
+                                                        "fdbsql.pipeline.indexScan.lookaheadQuantum=1",
+                                                        "fdbsql.pipeline.groupLookup.lookaheadQuantum=1",
+                                                        "fdbsql.pipeline.unionAll.openBoth=false",
+                                                        "fdbsql.pipeline.selectBloomFilter.enabled=false"};
 
-    public PostgresServerMiscYamlPipelineDT(String caseName, File file) {
+    public PostgresServerMiscYamlPipelineOffDT(String caseName, File file) {
         super(caseName, file);
     }
     
     @Override
     protected Map<String, String> startupConfigProperties() {
         Map<String, String> superProperties = super.startupConfigProperties();
-        Map<String, String> properties = new HashMap<String, String>();
+        Map<String, String> properties = new HashMap<>();
         for (String property : PIPELINE_PROPERTIES) {
             String[] pieces = property.split("=");
             properties.put(pieces[0], pieces[1]);
