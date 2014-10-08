12c73311fedd93f013a17f714c10801410b3898c
Scott Dugas <scott.dugas@foundationdb.com>
2014-10-08 16:11:12 -0400

    Added type to ResultSet fields

--- src/main/java/com/foundationdb/sql/optimizer/plan/ResultSet.java
+++ src/main/java/com/foundationdb/sql/optimizer/plan/ResultSet.java
@@ -75,7 +75,7 @@ public class ResultSet extends BasePlanWithInput
         }
 
         public String toString() {
-            return name;
+            return "(" + name + ":" + type + ")";
         }
     }
 
