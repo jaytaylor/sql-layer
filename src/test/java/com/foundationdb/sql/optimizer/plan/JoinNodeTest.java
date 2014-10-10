8a3f0fb3cb84499d608ccfabdba247b911bf08c2
Harrison Chandler <harrison.chandler@foundationdb.com>
2014-10-10 10:27:04 -0400

    updated test a little

--- src/test/java/com/foundationdb/sql/optimizer/plan/JoinNodeTest.java
+++ src/test/java/com/foundationdb/sql/optimizer/plan/JoinNodeTest.java
@@ -23,24 +23,37 @@ import com.foundationdb.ais.model.AkibanInformationSchema;
 import com.foundationdb.ais.model.Table;
 import com.foundationdb.ais.model.aisb2.AISBBasedBuilder;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class JoinNodeTest {
+
+    TypesTranslator typesTranslator;
+    AkibanInformationSchema ais;
+    Table table;
+    TableNode node;
+    TableSource source;
+
+    @Before
+    public void setup() {
+        typesTranslator = MTypesTranslator.INSTANCE;
+        ais = AISBBasedBuilder.create("s", typesTranslator)
+                              .table("t1").colString("first_name", 32)
+                              .colString("last_name", 32)
+                              .ais();
+        table = ais.getTable("s", "t1");
+        node = new TableNode(table, new TableTree());
+        source = new TableSource(node, true, "t1");
+    }
+
     @Test
     public void TestDuplicate() {
-        TypesTranslator typesTranslator = MTypesTranslator.INSTANCE;
-        AkibanInformationSchema ais = AISBBasedBuilder.create("s", typesTranslator)
-            .table("t1").colString("first_name", 32).colString("last_name", 32)
-            .ais();
-        Table table = ais.getTable("s", "t1");
-        TableNode node = new TableNode(table, new TableTree());
-        TableSource source = new TableSource(node, true, "t1");
-
         JoinNode joinNode = new JoinNode((Joinable)source, (Joinable)source, JoinNode.JoinType.LEFT);
-        JoinNode duplicate = (JoinNode)joinNode.duplicate();
-
+        JoinNode duplicate = (JoinNode)joinNode.duplicate(); // shouldn't throw null error
         assertEquals(joinNode.getJoinType(), duplicate.getJoinType());
+        assertEquals(joinNode.getJoinConditions(), duplicate.getJoinConditions());
+        assertEquals(joinNode.getImplementation(), duplicate.getImplementation());
     }
 }
