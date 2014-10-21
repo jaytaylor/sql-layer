221dd5ed4c84cce45379338d838acef4aa65b4c7
Scott Dugas <scott.dugas@foundationdb.com>
2014-10-21 10:51:44 -0400

    Fixed warning by importing correct class

--- src/test/java/com/foundationdb/http/CsrfProtectionRefererFilterTest.java
+++ src/test/java/com/foundationdb/http/CsrfProtectionRefererFilterTest.java
@@ -23,6 +23,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -30,7 +31,6 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class CsrfProtectionRefererFilterTest {
 
