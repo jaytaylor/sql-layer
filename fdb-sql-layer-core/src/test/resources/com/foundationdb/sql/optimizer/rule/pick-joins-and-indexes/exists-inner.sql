315946a9424b9b83f6df7d1234a5ef23713c25bb
Scott Dugas <scott.dugas@foundationdb.com>
2014-11-21 11:46:52 -0500

    Added expected output for exists-inner rule test

--- fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/rule/pick-joins-and-indexes/exists-inner.sql
+++ fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/rule/pick-joins-and-indexes/exists-inner.sql
@@ -1,3 +1,5 @@
+-- The key thing that I'm checking here is that the semi
+-- stays outside the inner. That was the original bug.
 SELECT t1.c1, t1.c2 FROM t1 WHERE EXISTS (
   SELECT 1 FROM t2, t3
     WHERE t1.c1 = t2.c1 AND t3.c1 = t2.c2 AND t3.c2 = 7)
