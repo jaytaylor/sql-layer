0627391b073c1fe213a18fb169adae81561ce66c
Scott Dugas <scott.dugas@foundationdb.com>
2014-12-04 14:44:02 -0500

    Reformatted test to have more expressions/conds
    this puts in line with the other tests in the same series and
    asserts that we do more correctly

--- fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/flatten/select-triple-nested-outer-count.sql
+++ fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/flatten/select-triple-nested-outer-count.sql
@@ -1,3 +1,3 @@
 -- The middle SELECT here can't be flattened out into the COUNT() because it has an expression, but the inner most
 -- SELECT can be flattened out into the middle one
-SELECT COUNT(val) FROM (SELECT id + 3 AS val FROM (SELECT id FROM t1) AS anon1) AS anon2
+SELECT COUNT(mid) FROM (SELECT iid * 5 AS mid FROM (SELECT id-10 AS iid FROM parent WHERE id > 3) AS innerSelect WHERE iid < 5) AS midselect WHERE mid % 2 = 0
