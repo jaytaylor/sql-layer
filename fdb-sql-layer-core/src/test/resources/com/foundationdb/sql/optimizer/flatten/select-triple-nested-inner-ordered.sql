5b5c18a0c9d2b877f87b7054719e4286bd2bc2ee
Scott Dugas <scott.dugas@foundationdb.com>
2014-12-04 14:36:13 -0500

    Expected output

--- fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/flatten/select-triple-nested-inner-ordered.sql
+++ fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/flatten/select-triple-nested-inner-ordered.sql
@@ -1,1 +1,1 @@
-SELECT mid + 6 FROM (SELECT iid * 5 FROM (SELECT id-10 AS iid FROM parent WHERE id > 3 ORDER BY name) AS innerSelect WHERE iid < 5) AS midselect WHERE mid % 2 = 0
+SELECT mid + 6 FROM (SELECT iid * 5 AS mid FROM (SELECT id-10 AS iid FROM parent WHERE id > 3 ORDER BY name) AS innerSelect WHERE iid < 5) AS midselect WHERE mid % 2 = 0
