1ad3a6276d5c0d457d34ed47fabdd735a732ece0
SytzeH <sytze.harkema@foundationdb.com>
2014-12-08 15:55:28 -0500

    add retrictions on calling routines

--- fdb-sql-layer-core/src/main/java/com/foundationdb/sql/server/ServerJavaRoutine.java
+++ fdb-sql-layer-core/src/main/java/com/foundationdb/sql/server/ServerJavaRoutine.java
@@ -22,7 +22,9 @@ import java.util.Queue;
 
 import com.foundationdb.ais.model.Parameter;
 import com.foundationdb.qp.operator.QueryBindings;
+import com.foundationdb.server.error.*;
 import com.foundationdb.server.explain.Explainable;
+import com.foundationdb.server.service.security.*;
 import com.foundationdb.sql.routinefw.ShieldedInvokable;
 import com.foundationdb.sql.routinefw.RoutineFirewall;
 
@@ -63,6 +65,10 @@ public abstract class ServerJavaRoutine implements Explainable, ShieldedInvokabl
     }
 
     public void invoke() {
+        SecurityService ss = context.getServiceManager().getServiceByClass(SecurityService.class);
+        if (!ss.isAccessible(context.getSession(), invocation.getRoutineName().getSchemaName()))  {
+            throw new ProtectedItemException(invocation.getRoutineName().getTableName(), invocation.getRoutineName().getSchemaName());
+        }
         RoutineFirewall.callInvoke(this);
     }
     
