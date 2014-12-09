21963bd37b4bd84a4581048b1e82416947a7b6ef
Mike McMahon <mmcm@comcast.net>
2014-12-09 16:59:26 -0500

    Add snapshot IT showing that whereas serializable conflicts, this skews. Add read committed IT showing that "long" scans complete with expected isolation violations.

---
+++ fdb-sql-layer-core/src/test/java/com/foundationdb/server/test/it/isolation/ReadCommittedIsolationIT.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (C) 2009-2014 FoundationDB, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.foundationdb.server.test.it.isolation;
+
+import com.foundationdb.sql.embedded.JDBCConnection;
+
+import java.sql.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReadCommittedIsolationIT extends IsolationITBase
+{
+    private static final int NROWS = 10;
+
+    @Override
+    public Map<String,String> startupConfigProperties() {
+        Map<String,String> props = new HashMap<>();
+        props.putAll(super.startupConfigProperties());
+        props.put("fdbsql.fdb.periodically_commit.scan_limit", "2");
+        // With the ordinary amount of lookahead, won't see changes made in
+        // the middle because the scans will have already been done.
+        props.put("fdbsql.pipeline.groupLookup.lookaheadQuantum", "2");
+        return props;
+    }
+
+    @Before
+    public void populate() {
+        int tid = createTable(SCHEMA_NAME, "t1", "id INT PRIMARY KEY, n INT");
+        for (int i = 0; i < NROWS; i++) {
+            writeRow(tid, i, i);
+        }
+    }
+
+    @Test
+    @Isolation(JDBCConnection.TRANSACTION_READ_COMMITTED_NO_SNAPSHOT)
+    public void scanIdle() throws SQLException {
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT id, n FROM t1")) {
+            for (int i = 0; i < NROWS; i++) {
+                assertTrue("more rows", rs.next());
+                assertEquals("id", i, rs.getInt(1));
+                assertEquals("n", i, rs.getInt(2));
+            }
+        }
+    }
+
+    @Test
+    @Isolation(JDBCConnection.TRANSACTION_READ_COMMITTED_NO_SNAPSHOT)
+    public void scanWhileUpdate() throws SQLException {
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT id, n FROM t1")) {
+            for (int i = 0; i < NROWS; i++) {
+                if (i == 3) {
+                    try (Connection conn2 = getAutoCommitConnection();
+                         Statement stmt2 = conn2.createStatement()) {
+                        assertEquals(1, stmt2.executeUpdate("UPDATE t1 SET n = n + 1 WHERE id = " + NROWS / 2));
+                    }
+                }
+                assertTrue("more rows", rs.next());
+                assertEquals("id", i, rs.getInt(1));
+                assertEquals("n", (i == NROWS / 2) ? i + 1 : i, rs.getInt(2));
+            }
+        }
+    }
+
+    @Test
+    @Isolation(JDBCConnection.TRANSACTION_READ_COMMITTED_NO_SNAPSHOT)
+    public void scanWhileDelete() throws SQLException {
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             // A plan that will have a nested scan.
+             ResultSet rs = stmt.executeQuery("SELECT id, n FROM t1 WHERE id > 0")) {
+            for (int i = 1; i < NROWS; i++) {
+                if (i == 2) {
+                    try (Connection conn2 = getAutoCommitConnection();
+                         Statement stmt2 = conn2.createStatement()) {
+                        assertEquals(1, stmt2.executeUpdate("DELETE FROM t1 WHERE id = " + NROWS / 2));
+                    }
+                }
+                if (i == NROWS / 2) continue;
+                assertTrue("more rows", rs.next());
+                assertEquals("id", i, rs.getInt(1));
+                assertEquals("n", i, rs.getInt(2));
+            }
+        }
+    }
+
+    @Test
+    @Isolation(JDBCConnection.TRANSACTION_READ_COMMITTED_NO_SNAPSHOT)
+    public void scanWhileAlter() throws SQLException {
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM t1")) {
+            for (int i = 0; i < NROWS; i++) {
+                if (i == 2) {
+                    try (Connection conn2 = getAutoCommitConnection();
+                         Statement stmt2 = conn2.createStatement()) {
+                        stmt2.execute("ALTER TABLE t1 DROP COLUMN n");
+                    }
+                }
+                if (!rs.next()) break; // Only check that it vanished.
+                assertEquals("id", i, rs.getInt(1));
+                assertEquals("n", i, rs.getInt(2));
+            }
+        }
+    }
+
+}
