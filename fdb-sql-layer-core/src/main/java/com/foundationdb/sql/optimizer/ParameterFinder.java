561d5d73ec52bbff75fa65fe26a1b0543fabdb34
Harrison Chandler <harrison.chandler@foundationdb.com>
2014-12-12 14:19:03 -0500

    skip some stuff in the parameter finder.

--- fdb-sql-layer-core/src/main/java/com/foundationdb/sql/optimizer/ParameterFinder.java
+++ fdb-sql-layer-core/src/main/java/com/foundationdb/sql/optimizer/ParameterFinder.java
@@ -19,6 +19,7 @@ package com.foundationdb.sql.optimizer;
 
 import com.foundationdb.sql.StandardException;
 import com.foundationdb.sql.parser.ParameterNode;
+import com.foundationdb.sql.parser.SetOperatorNode;
 import com.foundationdb.sql.parser.Visitable;
 import com.foundationdb.sql.parser.Visitor;
 import com.foundationdb.server.error.SQLParserInternalException;
@@ -53,12 +54,23 @@ public class ParameterFinder implements Visitor
 
     @Override
     public boolean skipChildren(Visitable node) {
+        if (node instanceof SetOperatorNode) {
+            try {
+                ((SetOperatorNode)node).getLeftResultSet().accept(this);
+                ((SetOperatorNode)node).getRightResultSet().accept(this);
+            } catch (StandardException e) {
+                throw new SQLParserInternalException(e);
+            }
+            return true;
+        }
         return false;
     }
+
     @Override
     public boolean visitChildrenFirst(Visitable node) {
         return false;
     }
+
     @Override
     public boolean stopTraversal() {
         return false;
