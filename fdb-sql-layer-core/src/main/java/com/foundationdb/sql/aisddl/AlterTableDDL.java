79b49b35195f1a4c0ace6d9421014a5083bd086e
Scott Dugas <scott.dugas@foundationdb.com>
2015-01-05 12:34:33 -0500

    Use accessor

--- fdb-sql-layer-core/src/main/java/com/foundationdb/sql/aisddl/AlterTableDDL.java
+++ fdb-sql-layer-core/src/main/java/com/foundationdb/sql/aisddl/AlterTableDDL.java
@@ -121,8 +121,8 @@ public class AlterTableDDL {
         }
 
         ChangeLevel level = null;
-        if((alterTable.tableElementList != null) && !alterTable.tableElementList.isEmpty()) {
-            level = processAlter(ddlFunctions, session, defaultSchemaName, table, alterTable.tableElementList, context);
+        if((alterTable.getTableElementList() != null) && !alterTable.getTableElementList().isEmpty()) {
+            level = processAlter(ddlFunctions, session, defaultSchemaName, table, alterTable.getTableElementList(), context);
         }
 
         if(level == null) {
