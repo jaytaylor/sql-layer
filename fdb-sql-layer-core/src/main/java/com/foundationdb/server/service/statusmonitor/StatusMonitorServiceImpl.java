e293dcecf659df4b42c357bb1218acd783fd7758
Thomas Jones-Low <thomas.jones-low@foundationdb.com>
2015-01-18 10:21:03 -0500

    Add the details to the Status Montitor Service JSON output.

--- fdb-sql-layer-core/src/main/java/com/foundationdb/server/service/statusmonitor/StatusMonitorServiceImpl.java
+++ fdb-sql-layer-core/src/main/java/com/foundationdb/server/service/statusmonitor/StatusMonitorServiceImpl.java
@@ -17,11 +17,15 @@
 package com.foundationdb.server.service.statusmonitor;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
-import java.util.Formatter;
 import java.util.List;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,17 +34,29 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.foundationdb.Database;
 import com.foundationdb.Transaction;
+import com.foundationdb.ais.model.TableName;
 import com.foundationdb.async.Function;
 import com.foundationdb.async.Future;
 import com.foundationdb.directory.DirectoryLayer;
 import com.foundationdb.directory.DirectorySubspace;
+import com.foundationdb.qp.operator.RowCursor;
+import com.foundationdb.qp.row.Row;
+import com.foundationdb.server.api.dml.ColumnSelector;
 import com.foundationdb.server.service.Service;
 import com.foundationdb.server.service.config.ConfigurationService;
+import com.foundationdb.server.service.externaldata.GenericRowTracker;
+import com.foundationdb.server.service.externaldata.JsonRowWriter;
 import com.foundationdb.server.store.FDBHolder;
+import com.foundationdb.server.types.FormatOptions;
+import com.foundationdb.server.types.value.ValueSource;
 import com.foundationdb.sql.Main;
 import com.foundationdb.sql.embedded.EmbeddedJDBCService;
+import com.foundationdb.sql.embedded.JDBCDriver;
+import com.foundationdb.sql.embedded.JDBCResultSet;
+import com.foundationdb.sql.embedded.JDBCResultSetMetaData;
 import com.foundationdb.tuple.ByteArrayUtil;
 import com.foundationdb.tuple.Tuple2;
+import com.foundationdb.util.AkibanAppender;
 import com.foundationdb.util.JsonUtils;
 import com.google.inject.Inject;
 
@@ -63,7 +79,7 @@ public class StatusMonitorServiceImpl implements StatusMonitorService, Service {
     protected volatile boolean running, backgroundIdle;
     private long flushInterval;
     private Future<Void> instanceWatch;
-    
+    FormatOptions options;
     @Inject
     public StatusMonitorServiceImpl (ConfigurationService configService, 
             FDBHolder fdbService,
@@ -71,11 +87,14 @@ public class StatusMonitorServiceImpl implements StatusMonitorService, Service {
         this.configService= configService;
         this.fdbService = fdbService;
         this.jdbcService = jdbcService;
+        this.options = new FormatOptions();
+        
     }
     
     @Override
     public void start() {
         flushInterval = Long.parseLong(configService.getProperty(CONFIG_FLUSH_INTERVAL));
+        options.set(FormatOptions.JsonBinaryFormatOption.fromProperty(configService.getProperty("fdbsql.sql.jsonbinary_output")));
 
         host = "127.0.0.1";
         try {
@@ -210,6 +229,12 @@ public class StatusMonitorServiceImpl implements StatusMonitorService, Service {
             gen.writeStringField("version", Main.VERSION_INFO.versionLong);
             gen.writeStringField("host", host);
             gen.writeStringField("port", port);
+            summary(INSTANCE, INSTANCE_SQL, gen);
+            summary(SERVERS, SERVERS_SQL, gen);
+            summary(SESSIONS, SESSIONS_SQL, gen);
+            summary(STATISTICS, STATISTICS_SQL, gen);
+            summary(GARBAGE_COLLECTORS, GARBAGE_COLLECTORS_SQL, gen);
+            summary(MEMORY_POOLS, MEMORY_POOLS_SQL, gen);
             gen.writeEndObject();
             gen.flush();
         } catch (JsonGenerationException ex) {
@@ -217,7 +242,137 @@ public class StatusMonitorServiceImpl implements StatusMonitorService, Service {
         } catch (IOException e) {
             return null;
         }
-        
+        logger.debug("status: {}", str.toString());
         return str.toString();
+    }
+    
+    private static final String INSTANCE = "instance";
+    private static final String INSTANCE_SQL =  "select server_host, server_store, server_jit_compiler_time from information_schema.server_instance_summary";
+    
+    private static final String SERVERS = "servers";
+    private static final String SERVERS_SQL = "select * from information_schema.server_servers";
+    
+    private static final String SESSIONS = "sessions";
+    private static final String SESSIONS_SQL  = "select session_id, start_time, server_type, remote_address,"+ 
+        "query_count, failed_query_count, query_from_cache, logged_statements," +
+        "call_statement_count, ddl_statement_count, dml_statement_count, select_statement_count," + 
+        "other_statement_count from information_schema.server_sessions";
+    
+    private static final String STATISTICS = "statistics";
+    private static final String STATISTICS_SQL = "select * from information_schema.server_statistics_summary";
+    
+    private static final String GARBAGE_COLLECTORS = "garbage collectors";
+    private static final String GARBAGE_COLLECTORS_SQL = "select * from information_schema.server_garbage_collectors";
+    
+    private static final String MEMORY_POOLS = "memory pools";
+    private static final String MEMORY_POOLS_SQL = "select * from information_schema.server_memory_pools";
+    
+    protected void summary  (String name, String sql, JsonGenerator gen) throws JsonGenerationException, IOException {
+       Properties props = new Properties();
+       props.setProperty("database", TableName.INFORMATION_SCHEMA);
+       try (Connection conn = jdbcService.getDriver().connect(JDBCDriver.URL, props);
+               Statement s = conn.createStatement()) {
+
+           gen.writeArrayFieldStart(name);
+           JDBCResultSet rs = (JDBCResultSet)s.executeQuery(sql);
+           StringWriter strings = new StringWriter();
+           PrintWriter writer = new PrintWriter(strings);
+           collectResults(rs, writer, options);
+           gen.writeRawValue(strings.toString());
+           gen.writeEndArray();
+       } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+       }
+    }
+
+    private void collectResults(JDBCResultSet resultSet, PrintWriter writer, FormatOptions opt) throws SQLException {
+        AkibanAppender appender = AkibanAppender.of(writer);
+
+        SQLOutput cursor = new SQLOutput(resultSet);
+        try {
+            JsonRowWriter jsonRowWriter = new JsonRowWriter(cursor);
+            if (jsonRowWriter.writeRowsFromOpenCursor(cursor, appender, "\n", cursor, opt)) {
+                appender.append('\n');
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
+    private class SQLOutput extends GenericRowTracker implements RowCursor, JsonRowWriter.WriteRow {
+
+        private JDBCResultSet resultSet;
+
+        public SQLOutput (JDBCResultSet rs) {
+            this.resultSet = rs;
+        }
+        @Override
+        public void open() {
+        }
+
+        @Override
+        public void close() {
+        }
+
+        @Override
+        public Row next() {
+            try {
+                if (resultSet.next()) {
+                    return resultSet.unwrap(Row.class);
+                } else {
+                    return null;
+                }
+            } catch(SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+
+
+        @Override
+        public void jump(Row row, ColumnSelector columnSelector) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isIdle() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isActive() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isClosed() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setIdle() {
+        }
+
+        @Override
+        public String getRowName() {
+            return null;
+        }
+
+        @Override
+        public void write(Row row, AkibanAppender appender,
+                FormatOptions options) {
+            try {
+                JDBCResultSetMetaData metaData = resultSet.getMetaData();
+                boolean begun = false;
+                for(int col = 1; col <= metaData.getColumnCount(); ++col) {
+                    String colName = metaData.getColumnLabel(col);
+                    ValueSource valueSource = row.value(col - 1);
+                    JsonRowWriter.writeValue(colName, valueSource, appender, !begun, options);
+                    begun = true;
+                }
+            } catch(SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        }
     }
 }
