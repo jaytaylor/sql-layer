13ff9e0d7416b6581e16b522260b9763874e6c0f
Mike McMahon <mmcm@comcast.net>
2015-02-05 13:55:18 -0500

    Will need types resolver for outer join promotion. Which means IN list will be sorted and deduped, so adjust an existing test.

--- fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/rule/promote-outer/in-condition.sql
+++ fdb-sql-layer-core/src/test/resources/com/foundationdb/sql/optimizer/rule/promote-outer/in-condition.sql
@@ -1,2 +1,2 @@
 SELECT * FROM customers LEFT JOIN orders ON customers.cid = orders.cid
- WHERE orders.order_date IN ('2011-01-01', '2010-12-31')
+ WHERE orders.order_date IN ('2010-12-31', '2011-01-01')
